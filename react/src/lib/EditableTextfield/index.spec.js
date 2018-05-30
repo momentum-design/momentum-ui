0b985355222e4e818a048bc05a5743616172fc4d
Shashank <smeyar@cisco.com>
2018-05-30 11:16:26 +0530

    feat(EditableTextfield): Added test cases for key handling

--- react/src/lib/EditableTextfield/index.spec.js
+++ react/src/lib/EditableTextfield/index.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import EditableTextfield from '@collab-ui/react/EditableTextfield';
 
 describe('tests for <EditableTextfield />', () => {
@@ -25,12 +25,39 @@ describe('tests for <EditableTextfield />', () => {
   });
 
   it('should click on Editable Textfield, edit and save', () => {
-    const container = shallow(<EditableTextfield inputText={'Hello World'}/>);
+    const container = mount(<EditableTextfield inputText={'Hello World'}/>);
     container.find('span').simulate('click');
-    container.find('Input').simulate('keyDown', { keyCode: 27, key: 'Enter' });
+
+    container.find('.cui-input').simulate('change', { target: { value: "Hello World Tom" } });
+    container.find('.cui-input').simulate('keydown', { keyCode: 13, key: 'Enter' });
+
+    expect(container.find('span').text()).toEqual('Hello World Tom');
+    expect(container.find('span').length).toEqual(1);
+    expect(container.find('Input').length).toEqual(0);
+  });
+
+  it('should click on Editable Textfield, edit and exit(escape)', () => {
+    const container = mount(<EditableTextfield inputText={'Hello World'}/>);
+    container.find('span').simulate('click');
+
+    container.find('.cui-input').simulate('change', { target: { value: "Hello World Tom" } });
+    container.find('.cui-input').simulate('keydown', { keyCode: 27, key: 'Escape' });
 
     expect(container.find('span').text()).toEqual('Hello World');
     expect(container.find('span').length).toEqual(1);
     expect(container.find('Input').length).toEqual(0);
   });
+
+  it('should click on Editable Textfield, edit and blur', () => {
+    const container = mount(<EditableTextfield inputText={'Hello World'}/>);
+    container.find('span').simulate('click');
+
+    container.find('.cui-input').simulate('change', { target: { value: "Hello World Tom" } });
+    container.find('.cui-input').simulate('blur');
+
+    expect(container.find('span').text()).toEqual('Hello World Tom');
+    expect(container.find('span').length).toEqual(1);
+    expect(container.find('Input').length).toEqual(0);
+  });
+
 });

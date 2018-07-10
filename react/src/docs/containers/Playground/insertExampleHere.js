cc3c5e083ac8da86bf713bcab949355ab0cdeb51
Paul Jeter <pajeter@cisco.com>
2018-07-10 14:49:34 -0500

    chore(playground): cleanup playground

--- react/src/docs/containers/Playground/insertExampleHere.js
+++ react/src/docs/containers/Playground/insertExampleHere.js
@@ -3,32 +3,14 @@ import ErrorBoundary from '@collab-ui/react/ErrorBoundary';
 import ErrorContainer from '../ErrorContainer';
 
 // Import Method Show Below
-import { Input } from '@collab-ui/react';
+// import { Button } from '@collab-ui/react';
 
 export default class PlaygroundComponent extends React.Component {
-  state = {
-    value: ''
-  };
-
-  handleChange = (event) => {
-    this.setState({value: event.target.value});
-  }
-
   render() {
     return (
-      <div>
-        <Input
-          type="text"
-          htmlId="clearTest"
-          name="clearTest"
-          label="This is an input with clear Button"
-          inputSize="small-6"
-          value={this.state.value}
-          onChange={this.handleChange}
-          clear
-        />
-        <h1>Input value is: {this.state.value}</h1>
-      </div>
+      <ErrorBoundary fallbackComponent={<ErrorContainer />}>
+        <div />
+      </ErrorBoundary>
     );
   }
 }

c57d77a2885bace595f977f893baaf83ef677fc0
Ben Biggs <bebiggs@cisco.com>
2018-08-13 16:08:46 -0500

    chore(eslint): change eslint to js file

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -1,10 +1,12 @@
-{
+const path = require('path');
+
+module.exports = {
   "root": true,
   "settings": {
     "import/resolver": {
       "alias": {
         "map": [
-          ["@collab-ui/react", "./src/lib"]
+          ["@collab-ui/react", path.resolve(__dirname, 'src', 'lib')]
         ]
       },
       "node": {

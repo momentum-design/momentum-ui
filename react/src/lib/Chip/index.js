a87ad5631e0d255313471c7dfd4924ac95f93205
Paul Jeter <pajeter@cisco.com>
2018-08-31 13:15:15 -0500

    test(chip): update specs and snapshots

--- react/src/lib/Chip/index.js
+++ react/src/lib/Chip/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Icon} from '@collab-ui/react'
+import { Icon } from '@collab-ui/react';
 /**
  *
  * @category containers
@@ -9,7 +9,16 @@ import {Icon} from '@collab-ui/react'
  *
  */
 
-const Chip = ({className, leftContent, fileDownloadLink, fileType, rightContent, subTitle, title, type}) => {
+const Chip = ({
+  className,
+  leftContent,
+  fileDownloadLink,
+  fileType,
+  rightContent,
+  subTitle,
+  title,
+  type,
+}) => {
   Chip.displayName = 'Chip';
 
   // This appears to be a false positive
@@ -17,15 +26,15 @@ const Chip = ({className, leftContent, fileDownloadLink, fileType, rightContent,
   // eslint-disable-next-line react/no-multi-comp
   function buildChipLeft() {
     if (type === 'file') {
-      return <Icon name={`file-${fileType}_32`}/>;
+      return <Icon name={`file-${fileType}_32`} />;
     }
 
     if (type === 'recording') {
-      return <Icon name="play-circle_32" color="white"/>;
+      return <Icon name="play-circle_32" color="white" />;
     }
 
     if (type === 'unauthorized') {
-      return <Icon name="warning_32"/>;
+      return <Icon name="warning_32" />;
     }
 
     return leftContent;
@@ -38,12 +47,9 @@ const Chip = ({className, leftContent, fileDownloadLink, fileType, rightContent,
     }
 
     if (type === 'file' && fileDownloadLink) {
-      return(
-        <a
-          className="file-download"
-          download
-          href={fileDownloadLink}>
-            <i className="icon icon-download_32"/>
+      return (
+        <a className="file-download" download href={fileDownloadLink}>
+          <i className="icon icon-download_32" />
         </a>
       );
     }
@@ -55,46 +61,57 @@ const Chip = ({className, leftContent, fileDownloadLink, fileType, rightContent,
   const chipRight = buildChipRight();
 
   return (
-    <div className={'cui-chip' + `${className && ` ${className}` || ''}`}>
-      <div className = {'cui-chip-left' + `${type && ` ${type}` || ''}`}>
-       {chipLeft}
+    <div className={'cui-chip' + `${(className && ` ${className}`) || ''}`}>
+      <div className={'cui-chip-left' + `${(type && ` ${type}`) || ''}`}>
+        {chipLeft}
       </div>
       <div className="cui-chip-center">
-       {title && <div className="cui-chip__title lead">{title}</div> }
-       {subTitle && <div className="cui-chip__sub-title subheader">{subTitle}</div>}
+        {title && <div className="cui-chip__title lead">{title}</div>}
+        {subTitle && (
+          <div className="cui-chip__sub-title subheader">{subTitle}</div>
+        )}
       </div>
-      {chipRight &&
-        <div className="cui-chip-right">
-          {chipRight}
-        </div>
-      }
+      {chipRight && <div className="cui-chip-right">{chipRight}</div>}
     </div>
   );
 };
 
 Chip.propTypes = {
   className: PropTypes.string,
-  fileType: PropTypes.oneOf(['audio', 'graph', 'image', 'locked', 'missing', 'pdf', 'spreadsheet', 'text', 'video', 'zip']),
-  leftContent: PropTypes.node,
-  subTitle: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['file', 'recording', 'unauthorized']).isRequired,
   fileDownloadLink: PropTypes.string,
-  rightContent: PropTypes.node
+  fileType: PropTypes.oneOf([
+    'audio',
+    'graph',
+    'image',
+    'locked',
+    'missing',
+    'pdf',
+    'spreadsheet',
+    'text',
+    'video',
+    'zip',
+  ]),
+  leftContent: PropTypes.node,
+  rightContent: PropTypes.node,
+  subTitle: PropTypes.string,
+  title: PropTypes.string,
+  type: PropTypes.oneOf(['file', 'recording', 'unauthorized']),
 };
 
 Chip.defaultProps = {
   className: '',
+  fileDownloadLink: '',
   fileType: null,
   leftContent: null,
+  rightContent: null,
   subTitle: '',
-  fileDownloadLink: '',
-  rightContent: null
+  title: null,
+  type: null,
 };
 
- export default Chip;
+export default Chip;
 
- /**
+/**
  * @name Recording Chip
  *
  * @category containers
@@ -109,7 +126,7 @@ Chip.defaultProps = {
          <h3>
            <p><code className="small">size=(16)</code></p>
          </h3>
-         <Chip type="recording"/>
+         <Chip type="recording" title="Webex Teams Recording" subTitle="18 min"/>
        </div>
      </div>
    );
@@ -117,7 +134,7 @@ Chip.defaultProps = {
 
  **/
 
- /**
+/**
  * @name File Chip
  *
  * @category containers
@@ -132,7 +149,7 @@ Chip.defaultProps = {
          <h3>
            <p><code className="small">size=(16)</code></p>
          </h3>
-         <Chip type="file" fileType="audio" fileDownloadLink="https://www.google.com"/>
+         <Chip type="file" fileType="audio" title="Filename.mp3" subTitle="23kb"  fileDownloadLink="https://www.google.com"/>
        </div>
      </div>
    );
--- react/src/lib/Chip/index.spec.js
+++ react/src/lib/Chip/index.spec.js
@@ -4,35 +4,35 @@ import { Chip } from '@collab-ui/react';
 
 describe('<Chip />', () => {
   it('should match SnapShot', () => {
-    const container = shallow(<Chip type="recording" />);
+    const container = shallow(<Chip type="recording" title="Recording" />);
 
     expect(container).toMatchSnapshot();
   });
 
   it('should render the correct left content for a recording chip', () => {
-    const container = shallow(<Chip type="recording"/>);
+    const container = shallow(<Chip type="recording" title="Recording" />);
     expect(container.find('.cui-chip-left.recording').length).toEqual(1);
   });
 
   it('should render the correct left content for a file chip', () => {
-    const container = shallow(<Chip type="file" fileType="audio"/>);
+    const container = shallow(<Chip type="file" fileType="audio" title="File" />);
     expect(container.find('.cui-chip-left.file').length).toEqual(1);
   });
 
   it('should accept a custom class', () => {
-    const container = shallow(<Chip className="custom-recording-class"/>);
+    const container = shallow(<Chip className="custom-recording-class" type="recording" title="Recording" />);
     expect(container.find('.custom-recording-class').length).toEqual(1);
   });
 
   it('should render custom left content', () => {
     const leftContent = <div className="custom-left"/>;
-    const container = shallow(<Chip leftContent={leftContent}/>);
+    const container = shallow(<Chip leftContent={leftContent} />);
     expect(container.find('.custom-left').length).toEqual(1);
   });
 
   it('should render custom right content', () => {
     const rightContent = <div className="custom-right"/>;
-    const container = shallow(<Chip rightContent={rightContent}/>);
+    const container = shallow(<Chip rightContent={rightContent} />);
     expect(container.find('.custom-right').length).toEqual(1);
   });
 

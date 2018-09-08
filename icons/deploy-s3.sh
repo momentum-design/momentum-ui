f642d86e6007cbf32e8dce70d91a376e8112901a
Paul Jeter <pajeter@cisco.com>
2018-09-08 15:40:10 -0500

    build(circleci): add deploy

---
+++ icons/deploy-s3.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# Where webpack is going to stick the compiled files.
+BUILD_FOLDER=dist
+
+# S3 Configuration
+S3_FLAGS="--region $AWS_S3_REGION --acl public-read --delete"
+
+# Sync
+aws s3 sync $BUILD_FOLDER $AWS_S3_BUCKET $S3_FLAGS

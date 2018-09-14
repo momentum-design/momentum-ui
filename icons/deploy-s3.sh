e6255b01e215d900165601ce15388ee60002f9b4
Paul Jeter <pajeter@cisco.com>
2018-09-13 22:12:04 -0500

    build(deploy): add cache invalidation

--- icons/deploy-s3.sh
+++ icons/deploy-s3.sh
@@ -9,3 +9,6 @@ S3_FLAGS="--region $AWS_S3_REGION --acl public-read --delete"
 
 # Sync
 aws s3 sync $BUILD_FOLDER $AWS_S3_BUCKET $S3_FLAGS
+
+#Invalidate cache
+aws cloudfront create-invalidation --distribution-id $AWS_DISTRIBUTION_ID --paths "/*"

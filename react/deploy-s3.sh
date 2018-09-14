463c08b3b6d2691c299f69b33b6a0dc2dbea89e0
Paul Jeter <pajeter@cisco.com>
2018-09-13 22:14:55 -0500

    build(deploy): add cache invalidation
    - stagger cron jobs to install latest core and icons

--- react/deploy-s3.sh
+++ react/deploy-s3.sh
@@ -3,9 +3,13 @@ set -e
 
 # Where webpack is going to stick the compiled files.
 BUILD_FOLDER=dist
+AWS_S3_BUCKET=
 
 # S3 Configuration
 S3_FLAGS="--region $AWS_S3_REGION --acl public-read --delete"
 
 # Sync
 aws s3 sync $BUILD_FOLDER $AWS_S3_BUCKET $S3_FLAGS
+
+#Invalidate cache
+aws cloudfront create-invalidation --distribution-id $AWS_DISTRIBUTION_ID --paths "/*"

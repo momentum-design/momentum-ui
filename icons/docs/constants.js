2f8656057bcdc52955a043ab2aaa63b27ac6eed7
Paul Jeter <pajeter@cisco.com>
2018-09-18 23:18:42 -0500

    docs(downloads): update download path for icon files

--- icons/docs/constants.js
+++ icons/docs/constants.js
@@ -1,4 +1,4 @@
 export const FETCH_ICONS_SUCCESS = 'FETCH_ICONS_SUCCESS';
 export const FETCH_ICONS_PENDING = 'FETCH_ICONS_PENDING';
 export const FETCH_ICONS_ERROR = 'FETCH_ICONS_ERROR';
-export const DOWNLOAD_URL = 'http://collab-ui-icons.cisco.com/downloads/';
+export const DOWNLOAD_URL = 'http://downloads.collab-ui.com/icons/';

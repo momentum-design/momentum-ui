1eeccc594943e62c5c708812c394dda569e2e953
Thomas Bassetto <thobasse@cisco.com>
2019-03-18 20:59:28 +0100

    fix(icons): change tooling to build icons

---
+++ icons/tools/generators.js
@@ -0,0 +1,89 @@
+const handlebars = require("handlebars");
+const path = require("path");
+const sass = require("node-sass");
+const svgson = require("svgson");
+
+const { readFile, writeFile } = require('./icons-utils');
+
+function sassRender(file, outFile, outputStyle) {
+  const sassOptions = {
+    file,
+    outFile,
+    outputStyle
+  };
+
+  return new Promise((resolve, reject) => {
+    sass.render(sassOptions, (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+
+      resolve(result.css);
+    });
+  });
+}
+
+async function compileCss(options = {}) {
+  const cssFilePath = options.compressed
+    ? "css/collab-ui-icons.min.css"
+    : "css/collab-ui-icons.css";
+  const outputStyle = options.compressed ? "compressed" : "expanded";
+  const css = await sassRender(
+    "scss/collab-ui-icons.scss",
+    cssFilePath,
+    outputStyle
+  );
+  await writeFile(cssFilePath, css);
+  console.log(`${cssFilePath} written!`);
+}
+
+async function generateFile(dest, fileName, data) {
+  const template = path.join("templates", `${fileName}.hbs`);
+  const source = await readFile(template, "utf8");
+  const compile = handlebars.compile(source);
+  const finalFile = path.join(dest, fileName);
+  await writeFile(finalFile, compile(data));
+  console.warn(`${finalFile} written!`);
+}
+
+async function generateIconsDataJson(data) {
+  const glyphs = data.glyphsData.map(({ file, name }) => ({
+    name,
+    file
+  }));
+  // const icons = {};
+
+  const promises = glyphs.map(async function({ file, name }) {
+    const svgString = await readFile(path.resolve(file), "utf-8");
+    const ast = await svgson.parse(svgString);
+    const svgPath = ast.children.find(child => child.name === "path");
+    const svgG = ast.children.find(child => child.name === "g");
+    const ds = [];
+    if (svgPath) {
+      const { d } = svgPath.attributes;
+      ds.push(d);
+    } else if (svgG) {
+      const paths = svgG.children;
+      for (let path of paths) {
+        const { d } = path.attributes;
+        ds.push(d);
+      }
+    }
+    return { name, ds };
+  });
+
+  const iconsDs = await Promise.all(promises);
+  const icons = iconsDs.reduce((acc, curr) => {
+    acc[curr.name] = curr.ds;
+    if (curr.ds.length === 0) console.error(`⚠️ ${curr.name} has no paths!`);
+    return acc;
+  }, {});
+  await writeFile(path.join("data", "iconsData.json"), JSON.stringify(icons));
+  console.info("data/iconsData.json written!");
+}
+
+module.exports = {
+  compileCss,
+  generateFile,
+  generateIconsDataJson
+};
--- icons/tools/icons-build.js
+++ icons/tools/icons-build.js
@@ -1,17 +1,61 @@
-const generateFonts = require('./generateFonts');
-const generateCss = require('./generateCss');
-const generateSvgSprite = require('./generateSvgSprite');
-const processSvg = require('./processSvg');
+const { camelCase } = require("lodash");
+const handlebars = require("handlebars");
+const path = require("path");
+const { generateFonts } = require("@tbassetto/webfonts-generator");
 
-// generateCss();
-generateFonts()
-  .then(() => {
-    return generateCss();
-  })
-  .then(() => {
-    return generateSvgSprite();
-  })
-  .then(() => {
-    return processSvg();
-  });
+const {
+  compileCss,
+  generateFile,
+  generateIconsDataJson
+} = require("./generators");
 
+const FONT_NAME = "collab-ui-icons";
+const DEST = "fonts";
+const ICONS_SRC = "svg/*.svg";
+const DEFAULT_TEMPLATE_OPTIONS = {
+  baseSelector: ".icon",
+  classPrefix: "icon-",
+};
+
+(async function() {
+  try {
+    const result = await generateFonts(FONT_NAME, ICONS_SRC, DEST);
+
+    // Useful helpers for handlebars
+    handlebars.registerHelper("removeDashes", (selector, spacer) =>
+      selector.replace(/[_-]/g, spacer)
+    );
+    handlebars.registerHelper("equals", params => params[0] === params[1]);
+    handlebars.registerHelper("camelCase", str => camelCase(str));
+    handlebars.registerHelper("iosCharCode", str => "\\u{" + str + "}");
+
+    // Generate Scss files
+    const templateData = {
+      ...DEFAULT_TEMPLATE_OPTIONS,
+      ...result,
+      src: `url("#{$icon-font-path}/${path.parse(result.fontFiles.woff2).name}") format("woff2"),
+      url("#{$icon-font-path}/${path.parse(result.fontFiles.woff).name}") format("woff")`
+    };
+
+    // generate SCSS files
+    await generateFile("scss", "icons.scss", templateData);
+    await generateFile("scss", "mixins.scss", templateData);
+    await generateFile("scss", "path.scss", templateData);
+    await generateFile("scss", "placeholders.scss", templateData);
+    await generateFile("scss", "variables.scss", templateData);
+
+    // generate CSS files
+    await compileCss();
+    await compileCss({ compressed: true });
+
+    // generate HTML file
+    await generateFile("fonts", "index.html", templateData);
+
+    // generate Other files
+    await generateFile("data", "CollabIconType.swift", templateData);
+    await generateFile("data", "iconNames.json", templateData);
+    await generateIconsDataJson(templateData);
+  } catch (e) {
+    console.error("Something went wrong when generating the fonts", e);
+  }
+})();

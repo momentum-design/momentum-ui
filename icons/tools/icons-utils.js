383c472f5e7b0907e3fe97ec00d3297ccfd90622
Thomas Bassetto <thobasse@cisco.com>
2019-03-19 10:35:02 +0100

    fix(icons): fix 5 SVG files and add a querystring after the font files URLs

--- icons/tools/icons-utils.js
+++ icons/tools/icons-utils.js
@@ -1,10 +1,19 @@
+const crypto = require("crypto");
 const fs = require("fs");
 const util = require("util");
+
+async function calcHash(file) {
+  const hash = crypto.createHash("md5");
+  const fileContent = await readFile(file, "utf8");
+  hash.update(fileContent);
+  return hash.digest("hex");
+}
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 module.exports = {
+  calcHash,
   readFile,
   writeFile
 };

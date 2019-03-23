7dafc96de740dda74e14c45418fb44470a072557
Ben Biggs <bebiggs@cisco.com>
2019-03-22 18:41:12 -0700

    chore(package.json): fix package causing www to not build

--- www/client/examples/Avatar/index.js
+++ www/client/examples/Avatar/index.js
@@ -1,11 +1,11 @@
 export { default as Avatardefault } from './default';
 export { default as Avataractive } from './active';
-export { default as Avatarself } from './self';
+export { default as Avatarinactive } from './inactive';
 export { default as Avatarstatus } from './status';
+export { default as Avatarnotification } from './notification';
+export { default as Avatarself } from './self';
+export { default as Avatartyping } from './typing';
 export { default as Avatargroup } from './group';
 export { default as Avatarbot } from './bot';
-export { default as Avatartyping } from './typing';
 export { default as Avatarfailure } from './failure';
 export { default as Avatarcomposite } from './composite';
-export { default as Avatarinactive } from './inactive';
-export { default as Avatarnotification } from './notification';
--- www/client/examples/Button/index.js
+++ www/client/examples/Button/index.js
@@ -1,8 +1,8 @@
 export { default as Buttondefault } from './default';
+export { default as Buttoncolor } from './color';
+export { default as Buttontags } from './tags';
+export { default as Buttoncircle } from './circle';
+export { default as Buttonsize } from './size';
+export { default as Buttondisabled } from './disabled';
 export { default as Buttonexpanded } from './expanded';
 export { default as Buttonloading } from './loading';
-export { default as Buttoncolor } from './color';
-export { default as Buttoncircle } from './circle';
-export { default as Buttondisabled } from './disabled';
-export { default as Buttonsize } from './size';
-export { default as Buttontags } from './tags';

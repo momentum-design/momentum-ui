ef7eab4785fa57f0c9a2eb2a86c998be20771b52
Paul Jeter <pauljeter@gmail.com>
2019-04-01 08:59:40 -0500

    fix(core): convert sidenav to new sidebar in kitchen sink
    build(core): add step to build react library for core playground and kitchen sink consumption
    fix(sidebar): update overflow to auto to remove scrollbar

--- core/app/containers/KitchenSink/index.js
+++ core/app/containers/KitchenSink/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavLink, Route, Switch } from 'react-router-dom';
 import {
-  List,
-  ListItem,
-  ListItemSection,
-  SideNav,
+  Sidebar,
+  SidebarBody,
+  SidebarNav,
+  SidebarNavItem,
 } from '@collab-ui/react';
 import Example from '../Example'
 import components from '../../../data/data.json';
@@ -16,17 +16,16 @@ class KitchenSink extends React.Component {
 
   createNavLinks = components.map((item, idx) => {
     return (
-      <ListItem
+      <SidebarNavItem
         key={`${item.id}-${idx}`}
         customAnchorNode={
           <NavLink activeClassName='cui-active-nav' to={`/${item.name}`} />
         }
         className='cui-list-item--primary'
-      >
-        <h5 className='cui-font-color--alternate'>
-          {item.name}
-        </h5>
-      </ListItem>
+        key={`${item.id}-${idx}`}
+        keyboardKey={item.name}
+        title={item.name}
+      />
     )
   });
 
@@ -46,9 +45,16 @@ class KitchenSink extends React.Component {
     return (
       <React.Fragment>
         <div className="docs-container--with-side-nav">
-          <SideNav className="docs-container__side-nav">
-            <List>{this.createNavLinks}</List>
-          </SideNav>
+          <Sidebar
+            className="docs-container__side-nav"
+            expandable={false}
+            withIcons={false}
+            isPageLevel={true}
+          >
+            <SidebarBody>
+              <SidebarNav>{this.createNavLinks}</SidebarNav>
+            </SidebarBody>
+          </Sidebar>
           <div className="docs-container__content">
             <h1>Kitchen Sink</h1>
             <Switch>

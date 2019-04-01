e9e8ff042b8ac5a033f30d4018d5e3a39fa76ece
Paul Jeter <pauljeter@gmail.com>
2019-03-31 22:26:35 -0500

    fix(angular): fix dependency and lint errors

--- angular/src/lib/list/examples/list-examples.module.ts
+++ angular/src/lib/list/examples/list-examples.module.ts
@@ -1,12 +1,10 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ListItemModule, ListModule } from '@collab-ui/angular';
 import { CommonModule } from '@angular/common';
-import {
-  ListDefaultComponent,
-  ListTabTypeComponent,
-  ListTypeComponent,
-  ListWrapComponent,
-} from './index';
+import { ListDefaultComponent } from './list-default.component';
+import { ListTabTypeComponent } from './list-tab-type.component';
+import { ListTypeComponent } from './list-type.component';
+import { ListWrapComponent } from './list-wrap.component';
 
 @NgModule({
   declarations: [

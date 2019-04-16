c23bca57752b350b86d5215d2885c15b9fe36318
Zama Khan Mohammed <mohammedzamakhan@gmail.com>
2019-04-16 17:23:05 -0500

    feat(schamatics): generate nav

---
+++ angular/src/schematics/src/generate/nav/index.ts
@@ -0,0 +1,34 @@
+import {chain, noop, Rule, Tree} from '@angular-devkit/schematics';
+import {
+  addModuleImportToModule,
+  buildComponent,
+  findModuleFromOptions,
+} from '@angular/cdk/schematics';
+
+import {Schema} from './schema';
+
+/**
+ * Scaffolds a new navigation component.
+ */
+export default function(options: Schema): Rule {
+  return chain([
+    buildComponent({...options}, {
+      template: './__path__/__name@dasherize@if-flat__/__name@dasherize__.component.html',
+      stylesheet: './__path__/__name@dasherize@if-flat__/__name@dasherize__.component.__style__',
+    }),
+    options.skipImport ? noop() : addNavModulesToModule(options)
+  ]);
+}
+
+/**
+ * Adds the required modules to the relative module.
+ */
+function addNavModulesToModule(options: Schema) {
+  return (host: Tree) => {
+    const modulePath = findModuleFromOptions(host, options)!;
+    addModuleImportToModule(host, modulePath, 'TopBarModule', '@collab-ui/angular');
+    addModuleImportToModule(host, modulePath, 'ButtonModule', '@collab-ui/angular');
+    addModuleImportToModule(host, modulePath, 'ListItemModule', '@collab-ui/angular');
+    return host;
+  };
+}

899638eb12b7a4dcb209126fcf12dc4cac57d681
Paul Jeter <pauljeter@gmail.com>
2019-04-18 11:44:28 -0500

    fix(avatar): change fill to color for type self

--- react/src/app/containers/Playground/insertExampleHere.js
+++ react/src/app/containers/Playground/insertExampleHere.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 // Import Method Show Below
-import { Icon } from '@collab-ui/react';
+import { Button } from '@collab-ui/react';
 
 // Sample Class Method Show Below
 export default class PlaygroundComponent extends React.Component {
   render() {
     return (
-      <div>
-        <Icon name='people-active_36' color='$white-89' />
-
-      </div>
-
+      <Button>Playground Button</Button>
     );
   }
 }

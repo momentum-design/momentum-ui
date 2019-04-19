2bc840dcaabfa934a624b3246b7e0f7e20554d7e
Ben Biggs <bebiggs@cisco.com>
2019-04-19 14:41:41 -0500

    test(CollapseButton): fix previous and add vr test for collapse button

--- core/scss/components/collapse-button/tests/collapse-button.cy.js
+++ core/scss/components/collapse-button/tests/collapse-button.cy.js
@@ -3,6 +3,7 @@ describe('@collab-ui/core', function() {
     cy.visit(`${Cypress.env('BASE_URL')}/collapse-button`)
       .get('.cui-collapse-button')
       .should('be.visible')
+      .wait(1000)
       .percySnapshot();
   });
 });

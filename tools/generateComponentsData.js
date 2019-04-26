877ac1e0ad5bcc45b0ce955e9429f86c28becbaa
Paul Jeter <pauljeter@gmail.com>
2019-04-26 09:05:36 -0500

    fix(scss): update import paths for icon/fonts

--- tools/generateComponentsData.js
+++ tools/generateComponentsData.js
@@ -5,8 +5,8 @@ const baseDataJSON = require('./constants/BASE_DATA.json');
 const dataDirectory = path.resolve(__dirname, '../data/');
 const globArray = [
   path.resolve(__dirname, '../core/scss/**/*.html'),
-  path.resolve(__dirname, '../core/scss/**/*.scss'),
-  // path.resolve(__dirname, '../react/src/lib/**/*.js'),
+  // path.resolve(__dirname, '../core/scss/**/*.scss'),
+  path.resolve(__dirname, '../react/src/lib/**/*.js'),
   // path.resolve(__dirname, '../angular/src/lib/**/*'),
   // path.resolve(__dirname, '../angularjs/src/**/*'),
 ];

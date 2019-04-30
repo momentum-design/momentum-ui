e50d9c8bee9616ba68b4871525c67d7d33547f46
Ben Biggs <bebiggs@cisco.com>
2019-04-29 23:47:02 -0500

    test(coachmark): add content example

--- core/scss/components/coachmark/tests/coachmark.cy.js
+++ core/scss/components/coachmark/tests/coachmark.cy.js
@@ -1,7 +1,7 @@
 describe('@momentum-ui/core', function() {
   it('snapshot of coachmark', function() {
     cy.visit(`${Cypress.env('BASE_URL')}/coachmark`)
-      .get('.md-coachmark')
+      .get('.md-coachmark__container')
       .should('be.visible')
       .percySnapshot();
   });

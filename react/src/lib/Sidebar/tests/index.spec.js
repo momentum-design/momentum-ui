379cd43cabf4fba7cdf27251c6115790bac1dcfe
Ben Biggs <bebiggs@cisco.com>
2019-06-05 15:46:49 -0500

    fix(Sidebar): change expandable prop to collapsable'
    BREAKING CHANGE:
    
    Collapsable prop in Sidebar will be false by default

--- react/src/lib/Sidebar/tests/index.spec.js
+++ react/src/lib/Sidebar/tests/index.spec.js
@@ -41,20 +41,20 @@ describe('<Sidebar />', () => {
     expect(wrapper.find('.md-sidebar--dark').exists()).toBeTruthy();
   });
 
-  it('should minimize Sidebar if the expandable/!expanded/withIcons are set', () => {
-    const wrapper = mount(<Sidebar expandable expanded={false} />);
+  it('should minimize Sidebar if the collapsable/!expanded/withIcons are set', () => {
+    const wrapper = mount(<Sidebar collapsable expanded={false} />);
 
     expect(wrapper.find('.md-sidebar--minimized').exists()).toBeTruthy();
   });
 
-  it('should collapse Sidebar if the expandable/!expanded/!withIcons', () => {
-    const wrapper = mount(<Sidebar expandable expanded={false} withIcons={false} />);
+  it('should collapse Sidebar if the collapsable/!expanded/!withIcons', () => {
+    const wrapper = mount(<Sidebar collapsable expanded={false} withIcons={false} />);
 
     expect(wrapper.find('.md-sidebar--collapsed').exists()).toBeTruthy();
   });
 
-  it('should show Sidebar if the expandable and expanded are set', () => {
-    const wrapper = shallow(<Sidebar expandable expanded />);
+  it('should show Sidebar if the collapsable and expanded are set', () => {
+    const wrapper = shallow(<Sidebar collapsable expanded />);
 
     expect(wrapper.find('.md-sidebar--collapsed').exists()).toBeFalsy();
   });
@@ -132,32 +132,32 @@ describe('<Sidebar />', () => {
     expect(wrapper.find('.md-sidebar--nested').exists()).toBeTruthy();
   });
 
-  it('should add toggle button if withToggle/expandable are true', () => {
-    const wrapper = shallow(<Sidebar withToggle expandable/>);
+  it('should add toggle button if withToggle is true', () => {
+    const wrapper = shallow(<Sidebar withToggle />);
 
     expect(wrapper.find('.md-sidebar__toggle').exists()).toBeTruthy();
   });
 
-  it('should not add toggle button if withToggle/expandable are false', () => {
-    const wrapper = shallow(<Sidebar withToggle expandable={false}/>);
+  it('should not add toggle button if withToggle is false', () => {
+    const wrapper = shallow(<Sidebar />);
 
     expect(wrapper.find('.md-sidebar__toggle').exists()).toBeFalsy();
   });
 
-  it('should add minimized toggle button if withIcons/withToggle/expandable/!expanded set', () => {
-    const wrapper = mount(<Sidebar withIcons withToggle expandable expanded={false}/>);
+  it('should add minimized toggle button if withIcons/withToggle/!expanded set', () => {
+    const wrapper = mount(<Sidebar withIcons withToggle expanded={false}/>);
 
     expect(wrapper.find('.md-sidebar__toggle--minimized').exists()).toBeTruthy();
   });
 
-  it('should add collapsed toggle button if !withIcons/withToggle/expandable/!expanded set', () => {
-    const wrapper = mount(<Sidebar withIcons={false} withToggle expandable expanded={false}/>);
+  it('should add collapsed toggle button if !withIcons/withToggle/!expanded set', () => {
+    const wrapper = mount(<Sidebar withIcons={false} withToggle expanded={false}/>);
 
     expect(wrapper.find('.md-sidebar__toggle--collapsed').exists()).toBeTruthy();
   });
 
   it('should apply buttonProps to toggle button', () => {
-    const wrapper = mount(<Sidebar withToggle expandable buttonProps={{ ariaLabel: 'test' }}/>);
+    const wrapper = mount(<Sidebar withToggle buttonProps={{ ariaLabel: 'test' }}/>);
 
     expect(wrapper.find('.md-button').props()['aria-label']).toEqual('test');
   });

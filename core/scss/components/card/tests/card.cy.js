98aa58bc98518330a1f64ad59eaf55f3c784a4b0
Ben Biggs <bebiggs@cisco.com>
2019-06-07 15:45:08 -0500

    fix(card): add v2 class suffix until ng conflict resolution

--- core/scss/components/card/tests/card.cy.js
+++ core/scss/components/card/tests/card.cy.js
@@ -1,7 +1,7 @@
 describe('@momentum-ui/core', function() {
   it('snapshot of card', function() {
     cy.visit(`${Cypress.env('BASE_URL')}/card`)
-      .get('.md-card')
+      .get('.md-cardv2')
       .should('be.visible')
       .percySnapshot();
   });

9ad419dcac9e2adbfbe508405e404b186aac924f
Ben Biggs <bebiggs@cisco.com>
2019-06-10 11:33:09 -0500

    fix(Label): add children as a prop, make label optional

--- react/src/lib/Label/index.js
+++ react/src/lib/Label/index.js
@@ -3,33 +3,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Label = ({ className, htmlFor, label, theme, ...props }) => {
+const Label = ({
+  children,
+  className,
+  htmlFor,
+  label,
+  theme,
+  ...props
+}) => {
   return (
     <label
       className={
-        `${className ? ` ${className}` : ''}`+
+        `${className ? `${className}` : ''}` +
         `${theme ? ` md-label--${theme}` : ''}`
       }
       htmlFor={htmlFor}
       {...props}
     >
-      <span>{label}</span>
+      {
+        label 
+        ? <span>{label}</span>
+        : children
+      }
     </label>
   );
 };
 
 Label.propTypes = {
+  /** @prop Children nodes to render inside the Label | null */
+  children: PropTypes.node,
   /** @prop HTML class name for associated Input | '' */
   className: PropTypes.string,
   /** @prop HTML ID for associated Input | null */
   htmlFor: PropTypes.string.isRequired,
   /** @prop Required Label text | null */
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
   /** @prop Set Label's color theme | '' */
   theme: PropTypes.string,
 };
 
 Label.defaultProps = {
+  children: null,
   className: '',
   htmlFor: null,
   label: null,

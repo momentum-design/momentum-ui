dfc452f94e498612f7830613b2aab4a7492dfd4b
mathSusan <sbmck@yahoo.com>
2019-06-20 12:27:05 -0500

    fix(angular): lightbox - generate metadata for supporting aot builds

--- angular/src/lib/lightbox/lightbox.module.ts
+++ angular/src/lib/lightbox/lightbox.module.ts
@@ -2,9 +2,9 @@ import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
 import { CommonModule } from '@angular/common';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, ModuleWithProviders } from '@angular/core';
-import { IconModule } from '../icon';
-import { SpinnerModule } from '../spinner';
-import { TooltipModule } from '../tooltip';
+import { IconModule } from '../icon/icon.module';
+import { SpinnerModule } from '../spinner/spinner.module';
+import { TooltipModule } from '../tooltip/tooltip.module';
 import { LightboxComponent } from './lightbox.component';
 import { LightboxService } from './lightbox.service';
 

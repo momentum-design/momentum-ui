eeee6cd2dc857bbf3ba224bd35faff9a3076f1bd
mathSusan <sbmck@yahoo.com>
2019-06-20 15:57:30 -0500

    feat(card): angular and core add states and click to cards

--- angular/src/lib/card/examples/card-examples.module.ts
+++ angular/src/lib/card/examples/card-examples.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardDefaultComponent } from './card-default.component';
+import { CardClickableComponent } from './card-clickable.component';
 import { CardContentComponent } from './card-content.component';
 import { CardContent2Component } from './card-content2.component';
 import { CardModule } from '../card.module';
@@ -9,7 +10,8 @@ import { AvatarModule, IconModule, ButtonModule } from '@momentum-ui/angular';
   declarations: [
     CardDefaultComponent,
     CardContentComponent,
-    CardContent2Component
+    CardContent2Component,
+    CardClickableComponent
   ],
   imports: [
     CommonModule,
@@ -21,7 +23,8 @@ import { AvatarModule, IconModule, ButtonModule } from '@momentum-ui/angular';
   exports: [
     CardDefaultComponent,
     CardContentComponent,
-    CardContent2Component
+    CardContent2Component,
+    CardClickableComponent
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })

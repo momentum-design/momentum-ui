77b68e673b9a37cfd44447285720561f4390ed9e
mathSusan <sbmck@yahoo.com>
2019-06-20 15:57:30 -0500

    feat(card): angular and core add states and click to cards

--- angular/src/lib/card/card.component.ts
+++ angular/src/lib/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output,  ElementRef } from '@angular/core';
+import {SPACE, ENTER} from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'md-card',
@@ -8,9 +9,42 @@ import { Component, ElementRef, Input } from '@angular/core';
   styles: [],
   host: {
     'class': 'md-card',
+    '[class.md-card--clickable]': 'clickable',
+    '[attr.tabindex]': 'clickable ? "0" : null',
+    '(click)': 'clickable ? onClick() : null',
+    '(keydown)': 'clickable ? onKeydown($event) : null'
   }
 })
 
 export class CardComponent {
+  /** catch this event to know the card was clicked on */
+  @Output() cardClicked: EventEmitter<Boolean> = new EventEmitter();
+  /** @prop clickable whether or not the card is clickable | false */
+  @Input() clickable: Boolean = false;
+
+  onClick(): void {
+    if (this.clickable) {
+      this.el.nativeElement.classList.add('active');
+      this.cardClicked.emit();
+    }
+  }
+
+  onKeydown(event: KeyboardEvent): void {
+// tslint:disable-next-line: deprecation
+    const key = event.key || event.which || event.keyCode;
+    switch (key) {
+      case SPACE:
+      case ENTER:
+      case ' ':
+      case 'Enter':
+        this.onClick();
+        break;
+      default:
+        break;
+    }
+
+  }
+
+  constructor(private el: ElementRef) {}
 
 }
--- angular/src/lib/card/tests/card.component.spec.ts
+++ angular/src/lib/card/tests/card.component.spec.ts
@@ -1,33 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CardModule } from '../card.module';
 import { Component } from '@angular/core';
+import { CardComponent } from '../card.component';
+import { By } from '@angular/platform-browser';
+import { dispatchKeyboardEvent } from '../../../../test/utils';
+import { SPACE, ENTER } from '@angular/cdk/keycodes';
 
-
+@Component({
+  selector: 'test-card',
+  template: `<md-card class="small-3 columns" clickable="true" (cardClicked)="onClick(event);">
+  <md-card-section>
+      <div>
+        <h4>Title</h4>
+        <h6>Subtitle</h6>
+    </div>
+  </md-card-section>
+  <md-card-section [full]='true'>
+    <img src="https://place-hold.it/400x400">
+  </md-card-section>
+  <md-card-section>
+    Content
+  </md-card-section>
+  <md-card-section>FOOTER</md-card-section>
+</md-card>`
+})
+class TestCardComponent {
+  onClick(event: Event) {}
+}
 
 describe('Card Test', () => {
   let component: TestCardComponent;
   let fixture: ComponentFixture<TestCardComponent>;
+  let cardElement: HTMLElement;
 
-  @Component({
-    selector: 'test-card',
-    template: `<md-card class="small-3 columns">
-    <md-card-section>
-       <div>
-          <h4>Title</h4>
-          <h6>Subtitle</h6>
-      </div>
-    </md-card-section>
-    <md-card-section [full]='true'>
-      <img  src="http://placekitten.com/600/300" >
-    </md-card-section>
-    <md-card-section>
-      Content
-    </md-card-section>
-    <md-card-section>FOOTER</md-card-section>
-  </md-card>`
-  })
-  class TestCardComponent {}
+
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -40,14 +47,29 @@ describe('Card Test', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TestCardComponent);
     component = fixture.componentInstance;
+    cardElement = fixture.debugElement.nativeElement.querySelector('md-card');
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    const element = fixture.nativeElement;
-    const cardElement =  element.querySelector('md-card');
+  it('should create and match snapshot', () => {
     expect(cardElement).toBeTruthy();
     expect(cardElement).toMatchSnapshot();
+  });
+
+  it('should call the function when clicked', () => {
+    spyOn(fixture.componentInstance, 'onClick');
+    fixture.detectChanges();
+    const cardTestComponent = fixture.debugElement.query(By.directive(CardComponent));
+    cardTestComponent.triggerEventHandler('click', null);
+    expect(fixture.componentInstance.onClick).toHaveBeenCalled();
+  });
+
+  it('should call the function when enter is pressed', () => {
+    spyOn(fixture.componentInstance, 'onClick');
+    fixture.detectChanges();
+    const cardTestComponent = fixture.debugElement.query(By.directive(CardComponent));
+    cardTestComponent.triggerEventHandler( 'keydown', {key: 'Enter'});
+    expect(fixture.componentInstance.onClick).toHaveBeenCalled();
   });
 
 

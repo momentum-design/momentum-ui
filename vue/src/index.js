59c292e7a7b87235b8e0afffff7a29ea66aa1d9a
Lin Wang <wl@wzpo.net>
2019-07-17 15:34:56 +0800

    feat(Vue Icon): add Vue Icon component

--- vue/src/index.js
+++ vue/src/index.js
@@ -1,10 +1,12 @@
 import Badge from './lib/badge/index.vue';
 import Button from './lib/button/index.vue';
+import Icon from './lib/icon/index.vue';
 import Loading from './lib/loading/index.vue';
 
 const components = [
   Badge,
   Button,
+  Icon,
   Loading
 ];
 
@@ -22,5 +24,6 @@ export default {
   install,
   Badge,
   Button,
+  Icon,
   Loading
 };
---
+++ vue/src/lib/icon/examples/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <md-icon :name="'accessories_16'" />
+    <md-icon :name="'accessories_20'" />
+    <md-icon :name="'accessories_36'" />
+    <md-icon :name="'accessories_56'" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExampleIconDefault',
+};
+</script>
+

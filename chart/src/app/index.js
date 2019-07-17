f814d7a7008ca292cb21d236cb73f1d0652934ac
Arthus <531151995@qq.com>
2019-07-17 15:22:14 +0800

    feat(chart): create chart folder
    add test
    
    add lib fix ignore
    
    update init
    
    add
    
    add tools/lib line symbol
    
    add pie animation
    
    add text scale zoom axis link
    
    ignore
    
    update config
    
    d
    
    c
    
    add
    
    enable yarn lock
    
    update

---
+++ chart/src/app/index.js
@@ -0,0 +1,7 @@
+import example from '../lib/link/example/index.js';
+// import example from '../lib/rect/example/index.js';
+// import './styles.scss';
+
+window.onload = function(){
+  example();
+};
---
+++ chart/src/lib/area/index.js
@@ -0,0 +1,33 @@
+import * as d3 from "d3";
+import Shape from '../shape/index';
+
+class Area extends Shape {
+
+  constructor(data, config) {
+    super();
+    super.init(data, config);
+    this.setGenerator(d3.area());
+  }
+
+  renderSelection(selection) {
+    this.Path = this.Generator(this.Data);
+    this.Stack.push({
+      path: this.Path
+    });
+    selection.attr('d', this.Path);
+    return selection;
+  }
+}
+
+Area.prototype.DomName = 'path';
+Area.prototype.defaultConfig = {
+  generator: {
+
+  },
+  modify: {
+
+  }
+};
+
+export default Area;
+

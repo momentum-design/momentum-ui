963129b1e54e2b331655cb2947af7d537e94bcb6
Ben Biggs <bfletcherbiggs@gmail.com>
2019-08-07 15:27:17 -0500

    chore(input): update react/core examples

--- react/src/lib/Input/examples/Clear.js
+++ react/src/lib/Input/examples/Clear.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import { Input } from '@momentum-ui/react';
-export default class InputClear extends React.PureComponent {
-  state = {
-    value: 'Press or click the clear icon to clear this input'
-  };
-  handleChange = (event) => {
-    this.setState({value: event.target.value});
-  }
-  render() {
-    return (
-      <Input
-        name='clearInput'
-        label='Input with clear'
-        htmlId='clearInput'
-        inputSize='small-5'
-        placeholder='Placeholder Text'
-        value={this.state.value}
-        onChange={this.handleChange}
-        clear
-      />
-    );
-  }
+
+export default function InputClear() {
+  return (
+    <Input
+      name='clearInput'
+      label='Input with clear'
+      htmlId='clearInput'
+      inputSize='medium-6'
+      placeholder='Placeholder Text'
+      value='Press or click the clear icon to clear this input'
+      clear
+    />
+  );
 }
--- react/src/lib/Input/examples/Success.js
+++ react/src/lib/Input/examples/Success.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Input } from '@momentum-ui/react';
+
 export default function InputSuccess() {
   return (
     <Input
-      name="inputSuccess"
-      label="Error (Success) Input"
-      htmlId="inputSuccess"
-      inputSize="small-5"
+      name='inputSuccess'
+      label='Success Input'
+      htmlId='inputSuccess'
+      inputSize='medium-6'
       messageArr={[
         {
           message: 'This is where the success message would be.',
           type: 'success',
         },
       ]}
+      value='Success Text'
     />
   );
 }

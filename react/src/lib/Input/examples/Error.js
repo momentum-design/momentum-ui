3bf8e9a7e09a52fbdfd69f8f4ae1552112b2af2d
Ben Biggs <bfletcherbiggs@gmail.com>
2019-08-07 15:27:17 -0500

    chore(input): update react/core examples

--- react/src/lib/Input/examples/Error.js
+++ react/src/lib/Input/examples/Error.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Input } from '@momentum-ui/react';
+
 export default function InputError() {
   return (
     <Input
-      name="inputError"
-      label="Error (Error) Input"
-      htmlId="inputError"
-      inputSize="small-5"
+      name='inputError'
+      label='Error Input'
+      htmlId='inputError'
+      inputSize='medium-6'
       messageArr={[
         { message: 'This is where the error message would be.', type: 'error' },
       ]}
+      value='Error Text'
     />
   );
 }

20df1ba9760a3adaf808fc29ec7e30fe55fbedb2
Ben Biggs <bfletcherbiggs@gmail.com>
2019-08-07 15:27:17 -0500

    chore(input): update react/core examples

---
+++ react/src/lib/Input/examples/Nested.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Input } from '@momentum-ui/react';
+
+export default function InputNested() {
+  return (
+    <Input
+      name='inputNested'
+      label='Nested 1'
+      htmlId='inputNested'
+      inputSize='medium-6'
+      nestedLevel={1}
+    />
+  );
+}

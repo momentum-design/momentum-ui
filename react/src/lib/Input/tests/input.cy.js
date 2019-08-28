a84f5e313f978ad7649b1ea5eb8900d2508a7fea
Paul Jeter <pauljeter@gmail.com>
2019-08-28 12:06:07 -0500

    chore(angular): add kitchen sink to app

--- react/src/lib/Input/tests/input.cy.js
+++ react/src/lib/Input/tests/input.cy.js
@@ -1,6 +1,6 @@
 describe('@momentum-ui/react', function() {
   it('snapshot of input', function() {
-    cy.visit(`${Cypress.env('BASE_URL')}/kitchen-sink`)
+    cy.visit(`${Cypress.env('BASE_URL')}/input`)
       .get('.md-input')
       .should('be.visible')
       .percySnapshot();

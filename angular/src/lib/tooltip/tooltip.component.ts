fdc26d18ef135a79d6c159eb447b40b61dd71e44
mathSusan <sbmck@yahoo.com>
2019-09-12 14:27:40 -0500

    fix(angular): add max-width so that tooltips wrap at some point
    Closes #359

--- angular/src/lib/tooltip/tooltip.component.ts
+++ angular/src/lib/tooltip/tooltip.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input, TemplateRef, OnInit, ViewContainerRef} from '@angular/core';
-
+import {
+  Component,
+  Input,
+  TemplateRef,
+  OnInit,
+  ViewContainerRef,
+  HostBinding
+} from '@angular/core';
 
 @Component({
   selector: 'md-tooltip',
   styles: [],
   template: `
-  <div id="{{id}}">
+  <div id="{{id}}" [style.max-width.px]="maxWidth">
     <span *ngIf="text != ''" class="md-tooltip__text" role="tooltip">{{ text }}</span>
     <ng-content></ng-content>
   </div>
@@ -16,6 +22,10 @@ export class TooltipComponent implements OnInit {
   @Input() text: string = '';
   @Input() tooltipTemplate: TemplateRef<any>;
   @Input() id: string;
+  @Input() maxWidth: number;
+
+  @HostBinding('style.max-width.px')
+      maximumWidth = this.maxWidth;
 
   constructor( public _vcr: ViewContainerRef ) {}
 

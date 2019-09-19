d178cb8eebe06da0d5bbb0ee556ad84cd7eeabb0
Daanish Nasir <daanishnasir@gmail.com>
2019-09-19 11:16:03 -0500

    feat(data-table): update checkbox style in data table

--- angular/src/docs/app/playground/playground.component.ts
+++ angular/src/docs/app/playground/playground.component.ts
@@ -8,6 +8,9 @@ import { Component } from '@angular/core';
         <h1>Playground</h1>
         <div class="docs-container">
           <!-- Insert Example Here -->
+        <example-data-table-multi-select></example-data-table-multi-select>
+
+        <example-select-multi></example-select-multi>
         </div>
       </article>
     </div>

efb0bc3e84ca06bc9174af7b3960b0e82e2a78b2
Jeff Personal <jeffbirori@gmail.com>
2019-09-20 11:47:35 -0700

    feat(Card): implement card and card section components

---
+++ react/src/lib/CardSection/tests/index.spec.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { CardSection } from '@momentum-ui/react';
+
+describe('tests for <CardSection />', () => {
+  it('should match SnapShot', () => {
+    const container = shallow(<CardSection id="test" />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render one CardSection', () => {
+    const container = shallow(<CardSection />);
+
+    expect(container.find('.md-card-section').length).toEqual(1);
+  });
+
+  it('should render one full CardSection', () => {
+    const container = shallow(<CardSection full/>);
+
+    expect(container.find('.md-card-section--full').length).toEqual(1);
+  });
+
+  it('should render a CardSection with the given classes', () => {
+    const container = shallow(<CardSection className="testing-class"/>);
+
+    expect(container.find('.md-card-section').hasClass('testing-class')).toEqual(true);
+  });
+
+  it('should render children', () => {
+    const container = shallow(
+      <CardSection>
+        <div className="testingforMB" />
+      </CardSection>
+    );
+
+    expect(container.find('.testingforMB').length).toEqual(1);
+  });
+});

9e3477ac1aefc512a958a8431bc8c1f73cd793a1
Brian Spence <brspence@cisco.com>
2019-10-21 10:18:28 -0500

    fix(charts): reference customElements on window
    - fixes ReferenceError seen in test environment

--- charts/src/lib/webComponents.js
+++ charts/src/lib/webComponents.js
@@ -20,6 +20,7 @@ const registorWebComponent = () => {
   if ((process && process.env && process.env.NODE_ENV === 'test')) {
     // currently do not support webcomponent test
   } else {
+    const customElements = window.customElements;
     if (customElements && customElements.define) {
       components.map((c) => {
         customElements.define(c.prototype.RegistorName, c);

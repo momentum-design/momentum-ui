66b7d5ee43147cf701418e03a17b7860cab7139a
Paul Jeter <pauljeter@gmail.com>
2019-11-03 21:14:11 -0600

    chore(CDN): add scripts to push files to S3

---
+++ angularjs/tools/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  CDN_OBJECTS: [
+    'bundles',
+    'dist',
+    'types',
+    'CHANGELOG.md',
+    'LICENSE',
+    'README.md',
+    'package.json',
+  ],
+};
---
+++ core/tools/replaceURLs.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const argv = require('yargs').argv;
+const { library } = argv;
+
+const packageJSON = require(path.resolve(__dirname, `../package.json`));
+const iconsPackageJSON = require(path.resolve(__dirname, `../../icons/package.json`));
+const cssPath = `../css/momentum-ui`;
+const version = packageJSON.version;
+const iconsVersion = iconsPackageJSON.version;
+const cssFile = path.resolve(__dirname, `${cssPath}.css`);
+const cssMinFile = path.resolve(__dirname, `${cssPath}.min.css`);
+
+console.log(library);
+
+fs.readFile(cssFile, 'utf8', function(err, data) {
+  if (err) return console.error(err);
+
+  const result = data.replace(
+    /\.\.\/\.\.\/icons\/fonts/g,
+    `https://downloads.momentum-ui.com/@momentum-ui/icons/${iconsVersion}/fonts`
+  ).replace(
+    /\.\.\/fonts/g,
+    `https://downloads.momentum-ui.com/@momentum-ui/core/${version}/fonts`
+  );
+
+  fs.writeFile(cssFile, result, 'utf8', function(err) {
+    if (err) return console.log(err);
+    console.log(`${cssFile} updated!`);
+  });
+});
+
+fs.readFile(cssMinFile, 'utf8', function(err, data) {
+  if (err) return console.error(err);
+  const result = data.replace(
+    /\.\.\/.\.\/icons\/fonts/g,
+    `https://downloads.momentum-ui.com/@momentum-ui/icons/${iconsVersion}/fonts`
+  ).replace(
+    /.\.\/fonts/g,
+    `https://downloads.momentum-ui.com/@momentum-ui/core/${version}/fonts`
+  );
+
+  fs.writeFile(cssMinFile, result, 'utf8', function(err) {
+    if (err) return console.log(err);
+    console.log(`${cssMinFile} updated!`);
+  });
+});

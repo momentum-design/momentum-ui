c025996d99460113541c2cbe8d3fd5cb101689b2
Ben Biggs <bfletcherbiggs@gmail.com>
2020-01-22 15:31:06 -0600

    feat(TopbarNav): add alignment prop

--- react/src/lib/TopbarNav/index.js
+++ react/src/lib/TopbarNav/index.js
@@ -8,8 +8,10 @@ import { prefix } from '../utils/index';
 class TopbarNav extends React.Component {
   render() {
     const {
+      alignment,
       children,
       className,
+      listProps,
       ...otherProps
     } = this.props;
 
@@ -17,26 +19,33 @@ class TopbarNav extends React.Component {
       <nav
         className={
           `${prefix}-top-bar__nav` +
+          `${alignment ? ` ${prefix}-top-bar__nav--${alignment}` : ''}` +
           `${className && ` ${className}` || ''}`
         }
         {...otherProps}
       >
-        <List tabType='horizontal'>{children}</List>
+        <List tabType='horizontal' {...listProps}>{children}</List>
       </nav>
     );
   }
 }
 
 TopbarNav.propTypes = {
+  /** @prop Optional flex justify content alignment | '' */
+  alignment: PropTypes.string,
   /** @prop Children node to render inside of TopbarNav | null */
   children: PropTypes.node,
   /** @prop Optional CSS class string | '' */
   className: PropTypes.string,
+  /** @prop Optional object for List Component props | {} */
+  listProps: PropTypes.object,
 };
 
 TopbarNav.defaultProps = {
+  alignment: null,
   children: null,
   className: '',
+  listProps: {},
 };
 
 TopbarNav.displayName = 'TopbarNav';

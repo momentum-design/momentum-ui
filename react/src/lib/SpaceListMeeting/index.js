a0259d7007b498d955e16367e73ea91b7ceeb112
Ben Biggs <bfletcherbiggs@gmail.com>
2020-02-27 13:32:02 -0600

    feat(SpaceListMeeting): add eventOverlayProps prop

--- react/src/lib/SpaceListMeeting/index.js
+++ react/src/lib/SpaceListMeeting/index.js
@@ -2,23 +2,14 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Avatar,
-  Icon,
-  List,
-  ListItem,
-  ListItemSection,
-  Button,
-  Popover,
-} from '@momentum-ui/react';
+import { Avatar, Icon, List, ListItem, ListItemSection, Button, Popover } from '@momentum-ui/react';
 import omit from 'lodash/omit';
 import uniqueId from 'lodash/uniqueId';
 
 class SpaceListMeeting extends React.PureComponent {
-
   state = {
     id: this.props.id || uniqueId('md-space-list-meeting-'),
-  }
+  };
 
   handleButtonClick = e => {
     const { buttonOnClick } = this.props;
@@ -26,7 +17,7 @@ class SpaceListMeeting extends React.PureComponent {
     buttonOnClick && buttonOnClick(e);
 
     e.stopPropagation();
-  }
+  };
 
   render() {
     const {
@@ -35,6 +26,7 @@ class SpaceListMeeting extends React.PureComponent {
       childrenLeft,
       childrenRight,
       className,
+      eventOverlayProps,
       header,
       isMessagingOnlyShare,
       isBold,
@@ -43,102 +35,93 @@ class SpaceListMeeting extends React.PureComponent {
       title,
       ...props
     } = this.props;
-    const {
-      id
-    } = this.state;
+    const { id } = this.state;
 
-    const otherProps = omit({...props}, ['buttonOnClick']);
+    const otherProps = omit({ ...props }, ['buttonOnClick']);
 
-    const getTitle =
-      (!title && typeof header === 'string')
-        ? header
-        : title;
+    const getTitle = !title && typeof header === 'string' ? header : title;
 
     const getLeftSection = () => {
-      switch(meetingType) {
-        case 'group': return <Avatar className='md-list-item__avatar' title={getTitle} alt={getTitle} type='group'/>;
-        case 'number': return <Avatar className='md-list-item__avatar' title='#' alt={getTitle}/>;
-        case 'device': return <Avatar className='md-list-item__avatar' icon={<Icon name='spark-board_16' />} alt={getTitle} />;
-        default: return <Avatar className='md-list-item__avatar' title={getTitle} alt={getTitle}/> ;
+      switch (meetingType) {
+        case 'group':
+          return (
+            <Avatar className="md-list-item__avatar" title={getTitle} alt={getTitle} type="group" />
+          );
+        case 'number':
+          return <Avatar className="md-list-item__avatar" title="#" alt={getTitle} />;
+        case 'device':
+          return (
+            <Avatar
+              className="md-list-item__avatar"
+              icon={<Icon name="spark-board_16" />}
+              alt={getTitle}
+            />
+          );
+        default:
+          return <Avatar className="md-list-item__avatar" title={getTitle} alt={getTitle} />;
       }
     };
 
     const getPopoverContent = (
       <List>
-        {
-          attendees.map((ele, idx) => (
-            <ListItem key={`attendee-${idx}`}>
-              <ListItemSection position='left'>
-                {
-                  ele.node
-                    ? ele.node
-                    : (
-                      <Avatar
-                        size='small'
-                        title={ele.title}
-                        alt={ele.alt || ele.title}
-                        src={ele.src || ''}
-                        type='group'
-                      />
-                    )
-                }
-              </ListItemSection>
-              <ListItemSection position='center'>
-                <div className='md-list-item__header'>
-                  {ele.title}
-                </div>
-              </ListItemSection>
-            </ListItem>
-          ))
-        }
+        {attendees.map((ele, idx) => (
+          <ListItem key={`attendee-${idx}`}>
+            <ListItemSection position="left">
+              {ele.node ? (
+                ele.node
+              ) : (
+                <Avatar
+                  size="small"
+                  title={ele.title}
+                  alt={ele.alt || ele.title}
+                  src={ele.src || ''}
+                  type="group"
+                />
+              )}
+            </ListItemSection>
+            <ListItemSection position="center">
+              <div className="md-list-item__header">{ele.title}</div>
+            </ListItemSection>
+          </ListItem>
+        ))}
       </List>
     );
 
-    const children = (
-      [
-        <ListItemSection key='child-0' position='left'>
-          {childrenLeft || getLeftSection()}
-        </ListItemSection>,
-        <ListItemSection key='child-1' position='center'>
-          <div className='md-list-item__header'>
-            {header}
-          </div>
-          <div className='md-list-item__subheader'>
-            {subheader}
-          </div>
-        </ListItemSection>,
-        <ListItemSection key='child-2' position='right'>
-          {childrenRight || attendees.length > 0 &&
+    const children = [
+      <ListItemSection key="child-0" position="left">
+        {childrenLeft || getLeftSection()}
+      </ListItemSection>,
+      <ListItemSection key="child-1" position="center">
+        <div className="md-list-item__header">{header}</div>
+        <div className="md-list-item__subheader">{subheader}</div>
+      </ListItemSection>,
+      <ListItemSection key="child-2" position="right">
+        {childrenRight ||
+          (attendees.length > 0 && (
             <Popover
               content={getPopoverContent}
-              direction='bottom-center'
-              targetOffset={{vertical: 3}}
+              direction="bottom-center"
+              targetOffset={{ vertical: 3 }}
               isDynamic
+              {...eventOverlayProps}
             >
-              <span
-                role='button'
-                tabIndex={0}
-                className='md-list-item--space-meeting--attendees'
-              >
+              <span role="button" tabIndex={0} className="md-list-item--space-meeting--attendees">
                 {isMessagingOnlyShare ? null : attendees.length}
-                {isMessagingOnlyShare ? <Icon name='share-screen_12' /> : <Icon name='people_12' />}
+                {isMessagingOnlyShare ? <Icon name="share-screen_12" /> : <Icon name="people_12" />}
               </span>
             </Popover>
-          }
-          {
-            buttonLabel
-            &&
-            <Button
-              color={isMessagingOnlyShare ? 'blue' : 'green'}
-              ariaLabel={buttonLabel}
-              children={buttonLabel}
-              onClick={this.handleButtonClick}
-              size={28}
-            />
-          }
-        </ListItemSection>
-      ]
-    );
+          ))}
+        {buttonLabel && (
+          <Button
+            color={isMessagingOnlyShare ? 'blue' : 'green'}
+            ariaLabel={buttonLabel}
+            children={buttonLabel}
+            onClick={this.handleButtonClick}
+            size={28}
+          />
+        )}
+      </ListItemSection>,
+    ];
 
     return (
       <ListItem
@@ -149,7 +132,7 @@ class SpaceListMeeting extends React.PureComponent {
         }
         id={id}
         title={getTitle}
-        type='space'
+        type="space"
         {...otherProps}
       >
         {children}
@@ -165,7 +148,7 @@ SpaceListMeeting.propTypes = {
       title: PropTypes.string.isRequired,
       alt: PropTypes.string,
       src: PropTypes.string,
-      node: PropTypes.element
+      node: PropTypes.element,
     })
   ),
   /** @prop Label string for button | '' */
@@ -178,6 +161,8 @@ SpaceListMeeting.propTypes = {
   childrenRight: PropTypes.node,
   /** @prop Optional HTML Class string | '' */
   className: PropTypes.string,
+  /** @prop Event Overlay props to be overwritten | null */
+  eventOverlayProps: PropTypes.shape({}),
   /** @prop ListItem header text | '' */
   header: PropTypes.node.isRequired,
   /** @prop HTML ID for SpaceListMeeting | '' */
@@ -191,7 +176,7 @@ SpaceListMeeting.propTypes = {
   /** @prop SpaceListMeeting sub header node | '' */
   subheader: PropTypes.node,
   /** @prop SpaceListMeeting title | '' */
-  title: PropTypes.string
+  title: PropTypes.string,
 };
 
 SpaceListMeeting.defaultProps = {
@@ -204,9 +189,10 @@ SpaceListMeeting.defaultProps = {
   id: '',
   isBold: false,
   isMessagingOnlyShare: false,
+  eventOverlayProps: null,
   meetingType: '',
   subheader: '',
-  title: ''
+  title: '',
 };
 
 SpaceListMeeting.displayName = 'SpaceListMeeting';

e6d21979013ea4f011544c5e21fa4d235340fa90
Kevin Hyde <kehyde@cisco.com>
2020-04-13 09:40:33 -0700

    build(tokens): redirect paths, initial setup of token NPM module

---
+++ tokens/src/core/zIndex.js
@@ -0,0 +1,12 @@
+const zIndex = {
+  sidebar:        1000,
+  topbar:         1005,
+  close:          1015,
+  popover:        1020,
+  tooltip:        1030,
+  topbarFixed:    1040,
+  modalBackdrop:  1050,
+  modal:          1060,
+};
+
+module.exports = zIndex;

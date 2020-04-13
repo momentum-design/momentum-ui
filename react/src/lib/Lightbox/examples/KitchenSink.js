a18cdeb42611b259b6cea11f1181d7f02c1a003a
aimma <aimma@cisco.com>
2020-04-13 11:57:38 -0700

    fix(Lightbox): update viewport dimensions onMount and display correct % size of image

--- react/src/lib/Lightbox/examples/KitchenSink.js
+++ react/src/lib/Lightbox/examples/KitchenSink.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Lightbox } from '@momentum-ui/react';
+import { Button, Lightbox } from '@momentum-ui/react';
 import reactIcon from '@momentum-ui/core/images/momentum/momentum.jpg';
 import ciscoWebex from '@momentum-ui/core/images/cisco-webex/lockup/cisco-webex-lockup-blue.png';
 
 export default class LightboxKitchenSink extends React.Component {
+  state = {
+    index: 0,
+    show: true
+  }
   render() {
     return (
       <React.Fragment>
-        <Lightbox
-          onClose={() => {}}
-          onChange={() => {}}
+        <Button onClick={() => {this.setState({show: true});}}>
+          Open
+        </Button>
+        {
+          this.state.show && <Lightbox
+          onClose={() => {this.setState({show: false});}}
+          onChange={idx => this.setState({ index: idx })}
           name="Screen Shot 2018-04-11 at 7.32.51 PM.png"
           applicationId="app"
           onDownload={() => {}}
@@ -19,7 +27,7 @@ export default class LightboxKitchenSink extends React.Component {
             sharedOn: 'At 4/17/2018, 10:02 AM',
             size: '34.4 KB',
           }}
-          index={0}
+          index={this.state.index}
           height={250}
           width={250}
           pages={[
@@ -35,6 +43,7 @@ export default class LightboxKitchenSink extends React.Component {
             },
           ]}
         />
+      }
       </React.Fragment>
     );
   }

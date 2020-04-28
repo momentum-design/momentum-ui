14e8d5e87f0fc538588e2c972e167fbf6ddb4908
yalo <yalo@cisco.com>
2020-04-28 13:02:25 -0700

    feat(listitem): only allow tabbable children when listitem is focused

--- react/src/lib/ListItem/index.js
+++ react/src/lib/ListItem/index.js
@@ -70,8 +70,8 @@ class ListItem extends React.Component {
     parentOnSelect && parentOnSelect(e, { value, label, eventKey });
   }
 
-  handleKeyDown = (e, eventKey, focusLockTabbableChildren, tabbableChildrenQuery) => {
-    const { disabled, onKeyDown, parentKeyDown, value, label } = this.props;
+  handleKeyDown = (e, eventKey) => {
+    const { disabled, onKeyDown, parentKeyDown, value, label, focusLockTabbableChildren, tabbableChildrenQuery } = this.props;
 
     if(disabled) {
       e.preventDefault();
@@ -125,8 +125,8 @@ class ListItem extends React.Component {
     return true;
   }
 
-  handleBlur = (e, focusLockTabbableChildren, tabbableChildrenQuery) => {
-    const { disabled } = this.props;
+  handleBlur = (e) => {
+    const { disabled, focusLockTabbableChildren, tabbableChildrenQuery } = this.props;
 
     if(disabled) {
       e.preventDefault();
@@ -235,8 +235,8 @@ class ListItem extends React.Component {
       },
       ...!isReadOnly && {
         onClick: e => this.handleClick(e, cxtProps.uniqueKey),
-        onKeyDown: e => this.handleKeyDown(e, cxtProps.uniqueKey, focusLockTabbableChildren, tabbableChildrenQuery),
-        onBlur: e => this.handleBlur(e, focusLockTabbableChildren, tabbableChildrenQuery),
+        onKeyDown: e => this.handleKeyDown(e, cxtProps.uniqueKey),
+        onBlur: e => this.handleBlur(e),
         tabIndex: (!disabled && cxtProps.focus) ? 0 : -1,
       },
       'data-md-event-key': cxtProps.uniqueKey,
@@ -350,6 +350,7 @@ ListItem.propTypes = {
     PropTypes.object,
     PropTypes.array
   ]),
+  tabbableChildrenQuery: PropTypes.string,
 };
 
 ListItem.defaultProps = {
@@ -380,6 +381,7 @@ ListItem.defaultProps = {
   title: '',
   type: '',
   value: '',
+  tabbableChildrenQuery: '',
 };
 
 ListItem.displayName = 'ListItem';

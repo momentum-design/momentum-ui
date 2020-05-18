49031145706aed80d4da71e1dba09915752cbaf5
Paul Jeter <pauljeter@gmail.com>
2020-05-18 17:07:46 -0500

    feat(angularjs): remove angularjs library from mono-repo

--- angularjs/src/app/core/vendor.ts
+++
@@ -1,4 +0,0 @@
-import 'jquery';
-import 'angular';
-import 'angular-ui-router';
-import '@momentum-ui/angularjs';
--- angularjs/src/lib/directives/windowsize/index.ts
+++
@@ -1,7 +0,0 @@
-import * as angular from 'angular';
-import { mdWindowsize } from './windowsize.directive';
-
-export default angular
-  .module('momentum.ui.windowsize', [])
-  .directive('windowsize', mdWindowsize)
-  .name;

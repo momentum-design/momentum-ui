15a44988b958fbe82e7a72b62d83f738fe699f6f
Paul Jeter <pauljeter@gmail.com>
2020-05-18 17:09:03 -0500

    feat(*): remove angular and vue from utils and tools

--- tools/generateComponentsData.js
+++ tools/generateComponentsData.js
@@ -7,9 +7,7 @@ const globArray = [
   path.resolve(__dirname, '../core/scss/**/*.html'),
   // path.resolve(__dirname, '../core/scss/**/*.scss'),
   path.resolve(__dirname, '../react/src/lib/**/*.js'),
-  path.resolve(__dirname, '../angular/src/lib/**/examples/*.component.ts'),
   `!**/node_modules/**`,
-  // path.resolve(__dirname, '../angularjs/src/**/*'),
 ];
 
 createComponentsData(globArray, dataDirectory, baseDataJSON, true);

187647fc9896e424923457b05b8252000da2aad4
smiller3 <smiller3@cisco.com>
2020-05-19 16:46:46 -0700

    chore: further code req changes

--- react/src/lib/ListItem/index.js
+++ react/src/lib/ListItem/index.js
@@ -127,7 +127,6 @@ class ListItem extends React.Component {
       role,
       separator,
       title,
-      theme,
       type,
       ...props
     } = this.props;
@@ -149,7 +148,6 @@ class ListItem extends React.Component {
       className:
         'md-list-item' +
         `${(cxtProps.type && ` md-list-item--${cxtProps.type}`) || ''}` +
-        `${(theme && ` ${theme}`) || ''}` +
         `${(cxtProps.active && ` active`) || ''}` +
         `${(disabled && ` disabled`) || ''}` +
         `${(isReadOnly && ` md-list-item--read-only`) || ''}` +
@@ -266,8 +264,6 @@ ListItem.propTypes = {
   role: PropTypes.string,
   /** @prop Prop that controls whether to show separator or not | false */
   separator: PropTypes.bool,
-  /** @prop ListItem theme | '' */
-  theme: PropTypes.string,
   /** @prop ListItem Title | '' */
   title: PropTypes.string,
   /** @prop ListItem size | '' */

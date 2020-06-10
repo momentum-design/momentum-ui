4427367d9cd0966070e093e9eaf306037478fd64
yalo <yalo@cisco.com>
2020-06-10 11:31:23 -0700

    fix(listitem): accessible children with popover

--- react/src/lib/ListItem/examples/index.js
+++ react/src/lib/ListItem/examples/index.js
@@ -2,3 +2,4 @@ export { default as ListItemDefault } from './Default';
 export { default as ListItemKitchenSink } from './KitchenSink';
 export { default as ListItemListItemSections } from './ListItemSections';
 export { default as ListItemListItemSeparator } from './ListItemSeparator';
+export { default as TabbableChildren } from './TabbableChildren';

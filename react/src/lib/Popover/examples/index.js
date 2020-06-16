48207bcb2fb2309d78c24d60cab90d93631515cf
Yan-Jen Lo <34595328+yjlo@users.noreply.github.com>
2020-06-16 12:01:48 -0700

    feat(popover): accessible hover popoover (#607)
    * feat(listitem): only allow tabbable children when listitem is focused
    
    * feat(listitem): only allow tabbable children when listitem is focused
    
    * feat(listitem): only allow tabbable children when listitem is focused
    
    * feat(listitem): only allow tabbable children when listitem is focused
    
    * feat(popover): mouse enter popover accessibility
    
    * feat(popover): mouse enter popover accessibility
    
    * feat(popover): mouse enter popover accessibility
    
    * fix(popover): fix MouseEnter popover to be accessible
    
    * fix(popover): fix MouseEnter popover to be accessible
    
    * fix(popover): fix MouseEnter popover to be accessible
    
    * fix(popover): fix MouseEnter popover to be accessible
    
    * fix(popover): fix MouseEnter popover to be accessible
    
    * fix(popover): accessibility for hover popover
    
    * fix(popover): accessibility for hover popover
    
    * fix(popover): accessibility for hover popover
    
    * fix(popover): accessibility for hover popover
    
    Co-authored-by: yalo <yalo@cisco.com>

--- react/src/lib/Popover/examples/index.js
+++ react/src/lib/Popover/examples/index.js
@@ -6,3 +6,4 @@ export { default as PopoverDirection } from './Direction';
 export { default as PopoverKitchenSink } from './KitchenSink';
 export { default as PopoverOffset } from './Offset';
 export { default as PopoverOverflow } from './Overflow';
+export { default as PopOverHoverAccessible} from './AccessibleHoverPopover';

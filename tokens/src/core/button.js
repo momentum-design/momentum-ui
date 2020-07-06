df59733bcb72431cbfcaeea19cbf11fab075ae3f
Elena Giere <egiere@cisco.com>
2020-07-06 13:35:34 -0400

    feat(tokens): adding token variable names to token js object

--- tokens/src/core/button.js
+++ tokens/src/core/button.js
@@ -2,131 +2,131 @@ const colors = require("./colors");
 
 const button = {
     primary: {
-        lightUi: colors.blue[60].hex,
-        darkUi: colors.blue[60].hex,
+        lightUi: colors.blue[60].name,
+        darkUi: colors.blue[60].name,
         hover: {
-            lightUi: colors.blue[70].hex,
-            darkUi: colors.blue[70].hex,
+            lightUi: colors.blue[70].name,
+            darkUi: colors.blue[70].name,
         },
         pressed: {
-            lightUi: colors.blue[80].hex,
-            darkUi: colors.blue[80].hex,  
+            lightUi: colors.blue[80].name,
+            darkUi: colors.blue[80].name,
         },
         outline: {
-            lightUi: colors.blue[70].hex,
-            darkUi: colors.blue[40].hex,
+            lightUi: colors.blue[70].name,
+            darkUi: colors.blue[40].name,
         },
     },
     secondary: {
-        lightUi: colors.gray[20].hex,
-        darkUi: colors.gray[60].hex,
+        lightUi: colors.gray[20].name,
+        darkUi: colors.gray[60].name,
         hover: {
-            lightUi: colors.gray[30].hex,
-            darkUi: colors.gray[70].hex,
+            lightUi: colors.gray[30].name,
+            darkUi: colors.gray[70].name,
         },
         pressed: {
-            lightUi: colors.gray[40].hex,
-            darkUi: colors.gray[80].hex,  
+            lightUi: colors.gray[40].name,
+            darkUi: colors.gray[80].name,
         },
         outline: {
-            lightUi: colors.gray[70].hex,
-            darkUi: colors.gray[40].hex,
+            lightUi: colors.gray[70].name,
+            darkUi: colors.gray[40].name,
         },
     },
     green: {
-        lightUi: colors.green[60].hex,
-        darkUi: colors.green[60].hex,
+        lightUi: colors.green[60].name,
+        darkUi: colors.green[60].name,
         hover: {
-            lightUi: colors.green[70].hex,
-            darkUi: colors.green[70].hex,
+            lightUi: colors.green[70].name,
+            darkUi: colors.green[70].name,
         },
         pressed: {
-            lightUi: colors.green[80].hex,
-            darkUi: colors.green[80].hex,  
+            lightUi: colors.green[80].name,
+            darkUi: colors.green[80].name,
         },
         outline: {
-            lightUi: colors.green[70].hex,
-            darkUi: colors.green[40].hex,
+            lightUi: colors.green[70].name,
+            darkUi: colors.green[40].name,
         },
     },
     white: {
-        lightUi: colors.white.default.hex,
+        lightUi: colors.white.default.name,
         hover: {
-            lightUi: colors.gray['05'].hex,
+            lightUi: colors.gray['05'].name,
         },
         pressed: {
-            lightUi: colors.gray[10].hex,
+            lightUi: colors.gray[10].name,
         },
     },
     darkGray: {
-        darkUi: colors.gray[80].hex,
+        darkUi: colors.gray[80].name,
         hover: {
-            darkUi: colors.gray[90].hex,
+            darkUi: colors.gray[90].name,
         },
         pressed: {
-            darkUi: colors.gray[95].hex,  
+            darkUi: colors.gray[95].name,
         },
     },
     red: {
-        lightUi: colors.red[60].hex,
-        darkUi: colors.red[60].hex,
+        lightUi: colors.red[60].name,
+        darkUi: colors.red[60].name,
         hover: {
-            lightUi: colors.red[70].hex,
-            darkUi: colors.red[70].hex,
+            lightUi: colors.red[70].name,
+            darkUi: colors.red[70].name,
         },
         pressed: {
-            lightUi: colors.red[80].hex,
-            darkUi: colors.red[80].hex,  
+            lightUi: colors.red[80].name,
+            darkUi: colors.red[80].name,
         },
         outline: {
-            lightUi: colors.red[70].hex,
-            darkUi: colors.red[40].hex,
+            lightUi: colors.red[70].name,
+            darkUi: colors.red[40].name,
         },
     },
     disabled: {
-        lightUi: colors.gray[20].hex,
-        darkUi: colors.gray[90].hex,
+        lightUi: colors.gray[20].name,
+        darkUi: colors.gray[90].name,
     },
     focusRing: {
-        lightUi: colors.blue[60].hex,
-        darkUi: colors.blue[40].hex,
+        lightUi: colors.blue[60].name,
+        darkUi: colors.blue[40].name,
     },
     text: {
         primary: {
-            lightUi: colors.gray['05'].hex,
-            darkUi: colors.gray['05'].hex,
+            lightUi: colors.gray['05'].name,
+            darkUi: colors.gray['05'].name,
             outline: {
-                lightUi: colors.blue[70].hex,
-                darkUi: colors.blue[40].hex,  
+                lightUi: colors.blue[70].name,
+                darkUi: colors.blue[40].name,
             },
         },
         secondary: {
-            lightUi: colors.gray[100].hex,
-            darkUi: colors.gray['05'].hex,
+            lightUi: colors.gray[100].name,
+            darkUi: colors.gray['05'].name,
             outline: {
-                lightUi: colors.blue[70].hex,
-                darkUi: colors.blue[40].hex,  
+                lightUi: colors.blue[70].name,
+                darkUi: colors.blue[40].name,
             },
         },
         green: {
-            lightUi: colors.gray['05'].hex,
-            darkUi: colors.gray['05'].hex,
+            lightUi: colors.gray['05'].name,
+            darkUi: colors.gray['05'].name,
             outline: {
-                lightUi: colors.green[70].hex,
-                darkUi: colors.green[40].hex,  
+                lightUi: colors.green[70].name,
+                darkUi: colors.green[40].name,
             },
         },
         red: {
-            lightUi: colors.gray['05'].hex,
-            darkUi: colors.gray['05'].hex,
+            lightUi: colors.gray['05'].name,
+            darkUi: colors.gray['05'].name,
             outline: {
-                lightUi: colors.red[70].hex,
-                darkUi: colors.red[40].hex,  
+                lightUi: colors.red[70].name,
+                darkUi: colors.red[40].name,
             },
         },
         disabled: {
-            lightUi: colors.gray[40].hex,
-            darkUi: colors.gray[70].hex,
+            lightUi: colors.gray[40].name,
+            darkUi: colors.gray[70].name,
         },
     },
 };

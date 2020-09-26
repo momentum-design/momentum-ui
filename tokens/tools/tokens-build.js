8146c34a1a1cdf8d2f28923b21f0b0d0b901994c
Jeff Personal <jeffbirori@gmail.com>
2020-09-25 17:10:48 -0700

    refactor(tokens): update token build file to include semantic color

--- tokens/tools/tokens-build.js
+++ tokens/tools/tokens-build.js
@@ -1,23 +1,23 @@
 const generateTokenJSON = require("./generateTokens");
 
 const border = require("../src/core/border");
-const button = require("../src/core/button");
 const colors = require("../src/core/colors");
 const font = require("../src/core/font");
 const lineHeight = require("../src/core/lineHeight");
 const opacity = require("../src/core/opacity");
 const radius = require("../src/core/radius");
+const semanticColor = require("../src/core/semanticColor");
 const zIndex = require("../src/core/zIndex");
 
 (async function() {
   try {
     await generateTokenJSON('border', border);
-    await generateTokenJSON('button', button);
     await generateTokenJSON('colors', colors);
     await generateTokenJSON('font', font);
     await generateTokenJSON('lineHeight', lineHeight);
     await generateTokenJSON('opacity', opacity);
     await generateTokenJSON('radius', radius);
+    await generateTokenJSON('semanticColor', semanticColor);
     await generateTokenJSON('zIndex', zIndex);
   } catch (e) {
     console.error("Failed to generate tokens\n", e);

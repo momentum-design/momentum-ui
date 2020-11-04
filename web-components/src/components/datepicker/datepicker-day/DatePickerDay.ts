829dbbd5b76646a92ba3c186be67a6ea15e08b28
Kevin Hyde <kehyde@cisco.com>
2020-11-04 15:02:10 -0800

    Add moment and dateUtils, flesh out date-picker-day

--- web-components/src/components/datepicker/datepicker-day/DatePickerDay.ts
+++ web-components/src/components/datepicker/datepicker-day/DatePickerDay.ts
@@ -1,13 +1,48 @@
 import "@/components/button/Button";
-import { customElement, html, LitElement } from "lit-element";
+import { getMonth, isDayDisabled, isSameDay, now } from "@/utils/dateUtils";
+import { customElement, html, LitElement, property } from "lit-element";
 
 export namespace DatePickerDay {}
 
 @customElement("md-datepicker-day")
 export class DatePickerDay extends LitElement {
+  @property() day: undefined; // meant to be a moment instance reflecting a day
+  @property() month: undefined; // meant to be a moment instance reflecting a month
+  @property() selected: undefined; // not sure, seems moment related?
+  //   @property() focus = false; replace this, otherwise it expects dom native focus()
+  @property() handleDayClick: (e, day) => {}; // a passed function from the main picker context
+
+  handleClick(e) {
+    const { handleDayClick, day } = this;
+    return handleDayClick && handleDayClick(e, day);
+  }
+
   render() {
+    const { selected, focus, day, month } = this;
+
+    const isOutsideMonth = month !== getMonth(day);
+    const isSelected = isSameDay(day, selected);
+    const isToday = isSameDay(day, now());
+    const disabled = isDayDisabled(day, this);
+    const hasFocus = isSameDay(day, focus);
+
     return html`
-      <md-button>10</md-button>
+      <md-button
+        circle
+        size=${28}
+        ?disabled=${disabled}
+        className=${"md-datepicker__day" +
+          `${(isSelected && ` md-datepicker__day--selected`) || ""}` +
+          `${(hasFocus && ` md-datepicker__day--focus`) || ""}` +
+          `${(isToday && ` md-datepicker__day--today`) || ""}` +
+          `${(isOutsideMonth && ` md-datepicker__day--outside-month`) || ""}`}
+        @click=${this.handleClick}
+        aria-label=${`${day.format("D, dddd MMMM YYYY")}`}
+        aria-selected=${isSelected}
+        tab-index=${-1}
+      >
+        10
+      </md-button>
     `;
   }
 }

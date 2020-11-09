23d2e29c7a43e60134930cc9f3180ab7b6028017
Kevin Hyde <kehyde@cisco.com>
2020-11-09 11:28:24 -0800

    Add datepickerDay classmap, update sandbox

--- web-components/src/[sandbox]/examples/datepicker.ts
+++ web-components/src/[sandbox]/examples/datepicker.ts
@@ -7,8 +7,18 @@ import { html } from "lit-element";
 
 export const datePickerTemplate = html`
   <h2 class="sandbox-header">md-datepicker</h2>
+  <h3>calendar</h3>
   <md-datepicker-calendar></md-datepicker-calendar>
+  <h3>month</h3>
   <md-datepicker-month></md-datepicker-month>
+  <h3>week</h3>
   <md-datepicker-week></md-datepicker-week>
-  <md-datepicker-day>test</md-datepicker-day>
+  <h3>day</h3>
+  <md-datepicker-day></md-datepicker-day>
+  <h3>disabled day</h3>
+  <md-datepicker-day disabled></md-datepicker-day>
+  <h3>selected day</h3>
+  <md-datepicker-day selected></md-datepicker-day>
+  <h3>focused day</h3>
+  <md-datepicker-day focused></md-datepicker-day>
 `;
--- web-components/src/components/datepicker/datepicker-day/DatePickerDay.ts
+++ web-components/src/components/datepicker/datepicker-day/DatePickerDay.ts
@@ -1,6 +1,7 @@
 import "@/components/button/Button";
 import { DayFilters, getDate, now } from "@/utils/dateUtils";
 import { customElement, html, internalProperty, LitElement, property } from "lit-element";
+import { classMap } from "lit-html/directives/class-map";
 import { ifDefined } from "lit-html/directives/if-defined";
 import { DateTime } from "luxon/index";
 
@@ -17,25 +18,20 @@ export namespace DatePickerDay {
 
 @customElement("md-datepicker-day")
 export class DatePickerDay extends LitElement {
+  @property({ type: Boolean }) focused = false; // passed in current selection, a DateTime instance
+  @property({ type: Boolean }) selected = false; // passed in current selection, a DateTime instance
+  @property({ type: Boolean }) disabled = false;
   @property({ attribute: false }) day: DateTime | undefined = undefined; // meant to be a DateTime instance reflecting a day
-  @property({ attribute: false }) focused = false; // passed in current selection, a DateTime instance
   @property({ attribute: false }) handleDayClick: Function | undefined = undefined; // a passed function from the main picker context
   @property({ attribute: false }) month: number | undefined = undefined; // meant to be a DateTime instance reflecting a month
-  @property({ attribute: false }) selected = false; // passed in current selection, a DateTime instance
   @property({ attribute: false }) filterParams: DayFilters | null = null; // passed in current selection, a DateTime instance
-  @property({ type: Boolean }) disabled = false;
 
-  @internalProperty() protected isOutsideMonth: boolean | undefined = undefined;
+  @internalProperty() protected isOutsideMonth = false;
   @internalProperty() protected isSelected: boolean | undefined = undefined;
-  @internalProperty() protected isToday: boolean | undefined = undefined;
+  @internalProperty() protected isToday = false;
   @internalProperty() protected hasFocus: boolean | undefined = undefined;
 
-  // constructor(props) {
-  //   super(props);
-  //   this.dayButton = React.createRef();
-  // }
-
-  // componentDidUpdate = () => {
+  // componentDidUpdate = () => {    /// FROM REACT . . . intentions unclear, keep until further use cases come up. kh 11/9/20
   //   const { day, focus } = this.props;
 
   //   isSameDay(day, focus) && this.dayButton.current.button.focus();
@@ -45,7 +41,6 @@ export class DatePickerDay extends LitElement {
     super.connectedCallback();
     if (this.day === undefined) {
       this.day = now();
-      console.log(this.day);
     }
   }
 
@@ -62,19 +57,21 @@ export class DatePickerDay extends LitElement {
   }
 
   render() {
+    const dayClassMap = {
+      "md-datepicker__day--selected": this.selected,
+      "md-datepicker__day--focus": this.focused,
+      "md-datepicker__day--today": this.isToday,
+      "md-datepicker__day--outside-month": this.isOutsideMonth
+    };
+
     return html`
-      <span>day</span>
       <md-button
         circle
         size=${28}
         ?disabled=${this.disabled}
-        class=${"md-datepicker__day" +
-          `${(this.isSelected && ` md-datepicker__day--selected`) || ""}` +
-          `${(this.hasFocus && ` md-datepicker__day--focus`) || ""}` +
-          `${(this.isToday && ` md-datepicker__day--today`) || ""}` +
-          `${(this.isOutsideMonth && ` md-datepicker__day--outside-month`) || ""}`}
+        class="md-datepicker__day ${classMap(dayClassMap)}"
         @click=${this.handleClick}
-        aria-label=${`${this.day?.toFormat("D, dddd MMMM YYYY")}`}
+        aria-label=${`${this.day?.toFormat("D, dd MMMM yyyy")}`}
         aria-selected=${ifDefined(this.isSelected)}
         tab-index=${-1}
       >

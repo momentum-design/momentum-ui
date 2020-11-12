7d0926b09923301217c2b5cfce114bdb021d45a1
Lena Vaganova (helenv) <helenv@cisco.com>
2020-11-12 09:40:35 +0200

    Added tokens

--- web-components/src/components/table/Table.ts
+++ web-components/src/components/table/Table.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Cisco Systems, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import reset from "@/wc_scss/reset.scss";
 import { customElement, html, internalProperty, LitElement, property, query } from "lit-element";
 import Papa from "papaparse";

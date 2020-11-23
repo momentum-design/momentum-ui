ad9ce5d21f0c7c0cdded272b8127b7a9674a03f9
Kevin Hyde <kehyde@cisco.com>
2020-11-23 13:26:13 -0800

    Datepicker Story Updates, remove callbacks

--- web-components/src/components/datepicker/DatePicker.ts
+++ web-components/src/components/datepicker/DatePicker.ts
@@ -3,26 +3,22 @@ import "@/components/input/Input";
 import "@/components/menu-overlay/MenuOverlay";
 import { MenuOverlay } from "@/components/menu-overlay/MenuOverlay";
 import { addDays, addWeeks, DayFilters, isDayDisabled, now, subtractDays, subtractWeeks } from "@/utils/dateUtils";
-import { customElement, html, internalProperty, LitElement, property, query } from "lit-element";
+import { customElement, html, internalProperty, LitElement, property, PropertyValues, query } from "lit-element";
 import { DateTime } from "luxon";
 
 export namespace DatePicker {}
-
 @customElement("md-datepicker")
 export class DatePicker extends LitElement {
   @property({ type: Boolean }) shouldCloseOnSelect = true;
   @property({ type: String }) maxDate: string | undefined = undefined;
   @property({ type: String }) minDate: string | undefined = undefined;
   @property({ type: String, reflect: true }) value: string | undefined = undefined;
+  @property({ type: String }) weekStart = "Sun";
 
-  @internalProperty() monthFormat = undefined;
   @internalProperty() locale: string = now().locale;
+  @internalProperty() selectedDate: DateTime = now();
   @internalProperty() focusedDate: DateTime = now();
   @internalProperty() filterDate: Function | undefined = undefined;
-  @internalProperty() onChange: Function | undefined = undefined;
-  @internalProperty() selectedDate: DateTime = now();
-  @internalProperty() onMonthChange: Function | undefined = undefined;
-  @internalProperty() onSelect: Function | undefined = undefined;
   @internalProperty() maxDateData: DateTime | undefined = undefined;
   @internalProperty() minDateData: DateTime | undefined = undefined;
 
@@ -34,11 +30,15 @@ export class DatePicker extends LitElement {
     this.maxDate !== undefined ? (this.maxDateData = DateTime.fromSQL(this.maxDate)) : null;
   }
 
-  setOpen = (open: boolean): void => {
+  update(changedProperties: PropertyValues) {
+    super.update(changedProperties);
+  }
+
+  setOpen = (open: boolean) => {
     this.menuOverlay.isOpen = open;
   };
 
-  handleSelect = (e: CustomEvent): void => {
+  handleSelect = (e: CustomEvent) => {
     const date = e.detail.date;
     const event = e.detail.sourceEvent;
     this.setPreSelection(date, event);
@@ -46,20 +46,19 @@ export class DatePicker extends LitElement {
     this.shouldCloseOnSelect && this.setOpen(false);
   };
 
-  setSelected = (date: DateTime, event: Event): void => {
+  setSelected = (date: DateTime, event: Event) => {
     const filters: DayFilters = { maxDate: this.maxDateData, minDate: this.minDateData, filterDate: this.filterDate };
     if (!isDayDisabled(date, filters)) {
       const dateString = `${date.year}-${date.month}-${date.day}`;
       this.selectedDate = date;
       this.value = dateString;
     }
-    this.onSelect && this.onSelect(event, date);
+    // CustonEvent expose payload
   };
 
-  setPreSelection = (date: DateTime, event: KeyboardEvent): void => {
+  setPreSelection = (date: DateTime, event: KeyboardEvent) => {
     const filters: DayFilters = { maxDate: this.maxDateData, minDate: this.minDateData, filterDate: this.filterDate };
     !isDayDisabled(date, filters) ? (this.focusedDate = date) : null;
-    this.onChange && this.onChange(event, date);
   };
 
   handleKeyDown = (e: CustomEvent) => {

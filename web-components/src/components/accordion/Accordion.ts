65c7c7fef0ecfa62bf7fe60648f55a388447b914
boiarchukk <bk1992@cisco.com>
2020-11-23 18:18:08 +0200

    feat(accordion): create new accordion component

---
+++ web-components/src/components/accordion/Accordion.ts
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) Cisco Systems, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import { SlottedMixin } from "@/mixins";
+import { customElement, html, LitElement, property, PropertyValues, query } from "lit-element";
+import { Key } from "@/constants";
+import reset from "@/wc_scss/reset.scss";
+import styles from "./scss/module.scss";
+import { AccordionItem, AccordionEvent } from "./AccordionItem";
+
+@customElement("md-accordion")
+export class Accordion extends SlottedMixin(LitElement) {
+  @property({ type: Boolean, reflect: true }) multiple = false;
+
+  @query('slot[name="accordion-item"]') accordionItemSlotElement!: HTMLSlotElement;
+
+  get slotItem() {
+    return this.accordionItemSlotElement;
+  }
+
+  filterSlotted() {
+    return this.accordionItemSlotElement.assignedElements() as HTMLElement[];
+  }
+
+  private switchFocusedAccordionItem(direction: number) {
+    const focusedAccordionItemIndex = this.slotted.findIndex(accordionItem =>
+      (accordionItem as AccordionItem).hasAttribute("focus-visible")
+    );
+
+    const accordionItemsCount = this.slotted.length;
+    let nextFocusedAccordionItemIndex = focusedAccordionItemIndex;
+
+    nextFocusedAccordionItemIndex =
+      (accordionItemsCount + nextFocusedAccordionItemIndex + direction) % accordionItemsCount;
+    while ((this.slotted[nextFocusedAccordionItemIndex] as AccordionItem).disabled) {
+      nextFocusedAccordionItemIndex =
+        (accordionItemsCount + nextFocusedAccordionItemIndex + direction) % accordionItemsCount;
+    }
+    this.focusAccordionItem(this.slotted[nextFocusedAccordionItemIndex] as AccordionItem);
+  }
+
+  private focusAccordionItem(accordionItem: AccordionItem) {
+    if (!accordionItem.hasAttribute("focus-visible")) {
+      requestAnimationFrame(() => accordionItem.header.focus());
+    }
+  }
+
+  private setActiveAccordionItem(accordionItem: AccordionItem) {
+    if (!this.multiple) {
+      this.slotted.forEach(item => {
+        const anotherAccordionItem = item as AccordionItem;
+        if (anotherAccordionItem.expanded && !item.isEqualNode(accordionItem)) {
+          anotherAccordionItem.expanded = false;
+        }
+      });
+    }
+    accordionItem.expanded = true;
+  }
+
+  private setupExpandedAccordionItems() {
+    if (!this.multiple) {
+      this.slotted
+        .filter(accordionItem => (accordionItem as AccordionItem).expanded)
+        .forEach((accordionItem, index) => {
+          const item = accordionItem as AccordionItem;
+          if (item.expanded && index > 0) {
+            item.expanded = false;
+          }
+        });
+    }
+  }
+
+  handleKeyDown(event: CustomEvent<AccordionEvent>) {
+    const { srcEvent } = event.detail;
+    const { code } = srcEvent as KeyboardEvent;
+
+    event.preventDefault();
+
+    switch (code) {
+      case Key.Space:
+      case Key.Enter: {
+        const { target } = event;
+        if (target) {
+          this.setActiveAccordionItem(target as AccordionItem);
+        }
+        break;
+      }
+      case Key.ArrowDown: {
+        this.switchFocusedAccordionItem(1);
+        break;
+      }
+      case Key.ArrowUp: {
+        this.switchFocusedAccordionItem(-1);
+        break;
+      }
+      case Key.Home: {
+        const firstAccordionItem = this.slotted[0] as AccordionItem;
+        this.focusAccordionItem(firstAccordionItem);
+        break;
+      }
+      case Key.End: {
+        const lastAccordionItem = this.slotted[this.slotted.length - 1] as AccordionItem;
+        this.focusAccordionItem(lastAccordionItem);
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  handleClick(event: CustomEvent<AccordionEvent>) {
+    const { target } = event;
+    const accordionItem = this.slotted.find(item => (item as AccordionItem).isSameNode(target as HTMLElement));
+    if (accordionItem) {
+      this.setActiveAccordionItem(accordionItem as AccordionItem);
+    }
+  }
+
+  protected updated(changedProperties: PropertyValues) {
+    super.updated(changedProperties);
+    if (changedProperties.has("slotted")) {
+      this.setupExpandedAccordionItems();
+    }
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener("accordion-item-keydown", this.handleKeyDown as EventListener);
+    this.addEventListener("accordion-item-click", this.handleClick as EventListener);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.removeEventListener("accordion-item-keydown", this.handleKeyDown as EventListener);
+    this.removeEventListener("accordion-item-click", this.handleClick as EventListener);
+  }
+
+  static get styles() {
+    return [reset, styles];
+  }
+
+  render() {
+    return html`
+      <slot name="accordion-item"></slot>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "md-accordion": Accordion;
+  }
+}
---
+++ web-components/src/components/accordion/AccordionItem.ts
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) Cisco Systems, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import { customElement, html, LitElement, property, query, PropertyValues } from "lit-element";
+import { ifDefined } from "lit-html/directives/if-defined";
+import { nanoid } from "nanoid";
+import reset from "@/wc_scss/reset.scss";
+import styles from "./scss/module.scss";
+import { FocusMixin } from "@/mixins";
+
+export type AccordionEvent = {
+  srcEvent: MouseEvent | KeyboardEvent;
+};
+
+@customElement("md-accordion-item")
+export class AccordionItem extends FocusMixin(LitElement) {
+  private _expanded = false;
+  private _level = 3;
+
+  private uniqueId = nanoid();
+
+  @property({ type: String }) label = "";
+  @property({ type: Boolean, reflect: true }) disabled = false;
+  @property({ type: Boolean, reflect: true })
+  get expanded() {
+    return this._expanded;
+  }
+  set expanded(value: boolean) {
+    const oldValue = this._expanded;
+    this._expanded = value;
+
+    if (value) {
+      this.notifyExpandedHeader();
+    }
+    this.requestUpdate("expanded", oldValue);
+  }
+  @property({ type: Number, reflect: true })
+  get level() {
+    return this._level;
+  }
+  set level(value: number) {
+    const oldValue = this.level;
+
+    if (value < 1 && value > 6) {
+      console.warn("Please set appropriate section heading level");
+      this._level = 3;
+    } else {
+      this._level = value;
+    }
+    this.requestUpdate("level", oldValue);
+  }
+
+  @query(".accordion-header") header!: HTMLButtonElement;
+
+  private notifyExpandedHeader() {
+    this.dispatchEvent(
+      new CustomEvent("focus-visible", {
+        composed: true,
+        bubbles: true
+      })
+    );
+  }
+
+  protected firstUpdated(changedProperties: PropertyValues) {
+    super.firstUpdated(changedProperties);
+    this.setAttribute("id", this.uniqueId);
+  }
+
+  static get styles() {
+    return [reset, styles];
+  }
+
+  handleClick(event: MouseEvent) {
+    if (!this.disabled) {
+      this.dispatchEvent(
+        new CustomEvent<AccordionEvent>("accordion-item-click", {
+          detail: {
+            srcEvent: event
+          },
+          composed: true,
+          bubbles: true
+        })
+      );
+    }
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    if (!this.disabled) {
+      this.dispatchEvent(
+        new CustomEvent<AccordionEvent>("accordion-item-keydown", {
+          detail: {
+            srcEvent: event
+          },
+          bubbles: true,
+          composed: true
+        })
+      );
+    }
+  }
+
+  render() {
+    return html`
+      <div role="heading" aria-level=${this.level}>
+        <button
+          type="button"
+          aria-expanded=${this.expanded}
+          class="accordion-header"
+          aria-label=${ifDefined(this.label || undefined)}
+          aria-controls="section-${this.uniqueId}"
+          aria-disabled=${this.disabled}
+          ?disabled=${this.disabled}
+          id="accordion-${this.uniqueId}"
+          part="accordion-header"
+          tabindex=${ifDefined(this.disabled ? -1 : undefined)}
+          @click=${this.handleClick}
+          @keydown=${this.handleKeyDown}
+        >
+          ${this.label}
+          <md-icon name=${this.expanded ? "icon-arrow-up_18" : "icon-arrow-down_18"}></md-icon>
+        </button>
+      </div>
+      <div
+        role="region"
+        id="section-${this.uniqueId}"
+        aria-labelledby="accordion-${this.uniqueId}"
+        class="accordion-panel"
+        part="accordion-panel"
+      >
+        <slot></slot>
+      </div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "md-accordion-item": AccordionItem;
+  }
+}

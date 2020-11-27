d215126bb3b8f935aed76ade23a2334543b69876
boiarchukk <bk1992@cisco.com>
2020-11-27 16:01:30 +0200

    docs(storybook): update storybook to floating modal

--- web-components/src/components/floating-modal/FloatingModal.stories.ts
+++ web-components/src/components/floating-modal/FloatingModal.stories.ts
@@ -1,6 +1,8 @@
 import "./FloatingModal";
 import { withA11y } from "@storybook/addon-a11y";
-import { boolean, text, withKnobs } from "@storybook/addon-knobs";
+import { boolean, withKnobs } from "@storybook/addon-knobs";
+import "@/components/radio/Radio";
+import "@/components/radio/RadioGroup";
 import { html } from "lit-element";
 
 export default {
@@ -16,14 +18,17 @@ export default {
 
 export const FloatingModal = () => {
   const show = boolean("show", false);
-  const heading = text("headerLabel", "Test header text");
-  const fixfull = boolean("fixfull", false);
+  const full = boolean("full-screen", false);
+  const fixed = boolean("fixed-strategy", false)
 
   return html`
-    <md-floating-modal .show=${show} heading="${heading}" .fixfull="${fixfull}">
-      <div slot="header">
-        <span>Test slot header</span>
-      </div>
+    <md-floating-modal ?show=${show} ?full-screen=${full} ?fixed-strategy=${fixed}>
+      <md-radiogroup group-label="group_process">
+        <md-radio slot="radio" value="Option 1">Option 1</md-radio>
+        <md-radio slot="radio" value="Option 2">Option 2</md-radio>
+        <md-radio slot="radio" value="Option 3">Option 3</md-radio>
+        <md-radio slot="radio" value="Option 4">Option 4</md-radio>
+      </md-radiogroup>
     </md-floating-modal>
   `;
 };

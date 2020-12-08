6d724ccae448b0235cbd23050c595aabb6653baf
boiarchukk <bk1992@cisco.com>
2020-12-08 17:22:29 +0200

    docs(sandbox): update pagination example

--- web-components/src/[sandbox]/examples/pagination.ts
+++ web-components/src/[sandbox]/examples/pagination.ts
@@ -3,5 +3,5 @@ import { html } from "lit-element";
 
 export const paginationTemplate = html`
   <h3>Default Pagination</h3>
-  <md-pagination page="1" total="101" limit="10" size="5"></md-pagination>
+  <md-pagination total-page="20" current-page="10" visible-page="6"></md-pagination>
 `;

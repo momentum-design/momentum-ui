b68e3e691432b3c2abbad93a61c59e13d550b3df
boiarchukk <bk1992@cisco.com>
2020-12-10 14:49:04 +0200

    docs(storybook): add new property to storybook

--- web-components/src/components/tooltip/Tooltip.stories.ts
+++ web-components/src/components/tooltip/Tooltip.stories.ts
@@ -8,7 +8,7 @@
 
 import { withA11y } from "@storybook/addon-a11y";
 import { select, text, boolean, withKnobs } from "@storybook/addon-knobs";
-import { action } from '@storybook/addon-actions';
+import { action } from "@storybook/addon-actions";
 import { html } from "lit-element";
 import "@/components/button/Button";
 import "@/components/tooltip/Tooltip";
@@ -35,10 +35,17 @@ export const Tooltip = () => {
   const lumos = boolean("Lumos Theme", false);
   const message = text("message", "Test tooltip");
   const placement = select("placement", tooltipPlacement, "right");
+  const opened = boolean("Opened", false);
 
   return html`
     <md-theme class="theme-toggle" id="tooltip" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-tooltip message=${message} placement=${placement} @tooltip-create=${(action('show'))} @tooltip-destroy=${(action('hide'))}>
+      <md-tooltip
+        message=${message}
+        placement=${placement}
+        @tooltip-create=${action("show")}
+        @tooltip-destroy=${action("hide")}
+        ?opened=${opened}
+      >
         <md-button>Test Button</md-button>
       </md-tooltip>
     </md-theme>

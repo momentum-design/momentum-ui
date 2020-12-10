9ce20bfbc5422c9a5fd06f2469a89ea8261d14f6
Elena Giere <egiere@cisco.com>
2020-12-10 11:45:06 -0800

    feat(datetime): wip: time related storybook changes

--- web-components/src/components/date-time-picker/DateTimePicker.stories.ts
+++ web-components/src/components/date-time-picker/DateTimePicker.stories.ts
@@ -1,8 +1,11 @@
 import { now } from "@/utils/dateUtils";
 import { withA11y } from "@storybook/addon-a11y";
-import { boolean, text, withKnobs } from "@storybook/addon-knobs";
+import { boolean, text, withKnobs, select } from "@storybook/addon-knobs";
 import { html } from "lit-element";
-import "./DateTimePicker";
+import { weekStartDays } from "@/components/datepicker/DatePicker";
+import { timeSpecificity } from "@/components/timepicker/TimePicker";
+import { TIME_UNIT } from "@/constants";
+import "@/components/date-time-picker/DateTimePicker";
 
 export default {
   title: "Date Time Picker",
@@ -17,6 +20,23 @@ export default {
 
 export const DateTimePicker = () => {
   const darkTheme = boolean("darkMode", false);
+  const lumos = boolean("Lumos Theme", false);
+  const weekStart = select("weekStart", weekStartDays, "");
+  const twoDigitAutoTab = boolean("twoDigitAutoTab", false);
+  const twentyFourHourFormat = boolean("twentyFourHourFormat", false);
+  const theTimeSpecificity = select("timeSpecificity", timeSpecificity, TIME_UNIT.SECOND);
+  const locale = text("locale", "en-US");
+
+  const value = text(
+    "value",
+    now().set({
+      hour: 12,
+      minute: 0,
+      second: 0,
+      millisecond: 0
+    }).toSQL()
+  );
+
   const minDate = text(
     "minimum date",
     now()
@@ -26,12 +46,22 @@ export const DateTimePicker = () => {
   const maxDate = text(
     "maximum date",
     now()
-      .plus({ day: 5 })
+      .plus({ day: 30 })
       .toSQLDate()
   );
+
   return html`
-    <md-theme ?darkTheme=${darkTheme}>
-      <md-date-time-picker minDate=${minDate} maxDate=${maxDate}></md-date-time-picker>>
+    <md-theme class="theme-toggle" id="datetimepicker" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+      <md-date-time-picker
+        value=${value}
+        minDate=${minDate}
+        maxDate=${maxDate}
+        locale=${locale}
+        weekStart=${weekStart}
+        ?twoDigitAutoTab=${twoDigitAutoTab}
+        ?twentyfourhourformat=${twentyFourHourFormat}
+        timeSpecificity=${theTimeSpecificity}>
+      </md-date-time-picker>
     </md-theme>
   `;
 };

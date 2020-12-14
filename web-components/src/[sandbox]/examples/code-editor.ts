80654af53dc5188bd118642d341fe1113b461944
boiarchukk <bk1992@cisco.com>
2020-12-14 15:44:22 +0200

    docs(sandbox): add slottable code-editor example

--- web-components/src/[sandbox]/examples/code-editor.ts
+++ web-components/src/[sandbox]/examples/code-editor.ts
@@ -2,6 +2,15 @@ import "@/components/code-editor/CodeEditor";
 import { html } from "lit-element";
 
 export const codeEditorTemplate = html`
-  <h3>Code Editor</h3>
+  <h3>Default</h3>
   <md-code-editor></md-code-editor>
+  <h3>Slotted Code</h3>
+  <md-code-editor>
+    <code class="javascript" slot="code-block">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </code>
+  </md-code-editor>
 `;

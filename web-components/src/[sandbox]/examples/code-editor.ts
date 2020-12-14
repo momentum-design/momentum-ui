c4d9af98fb640f91381d16882765744592927446
boiarchukk <bk1992@cisco.com>
2020-12-14 13:24:58 +0200

    docs(sandbox): add code editor sandbox example

--- web-components/src/[sandbox]/examples/code-editor.ts
+++ web-components/src/[sandbox]/examples/code-editor.ts
@@ -2,7 +2,6 @@ import "@/components/code-editor/CodeEditor";
 import { html } from "lit-element";
 
 export const codeEditorTemplate = html`
-  <h3 class="sandbox-header">Code Editor</h3>
-  <md-code-editor> </md-code-editor>
-
+  <h3>Code Editor</h3>
+  <md-code-editor></md-code-editor>
 `;

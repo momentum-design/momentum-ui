889d17c20fb1b125acfade8aeab0ec309d459247
helen-v <53182516+helen-v@users.noreply.github.com>
2021-01-13 22:52:55 +0200

    Create menu item component (#732)
    * feat(menuitem): create new component
    
    * feat(component): updated menu component
    
    * feat(menu): improve menu component
    
    * feat(menu): update styles for selected and disabled mode, add keydown
    
    * feat(menu): update component
    
    * fix(menu): update color tokens
    
    * feat(menu): add storybook
    
    * feat(menu): small fixes
    
    * feat(menu): add tests for components
    
    * feat(menu): fix alphabetic order in sandbox
    
    * feat(menu): update styles
    
    * fix(menu): remove console comand
    
    * feat(menu): fix according comments
    
    Co-authored-by: Yana Harris <ybernadskaya@gmail.com>

--- web-components/src/[sandbox]/sandbox.ts
+++ web-components/src/[sandbox]/sandbox.ts
@@ -28,6 +28,7 @@ import {
   listTemplate,
   loadingTemplate,
   meetingAlertTemplate,
+  menuItemTemplate,
   menuOverlayTemplate,
   modalTemplate,
   paginationTemplate,
@@ -286,6 +287,13 @@ export class Sandbox extends LitElement {
             <h2>md-meeting-alert</h2>
             <sass-stats component="meeting-alert">
               ${meetingAlertTemplate}
+            </sass-stats>
+          </div>
+
+          <div class="container" aria-label="md-menu">
+            <h2>md-menu and md-menu-item</h2>
+            <sass-stats component="menu">
+              ${menuItemTemplate}
             </sass-stats>
           </div>
 

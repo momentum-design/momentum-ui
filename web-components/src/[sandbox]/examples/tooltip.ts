52d827989356b79ce040ef4802d85df3ca861896
boiarchukk <bk1992@cisco.com>
2021-01-15 17:02:58 +0200

    docs(tooltip): add examples to tooltip sandbox

--- web-components/src/[sandbox]/examples/tooltip.ts
+++ web-components/src/[sandbox]/examples/tooltip.ts
@@ -1,51 +1,110 @@
 import "@/components/button/Button";
 import "@/components/icon/Icon.ts";
 import "@/components/tooltip/Tooltip.ts";
-import { html } from "lit-element";
+import { customElement, html, LitElement, property } from "lit-element";
+
+@customElement("tooltip-message-template-sandbox")
+export class TooltipMessageTemplateSandbox extends LitElement {
+  @property({ type: Boolean }) isClicked = false;
+
+  private handleClick() {
+    this.isClicked = !this.isClicked;
+  }
+
+  render() {
+    return html`
+      <md-tooltip placement="top" message=${this.isClicked ? "Copied" : "Copy"}>
+        <md-button
+          type="button"
+          circle
+          size="28"
+          variant=${this.isClicked ? "green" : "secondary"}
+          @click=${this.handleClick}
+        >
+          <md-icon name=${this.isClicked ? "icon-copy_16" : "icon-mouse-cursor_16"}></md-icon
+        ></md-button>
+      </md-tooltip>
+    `;
+  }
+}
+
+@customElement("tooltip-slot-template-sandbox")
+export class TooltipTemplateSandbox extends LitElement {
+  @property({ type: Boolean }) isClicked = false;
+
+  private handleClick() {
+    this.isClicked = !this.isClicked;
+  }
+
+  render() {
+    return html`
+      <md-tooltip placement="top">
+        ${this.isClicked
+          ? html`
+              <md-icon slot="tooltip-content" name="icon-copy_16"></md-icon>
+            `
+          : html`
+              <md-icon slot="tooltip-content" name="icon-mouse-cursor_16"></md-icon>
+            `}
+        <md-button
+          type="button"
+          circle
+          size="28"
+          variant=${this.isClicked ? "green" : "red"}
+          @click=${this.handleClick}
+        >
+          <md-icon name="icon-alert-active_16"></md-icon>
+        </md-button>
+      </md-tooltip>
+    `;
+  }
+}
 
 export const tooltipTemplate = html`
-  <div class="container">
-    <div class="column ">
-      <div class="row md-margin__bottom"><h3>md-tooltip</h3></div>
-      <div class="row md-padding__vertical">
-        <md-tooltip message="Tooltip Tooltip Tooltip v Tooltip v Tooltip">
-          <md-button>Tooltip Default</md-button>
-        </md-tooltip>
-      </div>
-      <div class="row md-padding__vertical">
-        <md-tooltip message="Tooltip" placement="right">
-          <md-button>Tooltip Right</md-button>
-        </md-tooltip>
-        <md-tooltip message="Tooltip" placement="right">
-          <md-button>Tooltip Right</md-button>
-        </md-tooltip>
-      </div>
-      <div class="row md-padding__vertical">
-        <md-tooltip message="Tooltip" placement="top">
-          <md-button>Tooltip Top</md-button>
-        </md-tooltip>
-      </div>
-      <div class="row md-padding__vertical">
-        <md-tooltip message="Tooltip" placement="bottom">
-          <md-button>Tooltip Bottom</md-button>
-        </md-tooltip>
-      </div>
-      <div class="row md-padding__vertical">
-        <md-tooltip message="Tooltip" placement="left">
-          <md-button>Tooltip Left</md-button>
-        </md-tooltip>
-      </div>
-      <div class="row md-padding__vertical">
-        <md-tooltip message="Tooltip" placement="right">
-          <md-button>Tooltip Right</md-button>
-        </md-tooltip>
-      </div>
-      <div class="row md-padding__vertical">
-        <md-tooltip placement="right">
-          <div slot="tooltip-content"><md-icon style="margin-right: .5rem" name="headset_12"></md-icon> Call</div>
-          <md-button>Tooltip Slot Content</md-button>
-        </md-tooltip>
-      </div>
-    </div>
+  <div class="row md-margin__bottom"><h3>md-tooltip</h3></div>
+  <div class="row md-padding__vertical">
+    <md-tooltip message="Tooltip Tooltip Tooltip v Tooltip v Tooltip">
+      <md-button>Tooltip Default</md-button>
+    </md-tooltip>
+  </div>
+  <div class="row md-padding__vertical">
+    <md-tooltip message="Tooltip" placement="right">
+      <md-button>Tooltip Right</md-button>
+    </md-tooltip>
+    <md-tooltip message="Tooltip" placement="right">
+      <md-button>Tooltip Right</md-button>
+    </md-tooltip>
+  </div>
+  <div class="row md-padding__vertical">
+    <md-tooltip message="Tooltip" placement="top">
+      <md-button>Tooltip Top</md-button>
+    </md-tooltip>
+  </div>
+  <div class="row md-padding__vertical">
+    <md-tooltip message="Tooltip" placement="bottom">
+      <md-button>Tooltip Bottom</md-button>
+    </md-tooltip>
+  </div>
+  <div class="row md-padding__vertical">
+    <md-tooltip message="Tooltip" placement="left">
+      <md-button>Tooltip Left</md-button>
+    </md-tooltip>
+  </div>
+  <div class="row md-padding__vertical">
+    <md-tooltip message="Tooltip" placement="right">
+      <md-button>Tooltip Right</md-button>
+    </md-tooltip>
+  </div>
+  <div class="row md-padding__vertical">
+    <md-tooltip placement="right">
+      <div slot="tooltip-content"><md-icon style="margin-right: .5rem" name="headset_12"></md-icon> Call</div>
+      <md-button>Tooltip Slot Content</md-button>
+    </md-tooltip>
+  </div>
+  <div class="row md-padding__vertical">
+    <tooltip-message-template-sandbox></tooltip-message-template-sandbox>
+  </div>
+  <div class="row md-padding__vertical">
+    <tooltip-slot-template-sandbox></tooltip-slot-template-sandbox>
   </div>
 `;

e738741b74b8f4ba586f8082693fd2db111af17c
Lena Vaganova (helenv) <helenv@cisco.com>
2021-01-19 19:17:06 +0200

    feat(components): add drag event

--- web-components/src/components/draggable-item/DraggableItem.ts
+++ web-components/src/components/draggable-item/DraggableItem.ts
@@ -14,6 +14,20 @@ export class DraggableItem extends LitElement {
     super.connectedCallback();
   }
 
+  dragStart(event: DragEvent) {
+    event.dataTransfer!.setData("text/html", "test");
+    this.setAttribute("dragging", "");
+    console.log(event)
+  }
+
+  dragOver() {
+    if (this.hasAttribute("dragging")) {
+      this.removeAttribute("over");
+    } else {
+      this.setAttribute("over", "");
+    }
+  }
+
   static get styles(): CSSResultArray {
     return [reset, styles];
   }
@@ -23,11 +37,21 @@ export class DraggableItem extends LitElement {
     return html`
       <div class="flex flex-col items-center justify-center py-16">
         <div id="list">
-            <div draggable=true class="draggable flex items-center justify-center">A</div>
-            <div draggable=true class="draggable flex items-center justify-center">B</div>
-            <div draggable=true class="draggable flex items-center justify-center">C</div>
-            <div draggable=true class="draggable flex items-center justify-center">D</div>
-            <div draggable=true class="draggable flex items-center justify-center">E</div>
+            <div
+              id="1"
+              draggable=true
+              @dragstart=${(event: DragEvent) => this.dragStart(event)} 
+              @dragover=${() => this.dragOver()} 
+              class="draggable flex items-center justify-center">A</div>
+            <div
+              id="2"
+              draggable=true
+              @dragstart=${(event: DragEvent) => this.dragStart(event)} 
+              class="draggable flex items-center justify-center">B</div>
+            <div
+              id="3" draggable=true @dragstart=${(event: DragEvent) => this.dragStart(event)} class="draggable flex items-center justify-center">C</div>
+            <div id="1" draggable=true @dragstart=${(event: DragEvent) => this.dragStart(event)} class="draggable flex items-center justify-center">D</div>
+            <div id="1" draggable=true @dragstart=${(event: DragEvent) => this.dragStart(event)} class="draggable flex items-center justify-center">E</div>
         </div>
     </div>
     `;

18cf654ab1d6041b29209d324bfa30032e67d640
Yana Harris <yanaha@cisco.com>
2021-02-01 14:16:57 -0800

    adding name attribute to input

--- web-components/src/[sandbox]/examples/form.ts
+++ web-components/src/[sandbox]/examples/form.ts
@@ -5,23 +5,24 @@ import { html } from "lit-element";
 export const formTemplate = html`
   <h3>Default</h3>
   <md-form is-valid>
-    <md-input label="Name"></md-input>
-    <md-input label="Email"></md-input>
-    <md-input label="Count"></md-input>
+    <md-input label="Name" name="name"></md-input>
+    <md-input label="Address" name="address"></md-input>
+    <md-input label="Email" name="email"></md-input>
+    <md-input label="Phone" name="phone"></md-input>
     <md-button type="submit" label="Please Submit Form" size="52" color="blue">Submit</md-button>
   </md-form>
   <h3>Target</h3>
   <md-form target="_blank" is-valid>
-    <md-input></md-input>
+    <md-input name="name"></md-input>
   </md-form>
   <h3>Not Valid</h3>
   <md-form>
-    <md-input></md-input>
+    <md-input name="name"></md-input>
     <button type="submit">Submit</button>
   </md-form>
   <h3>No Validation</h3>
   <md-form no-validate>
-    <md-input></md-input>
+    <md-input name="name"></md-input>
     <md-button type="submit" color="green">Submit</md-button>
   </md-form>
 `;

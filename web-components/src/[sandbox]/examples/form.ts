3bc99d3689a545c3098b40c9ee45b6159b8e61dd
boiarchukk <bk1992@cisco.com>
2021-02-01 16:14:47 +0200

    docs(form): update form sandbox examples

--- web-components/src/[sandbox]/examples/form.ts
+++ web-components/src/[sandbox]/examples/form.ts
@@ -8,7 +8,7 @@ export const formTemplate = html`
     <md-input label="Name"></md-input>
     <md-input label="Email"></md-input>
     <md-input label="Count"></md-input>
-    <md-button type="submit" label="Please Submit Form" size="52" color="blue">Click</md-button>
+    <md-button type="submit" label="Please Submit Form" size="52" color="blue">Submit</md-button>
   </md-form>
   <h3>Target</h3>
   <md-form target="_blank" is-valid>
@@ -17,9 +17,11 @@ export const formTemplate = html`
   <h3>Not Valid</h3>
   <md-form>
     <md-input></md-input>
+    <button type="submit">Submit</button>
   </md-form>
   <h3>No Validation</h3>
   <md-form no-validate>
     <md-input></md-input>
+    <md-button type="submit" color="green">Submit</md-button>
   </md-form>
 `;

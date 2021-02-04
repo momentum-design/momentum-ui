7e84344ea390bb745610696c28d7996a095eff29
Roman Manchenko (manro) <manro@cisco.com>
2021-02-04 17:21:41 +0200

    feat(dropdown): create dropdown component

--- web-components/src/components/dropdown/Dropdown.ts
+++ web-components/src/components/dropdown/Dropdown.ts
@@ -2,6 +2,7 @@ import { FocusMixin } from "@/mixins";
 import reset from "@/wc_scss/reset.scss";
 import { customElement, internalProperty, LitElement, property, PropertyValues } from "lit-element";
 import { html } from "lit-html";
+import { classMap } from "lit-html/directives/class-map.js";
 import { repeat } from "lit-html/directives/repeat.js";
 import { styleMap } from "lit-html/directives/style-map";
 import styles from "./scss/module.scss";
@@ -13,9 +14,24 @@ type RenderOptionMember = { key: string; value: string };
 export class Dropdown extends FocusMixin(LitElement) {
   @property({ type: String }) label = "Select...";
   @property({ type: Array }) options: (string | OptionMember)[] = [];
+  @property({ type: String, attribute: "option-id", reflect: true }) optionId = "";
+  @property({ type: String, attribute: "option-value", reflect: true }) optionValue = "";
 
   @internalProperty() renderOptions: RenderOptionMember[] = [];
   @internalProperty() isExpanded = false;
+  @internalProperty() isDisabled = false;
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.setupEvents();
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    this.teardownEvents();
+  }
 
   protected updated(changedProperties: PropertyValues) {
     super.updated(changedProperties);
@@ -24,9 +40,9 @@ export class Dropdown extends FocusMixin(LitElement) {
       if (name === "options") {
         const existingKeys: string[] = [];
 
-        this.renderOptions = this.options.reduce((acc, opt) => {
-          const key = typeof opt === "string" ? opt.trim() : Object.keys(opt as OptionMember)[0];
-          const value = typeof opt === "string" ? opt : Object.values(opt as OptionMember)[0];
+        this.renderOptions = this.options.reduce((acc, option) => {
+          const { key, value } = this.getOptionKeyValuePair(option);
+
           if (existingKeys.indexOf(key) !== -1) {
             console.error(`Dropdown already have option key: "${key}". Ignoring `);
           } else {
@@ -43,9 +59,70 @@ export class Dropdown extends FocusMixin(LitElement) {
     return [reset, styles];
   }
 
+  setupEvents() {
+    document.addEventListener("click", this.handleOutsideClick);
+  }
+
+  teardownEvents() {
+    document.removeEventListener("click", this.handleOutsideClick);
+  }
+
+  expand() {
+    this.isExpanded = true;
+  }
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  handleOutsideClick = (e: MouseEvent) => {
+    let insideSelfClick = false;
+    const path = e.composedPath();
+    if (path.length) {
+      insideSelfClick = !!path.find(element => element === this);
+      if (!insideSelfClick) {
+        this.collapse();
+      }
+    }
+  };
+
+  onLabelClick() {
+    this.toggle();
+  }
+
+  getOptionKeyValuePair(option: string | OptionMember) {
+    if (typeof option === "string") {
+      return { key: option.trim(), value: option };
+    }
+
+    if (option && typeof option === "object") {
+      const optionKeys = Object.keys(option as OptionMember);
+      const optionValues = Object.values(option as OptionMember);
+
+      if (optionKeys.length) {
+        if (optionKeys.length === 1 || !this.optionId) {
+          return { key: optionKeys[0], value: optionValues[0] };
+        } else if (this.optionId) {
+          return { key: option[this.optionId], value: option[this.optionValue || this.optionId] };
+        }
+      }
+    }
+
+    return { key: "undefined", value: "undefined" };
+  }
+
   render() {
     return html`
-      <div class="md-dropdown">
+      <div
+        class="${classMap({
+          "md-dropdown": true,
+          "md-dropdown__expanded": this.isExpanded
+        })}"
+      >
         <select class="md-dropdown-select">
           ${repeat(
             this.renderOptions,
@@ -55,15 +132,22 @@ export class Dropdown extends FocusMixin(LitElement) {
             `
           )}
         </select>
-        <label class="md-dropdown-label" @click="${() => console.log("CLICK")}">
-          ${this.label}
+        <label class="md-dropdown-label" @click="${() => this.onLabelClick()}">
+          <span class="md-dropdown-label--text">${this.label}</span>
+          <span class="md-dropdown-label--icon">
+            <md-icon name="icon-arrow-down_16"></md-icon>
+          </span>
         </label>
         <ul class="md-dropdown-list" style="${styleMap({ display: this.isExpanded ? "block" : "none" })}">
           ${repeat(
             this.renderOptions,
             o => o.key,
             o => html`
-              <li class="md-dropdown-option">${o.value}</li>
+              <li class="md-dropdown-option" role="option" aria-label="${o.value}">
+                <span class="select-label">
+                  <span>${o.value}</span>
+                </span>
+              </li>
             `
           )}
         </ul>

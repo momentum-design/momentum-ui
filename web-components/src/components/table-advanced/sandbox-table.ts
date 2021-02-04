b742e685124d139647b923df395ef1b38af03384
Lena Vaganova (helenv) <helenv@cisco.com>
2021-02-04 16:12:49 +0200

    feat(table): update sticky header styles

--- web-components/src/components/table-advanced/sandbox-table.ts
+++ web-components/src/components/table-advanced/sandbox-table.ts
@@ -10,7 +10,10 @@ export class Sandbox extends LitElement {
       <!-- <md-table-advanced .config=${TableMock.CONF} .data=${TableMock.DATA}></md-table-advanced> -->
       
       <h3>Advanced Table Complex</h3>
-      <md-table-advanced .config=${TableMock.COMPLEX.config} .data=${TableMock.COMPLEX.data}></md-table-advanced>
+      <div style="height: 400px; width: 100%;">
+        <md-table-advanced .config=${TableMock.COMPLEX.config} .data=${TableMock.COMPLEX.data}></md-table-advanced>
+      </div>
+      
     `;
   }
 }

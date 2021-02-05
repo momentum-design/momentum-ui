2d160b5f87d182255dc6151200abf3fc8a302935
boiarchukk <bk1992@cisco.com>
2021-02-05 17:32:24 +0200

    refactor(draggable): refactor draggable options

--- web-components/src/components/draggable/Draggable.ts
+++ web-components/src/components/draggable/Draggable.ts
@@ -1,4 +1,4 @@
-import { customElement, html, LitElement, property } from "lit-element";
+import { customElement, html, LitElement, property, query } from "lit-element";
 import Sortable from "sortablejs";
 import reset from "@/wc_scss/reset.scss";
 import { debounce } from "@/utils/helpers";
@@ -7,18 +7,21 @@ import { SlottedMixin } from "@/mixins";
 
 @customElement("md-draggable")
 export class Draggable extends SlottedMixin(LitElement) {
-  @property({ type: Boolean, reflect: true }) disabled = false;
   @property({ type: Number }) delay = 0;
   @property({ type: Number }) animation = 0;
   @property({ type: Boolean }) sort = false;
   @property({ type: String }) handle = "";
   @property({ type: String }) filter = "";
   @property({ type: String }) easing = "";
+  @property({ type: Boolean, reflect: true }) disabled = false;
+  @property({ type: String }) direction: "horizontal" | "vertical" = "vertical";
   @property({ type: Object }) group: Sortable.GroupOptions = { name: "group" };
+  @property({ type: Boolean, attribute: "custom-placeholder" }) customPlaceholder = false;
   @property({ type: String, attribute: "draggable-items" }) draggableItems = "";
   @property({ type: String, attribute: "ghost-class" }) ghostClass = "";
   @property({ type: String, attribute: "chosen-class" }) chosenClass = "";
-  @property({ type: String }) direction: "horizontal" | "vertical" = "vertical";
+
+  @query(".md-draggable-container") draggableContainer!: HTMLDivElement;
 
   private sortableInstance: Sortable | null = null;
 
@@ -26,60 +29,40 @@ export class Draggable extends SlottedMixin(LitElement) {
     return [reset, styles];
   }
 
-  get draggableContainer() {
-    return this.slotted[0];
+  get container() {
+    return this.customPlaceholder ? this.draggableContainer : this.slotted[0];
+  }
+
+  private generateOptions() {
+    return {
+      group: this.group,
+      disabled: this.disabled,
+      animation: this.animation,
+      delay: this.delay,
+      handle: this.handle,
+      easing: this.easing,
+      filter: this.filter,
+      draggable: this.draggableItems,
+      direction: this.direction,
+      ghostClass: this.ghostClass,
+      chosenClass: this.chosenClass,
+      onStart: this.handleOnStart,
+      onMove: this.handleOnMove,
+      onEnd: this.handleOnEnd,
+      onChoose: this.handleOnChoose,
+      onAdd: this.handleOnAdd,
+      onRemove: this.handleOnRemove,
+      onUnchoose: this.handleOnUnchoose,
+      onClone: this.handleOnClone,
+      onChange: this.handleOnChange
+    };
   }
 
   private initializeSortable() {
-    const {
-      slotted,
-      disabled,
-      animation,
-      delay,
-      handle,
-      easing,
-      filter,
-      draggableItems,
-      direction,
-      ghostClass,
-      chosenClass,
-      group,
-      draggableContainer,
-      handleOnStart,
-      handleOnMove,
-      handleOnEnd,
-      handleOnChoose,
-      handleOnAdd,
-      handleOnRemove,
-      handleOnUnchoose,
-      handleOnClone,
-      handleOnChange
-    } = this;
-
-    if (slotted.length) {
-      this.sortableInstance = Sortable.create(draggableContainer, {
-        group,
-        disabled,
-        animation,
-        delay,
-        handle,
-        easing,
-        filter,
-        draggable: draggableItems,
-        direction,
-        ghostClass,
-        chosenClass,
-        onStart: handleOnStart,
-        onMove: handleOnMove,
-        onEnd: handleOnEnd,
-        onChoose: handleOnChoose,
-        onAdd: handleOnAdd,
-        onRemove: handleOnRemove,
-        onUnchoose: handleOnUnchoose,
-        onClone: handleOnClone,
-        onChange: handleOnChange
-      });
+    if (this.sortableInstance) {
+      this.cleanupSortable();
     }
+    this.sortableInstance = Sortable.create(this.container, this.generateOptions());
   }
 
   private dispatchDragEvent(eventName: string, srcEvent: Sortable.SortableEvent | Sortable.MoveEvent) {
@@ -95,38 +78,47 @@ export class Draggable extends SlottedMixin(LitElement) {
   }
 
   private handleOnChange = (event: Sortable.SortableEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-change", event);
   };
 
   private handleOnClone = (event: Sortable.SortableEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-clone", event);
   };
 
   private handleOnUnchoose = (event: Sortable.SortableEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-unchoose", event);
   };
 
   private handleOnRemove = (event: Sortable.SortableEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-remove", event);
   };
 
   private handleOnAdd = (event: Sortable.SortableEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-add", event);
   };
 
   private handleOnChoose = (event: Sortable.SortableEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-choose", event);
   };
 
   private handleOnEnd = (event: Sortable.SortableEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-end", event);
   };
 
-  private handleOnMove = debounce((event: Sortable.MoveEvent, srcEvent: Event) => {
+  private handleOnMove = debounce((event: Sortable.MoveEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-move", event);
   }, 100);
 
   private handleOnStart = (event: Sortable.SortableEvent) => {
+    event.stopPropagation();
     this.dispatchDragEvent("drag-start", event);
   };
 
@@ -148,7 +140,7 @@ export class Draggable extends SlottedMixin(LitElement) {
 
   render() {
     return html`
-      <div class="md-draggable-wrapper" part="draggable-wrapper">
+      <div class="md-draggable-container" part="draggable-container">
         <slot></slot>
       </div>
     `;

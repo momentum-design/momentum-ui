56ae5bb17bf1de125ab4b6895da04864c313bfab
Roman Manchenko (manro) <manro@cisco.com>
2021-02-08 19:07:25 +0200

    feat(dropdown): create dropdown component

--- web-components/src/[sandbox]/examples/dropdown.ts
+++ web-components/src/[sandbox]/examples/dropdown.ts
@@ -1,13 +1,111 @@
+import { DropdownEvents } from "@/components/dropdown/Dropdown";
 import { html } from "lit-element";
 
-const stringOptions = ["one", "two", "three"];
+const stringOptions = ["one", "two", "three", "   non-trimmed "];
 
-const objectOptions = [{ one: "One" }, { two: "Two" }, { three: "Three" }];
+const stringLongOptions = [
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+  "29",
+  "30",
+  "31",
+  "32",
+  "33",
+  "34",
+  "35",
+  "36",
+  "37",
+  "38",
+  "39",
+  "40"
+];
+
+const objectKeyValueOptions = [{ one: "One" }, { two: "Two" }, { three: "Three" }];
+
+const objectOptions = [
+  {
+    user_id: "xxx-111",
+    user_name: "jd",
+    full_name: "John Doe"
+  },
+  {
+    user_id: "xxx-222",
+    user_name: "as",
+    full_name: "Adam Smith"
+  },
+  {
+    user_id: "xxx-333",
+    user_name: "ba",
+    full_name: "Boris Anderson"
+  }
+];
 
 export const dropdownTemplate = html`
-  <h3>Default</h3>
-  <md-dropdown .options="${stringOptions}"></md-dropdown>
-
-  <h3>Default Objects</h3>
-  <md-dropdown .options="${objectOptions}"></md-dropdown>
+  <h3>Strings Options</h3>
+  <md-dropdown
+    .options="${stringOptions}"
+    @dropdown-selected="${(e: CustomEvent<DropdownEvents["dropdown-selected"]>) => {
+      console.log("[Strings Options] Selected: ", e.detail);
+    }}"
+  ></md-dropdown>
+  <h3>Strings Options (Disabled)</h3>
+  <md-dropdown .options="${stringOptions}" disabled></md-dropdown>
+  <h3>Strings Options (Allow unselected)</h3>
+  <md-dropdown
+    .options="${stringOptions}"
+    allow-unselected
+    @dropdown-selected="${(e: CustomEvent<DropdownEvents["dropdown-selected"]>) => {
+      console.log("[Strings Options (Allow unselected)] Selected: ", e.detail);
+    }}"
+  ></md-dropdown>
+  <h3>Strings Long Options (Preselected)</h3>
+  <md-dropdown
+    .options="${stringLongOptions}"
+    .defaultOption="${stringLongOptions[29]}"
+    @dropdown-selected="${(e: CustomEvent<DropdownEvents["dropdown-selected"]>) => {
+      console.log("[Strings Long Options (Preselected)] Selected: ", e.detail);
+    }}"
+  ></md-dropdown>
+  <h3>Single KeyValue Objects Options</h3>
+  <md-dropdown
+    .options="${objectKeyValueOptions}"
+    @dropdown-selected="${(e: CustomEvent<DropdownEvents["dropdown-selected"]>) => {
+      console.log("[Single KeyValue Objects Options] Selected: ", e.detail);
+    }}"
+  ></md-dropdown>
+  <h3>Multi KeyValue Objects Options</h3>
+  <md-dropdown
+    .options="${objectOptions}"
+    option-id="user_id"
+    option-value="full_name"
+    @dropdown-selected="${(e: CustomEvent<DropdownEvents["dropdown-selected"]>) => {
+      console.log("[Multi KeyValue Objects Options] Selected: ", e.detail);
+    }}"
+  ></md-dropdown>
 `;
--- web-components/src/components/dropdown/Dropdown.ts
+++ web-components/src/components/dropdown/Dropdown.ts
@@ -1,32 +1,49 @@
 import { Key } from "@/constants";
 import { FocusMixin } from "@/mixins";
 import reset from "@/wc_scss/reset.scss";
-import { customElement, internalProperty, LitElement, property, PropertyValues, query } from "lit-element";
+import { customElement, internalProperty, LitElement, property, PropertyValues, query, queryAll } from "lit-element";
 import { html } from "lit-html";
 import { classMap } from "lit-html/directives/class-map.js";
 import { repeat } from "lit-html/directives/repeat.js";
 import styles from "./scss/module.scss";
 
+const EMPTY_KEY = "";
+
 type OptionMember = { [key: string]: string };
-type RenderOptionMember = { key: string; value: string };
+type Option = string | OptionMember;
+type RenderOptionMember = { key: string; value: string; option?: Option };
+
+export type DropdownEvents = {
+  "dropdown-focus-in": undefined;
+  "dropdown-focus-out": undefined;
+  "dropdown-selected": {
+    option: Option;
+  };
+};
 
 @customElement("md-dropdown")
 export class Dropdown extends FocusMixin(LitElement) {
   @property({ type: String }) title = "Select...";
-  @property({ type: Array }) options: (string | OptionMember)[] = [];
-  @property({ type: String }) optionId = "";
-  @property({ type: String }) optionValue = "";
-  @property({ type: Object }) defaultOption: string | OptionMember = "";
+  @property({ type: Array }) options: Option[] = [];
+  @property({ type: String, attribute: "option-id" }) optionId = "";
+  @property({ type: String, attribute: "option-value" }) optionValue = "";
+  @property({ type: Object }) defaultOption: Option = EMPTY_KEY;
 
   @property({ type: Boolean, reflect: true }) disabled = false;
+  @property({ type: Boolean, attribute: "allow-unselected", reflect: true }) allowUnselected = false;
+  @property({ type: Number, attribute: "visible-option", reflect: true }) visibleOptions = 8;
 
   @internalProperty() private renderOptions: RenderOptionMember[] = [];
-  @internalProperty() private selectedKey?: string;
+  @internalProperty() private selectedKey: string = EMPTY_KEY;
 
   @internalProperty() private expanded = false;
   @internalProperty() private focusedIndex = -1;
 
   @query("label") label!: HTMLLabelElement;
+  @query("ul.md-dropdown-list") optionsList!: HTMLUListElement;
+  @queryAll("li.md-dropdown-option") optionsListItems?: HTMLLIElement[];
+
+  private lastMaxHeight = "";
 
   connectedCallback() {
     super.connectedCallback();
@@ -65,8 +82,16 @@ export class Dropdown extends FocusMixin(LitElement) {
       if (name === "selectedKey") {
         const idx = this.renderOptions.findIndex(o => o.key === this.selectedKey);
         if (idx !== -1) {
-          this.focusTo(idx);
+          this.focusToIndex(idx);
         }
+      }
+      if (name === "expanded") {
+        if (this.expanded) {
+          this.updateListDOM();
+        }
+      }
+      if (name === "focusedIndex") {
+        this.updateListDOM();
       }
     });
   }
@@ -76,30 +101,69 @@ export class Dropdown extends FocusMixin(LitElement) {
 
     this.focusReset();
 
-    this.renderOptions = this.options.reduce((acc, option) => {
+    const renderOptions = this.options.reduce((acc, option) => {
       const { key, value } = this.getOptionKeyValuePair(option);
 
       if (existingKeys.indexOf(key) !== -1) {
         console.error(`Dropdown already have option key: "${key}". Ignoring `);
+      } else if (!key) {
+        console.error(`Dropdown key is not defined: "${key}". (value: "${value}"). Ignoring `);
       } else {
         existingKeys.push(key);
-        acc.push({ key, value });
+        acc.push({ key, value, option });
       }
       return acc;
     }, [] as RenderOptionMember[]);
+
+    if (this.allowUnselected) {
+      renderOptions.unshift({
+        key: EMPTY_KEY,
+        value: this.title // || "" ?
+      });
+    }
+
+    this.renderOptions = renderOptions;
+  }
+
+  async updateListDOM() {
+    await this.resizeDropdownList();
+    await this.scrollToIndex(this.focusedIndex);
+  }
+
+  async resizeDropdownList() {
+    await new Promise(resolve => {
+      requestAnimationFrame(() => {
+        if (this.optionsListItems) {
+          if (this.optionsListItems.length > this.visibleOptions) {
+            const maxHeight = [...this.optionsListItems]
+              .slice(0, this.visibleOptions)
+              .reduce((acc, option) => acc + option.offsetHeight, 0);
+
+            const nextMaxHeight = `${maxHeight}px`;
+            if (this.lastMaxHeight !== nextMaxHeight) {
+              this.optionsList.style.maxHeight = nextMaxHeight;
+              this.lastMaxHeight = nextMaxHeight;
+            }
+          } else {
+            const nextMaxHeight = `auto`;
+            if (this.lastMaxHeight !== nextMaxHeight) {
+              this.optionsList.style.maxHeight = nextMaxHeight;
+              this.lastMaxHeight = nextMaxHeight;
+            }
+          }
+        }
+        resolve();
+      });
+    });
   }
 
   protected handleFocusIn(event: Event) {
     if (!this.disabled) {
-      requestAnimationFrame(() => {
-        this.label.focus();
-        console.log("this.label.focus()");
-      });
-
       super.handleFocusIn && super.handleFocusIn(event);
     }
+
     this.dispatchEvent(
-      new CustomEvent("dropdown-focus-in", {
+      new CustomEvent<DropdownEvents["dropdown-focus-in"]>("dropdown-focus-in", {
         composed: true,
         bubbles: true
       })
@@ -108,8 +172,9 @@ export class Dropdown extends FocusMixin(LitElement) {
 
   protected handleFocusOut(event: Event) {
     super.handleFocusOut && super.handleFocusOut(event);
+
     this.dispatchEvent(
-      new CustomEvent("dropdown-focus-out", {
+      new CustomEvent<DropdownEvents["dropdown-focus-out"]>("dropdown-focus-out", {
         composed: true,
         bubbles: true
       })
@@ -150,7 +215,24 @@ export class Dropdown extends FocusMixin(LitElement) {
 
   select() {
     if (this.focusedIndex !== -1) {
-      this.selectedKey = this.renderOptions[this.focusedIndex].key;
+      const renderOption = this.renderOptions[this.focusedIndex];
+
+      const nextSelectedKey = renderOption.key;
+      const prevSelectedKey = this.selectedKey;
+
+      if (nextSelectedKey !== prevSelectedKey) {
+        this.selectedKey = nextSelectedKey;
+
+        this.dispatchEvent(
+          new CustomEvent<DropdownEvents["dropdown-selected"]>("dropdown-selected", {
+            composed: true,
+            bubbles: true,
+            detail: {
+              option: renderOption.option ? renderOption.option : renderOption.key
+            }
+          })
+        );
+      }
     }
   }
 
@@ -165,10 +247,6 @@ export class Dropdown extends FocusMixin(LitElement) {
     }
   };
 
-  onLabelClick() {
-    this.toggle();
-  }
-
   onKeyDown = (e: KeyboardEvent) => {
     switch (e.code) {
       case Key.Enter: {
@@ -182,17 +260,53 @@ export class Dropdown extends FocusMixin(LitElement) {
         break;
       }
       case Key.ArrowDown: {
-        this.focusNext();
+        if (!this.expanded) {
+          this.expand();
+        } else {
+          this.focusNext();
+        }
         break;
       }
       case Key.ArrowUp: {
-        this.focusPrev();
+        if (!this.expanded) {
+          this.expand();
+        } else {
+          this.focusPrev();
+        }
+
+        break;
+      }
+      case Key.Home: {
+        if (!this.expanded) {
+          this.expand();
+        } else {
+          this.focusFirst();
+        }
+        break;
+      }
+      case Key.End: {
+        if (!this.expanded) {
+          this.expand();
+        } else {
+          this.focusLast();
+        }
+        break;
+      }
+      case Key.Escape: {
+        if (this.expanded) {
+          this.collapse();
+        }
         break;
       }
       default: {
+        //
       }
     }
   };
+
+  onLabelClick() {
+    this.toggle();
+  }
 
   focusFirst() {
     if (this.renderOptions.length) {
@@ -226,7 +340,7 @@ export class Dropdown extends FocusMixin(LitElement) {
     }
   }
 
-  focusTo(n: number) {
+  focusToIndex(n: number) {
     if (this.renderOptions.length) {
       if (n >= 0 && n <= this.renderOptions.length - 1) {
         this.focusedIndex = n;
@@ -238,7 +352,40 @@ export class Dropdown extends FocusMixin(LitElement) {
     this.focusedIndex = -1;
   }
 
-  getOptionKeyValuePair(option: string | OptionMember) {
+  async scrollToIndex(n: number) {
+    await new Promise(resolve => {
+      requestAnimationFrame(() => {
+        if (
+          this.optionsListItems &&
+          this.optionsListItems.length > this.visibleOptions &&
+          n >= 0 &&
+          this.optionsListItems.length > n
+        ) {
+          let distance = 0;
+
+          const { top, bottom } = this.optionsList.getBoundingClientRect();
+          const option = this.optionsListItems[n];
+
+          const nextOption = this.optionsListItems[n + 1] || option;
+          const prevOption = this.optionsListItems[n - 1] || option;
+
+          const nextOptionRect = nextOption.getBoundingClientRect();
+          const prevOptionRect = prevOption.getBoundingClientRect();
+
+          if (nextOptionRect.bottom > bottom) {
+            distance = nextOptionRect.bottom - bottom + 2;
+          } else if (prevOptionRect.top < top) {
+            distance = prevOptionRect.top - top - 2;
+          }
+
+          this.optionsList.scrollTop += distance;
+        }
+        resolve();
+      });
+    });
+  }
+
+  getOptionKeyValuePair(option: Option) {
     if (typeof option === "string") {
       return { key: option.trim(), value: option };
     }
@@ -259,6 +406,16 @@ export class Dropdown extends FocusMixin(LitElement) {
     return { key: "undefined", value: "undefined" };
   }
 
+  get labelTitle() {
+    if (this.selectedKey) {
+      const option = this.renderOptions.find(o => o.key === this.selectedKey);
+      if (option) {
+        return option.value;
+      }
+    }
+    return this.title;
+  }
+
   render() {
     return html`
       <div
@@ -267,17 +424,17 @@ export class Dropdown extends FocusMixin(LitElement) {
           "md-dropdown__expanded": this.expanded
         })}"
       >
-        <select class="md-dropdown-select" ?disabled="${this.disabled}">
+        <!-- <select class="md-dropdown-select" ?disabled="${this.disabled}">
           ${repeat(
-            this.renderOptions,
-            o => o.key,
-            o => html`
-              <option value="${o.key}">${o.value}</option>
-            `
-          )}
-        </select>
+          this.renderOptions,
+          o => o.key,
+          o => html`
+            <option value="${o.key}" ?selected="${o.key === this.selectedKey}">${o.value}</option>
+          `
+        )}
+        </select> -->
         <label class="md-dropdown-label" ?disabled="${this.disabled}" @click="${() => this.onLabelClick()}">
-          <span class="md-dropdown-label--text">${this.title}</span>
+          <span class="md-dropdown-label--text">${this.labelTitle}</span>
           <span class="md-dropdown-label--icon">
             <md-icon name="icon-arrow-down_16"></md-icon>
           </span>
@@ -292,6 +449,11 @@ export class Dropdown extends FocusMixin(LitElement) {
                 role="option"
                 aria-label="${o.value}"
                 ?focused="${idx === this.focusedIndex}"
+                @click="${() => {
+                  this.focusToIndex(idx);
+                  this.select();
+                  this.collapse();
+                }}"
               >
                 <span class="select-label">
                   <span>${o.value}</span>

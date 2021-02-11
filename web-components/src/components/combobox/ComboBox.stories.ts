336bb09170cf04e81d0092dcbd09ca5af85b59c2
Kostya Boiarchuk <73885142+boiarchukk@users.noreply.github.com>
2021-02-11 04:50:26 +0200

    refactor(form): update form (#779)
    * docs(*): fix storybook and sandbox examples
    
    * build(webpack): add new component to webpack build process
    
    * docs(form): update form component examples
    
    * feat(form): dispatch event in case when form submitted
    
    * refactor(mixin): include new mixin in mixins import scope
    
    * test(form): update test covarage
    
    * refactor(form): use new decorator to register component

--- web-components/src/components/combobox/ComboBox.stories.ts
+++ web-components/src/components/combobox/ComboBox.stories.ts
@@ -32,7 +32,7 @@ export const Combobox = () => {
   const disabled = boolean("Disabled", false);
   const trimSpace = boolean("Trim white space", false);
   const init = boolean("InitialValue", false);
-  const customValue = boolean("Cuatom Value", false);
+  const customValue = boolean("Custom Value", false);
   const multi = boolean("Multi", false);
   const multiInit = boolean("Multi InitialValue", false);
   const object = boolean("Object Data", false);
@@ -42,94 +42,126 @@ export const Combobox = () => {
 
   if (init) {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-combobox placeholder=${placeholder} .options=${comboBoxOptions} ?disabled=${disabled} ?search-trim-space=${trimSpace} .value=${[comboBoxOptions[3]]}></md-combobox>
-    </md-theme>
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox
+          placeholder=${placeholder}
+          .options=${comboBoxOptions}
+          ?disabled=${disabled}
+          ?search-trim-space=${trimSpace}
+          .value=${[comboBoxOptions[3]]}
+        ></md-combobox>
+      </md-theme>
     `;
   } else if (multi) {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-combobox placeholder=${placeholder} .options=${comboBoxOptions} is-multi></md-combobox>
-    </md-theme>`;
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox placeholder=${placeholder} .options=${comboBoxOptions} is-multi></md-combobox>
+      </md-theme>
+    `;
   } else if (customValue) {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}> 
-      <md-combobox placeholder=${placeholder} .options=${comboBoxOptions} .is-multi=${multi} allow-custom-value></md-combobox>
-    </md-theme>`;
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox
+          placeholder=${placeholder}
+          .options=${comboBoxOptions}
+          .is-multi=${multi}
+          allow-custom-value
+        ></md-combobox>
+      </md-theme>
+    `;
   } else if (multiInit) {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-combobox placeholder=${placeholder} .options=${comboBoxOptions} is-multi .value=${[comboBoxOptions[3], comboBoxOptions[5]]}></md-combobox>
-    </md-theme>
-  `;
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox
+          placeholder=${placeholder}
+          .options=${comboBoxOptions}
+          is-multi
+          .value=${[comboBoxOptions[3], comboBoxOptions[5]]}
+        ></md-combobox>
+      </md-theme>
+    `;
   } else if (object) {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-combobox placeholder=${placeholder} .options=${comboBoxObjectOptions} option-id="id" option-value="country" .is-multi=${multi}></md-combobox>
-    </md-theme>`;
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox
+          placeholder=${placeholder}
+          .options=${comboBoxObjectOptions}
+          option-id="id"
+          option-value="country"
+          .is-multi=${multi}
+        ></md-combobox>
+      </md-theme>
+    `;
   } else if (objectInit) {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-combobox
-        placeholder=${placeholder}
-        .options=${comboBoxObjectOptions}
-        option-id="id"
-        option-value="country"
-        .is-multi=${multi}
-        .value=${[comboBoxObjectOptions[4]]}
-        ?search-trim-space=${trimSpace}
-      ></md-combobox>
-    </md-theme>
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox
+          placeholder=${placeholder}
+          .options=${comboBoxObjectOptions}
+          option-id="id"
+          option-value="country"
+          .is-multi=${multi}
+          .value=${[comboBoxObjectOptions[4]]}
+          ?search-trim-space=${trimSpace}
+        ></md-combobox>
+      </md-theme>
     `;
   } else if (customContent) {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-combobox with-custom-content .is-multi=${multi}>
-        <div slot="one" aria-label="Facebook" display-value="Facebook">
-          <span>Facebook</span>
-          <md-icon name="icon-facebook_16"></md-icon>
-        </div>
-        <div slot="two" aria-label="Twitter" display-value="Twitter">
-          <span class="company-value">Twitter</span>
-          <md-icon name="icon-twitter_16"></md-icon>
-        </div>
-        <div slot="three" aria-label="Wikipedia" display-value="Wikipedia">
-          <span class="company-value">Wikipedia</span>
-          <md-icon name="icon-wikipedia_16"></md-icon>
-        </div>
-        <div slot="four" aria-label="Google" display-value="Google">
-          <span class="company-value">Google</span>
-          <md-icon name="icon-google_16"></md-icon>
-        </div>
-      </md-combobox>
-    </md-theme>`;
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox with-custom-content .is-multi=${multi}>
+          <div slot="one" aria-label="Facebook" display-value="Facebook">
+            <span>Facebook</span>
+            <md-icon name="icon-facebook_16"></md-icon>
+          </div>
+          <div slot="two" aria-label="Twitter" display-value="Twitter">
+            <span class="company-value">Twitter</span>
+            <md-icon name="icon-twitter_16"></md-icon>
+          </div>
+          <div slot="three" aria-label="Wikipedia" display-value="Wikipedia">
+            <span class="company-value">Wikipedia</span>
+            <md-icon name="icon-wikipedia_16"></md-icon>
+          </div>
+          <div slot="four" aria-label="Google" display-value="Google">
+            <span class="company-value">Google</span>
+            <md-icon name="icon-google_16"></md-icon>
+          </div>
+        </md-combobox>
+      </md-theme>
+    `;
   } else if (customContentInit) {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-combobox with-custom-content is-multi .value=${[{ id: "Wikipedia", value: "Wikipedia" }]}>
-        <div slot="one" aria-label="Facebook" display-value="Facebook">
-          <span>Facebook</span>
-          <md-icon name="icon-facebook_16"></md-icon>
-        </div>
-        <div slot="two" aria-label="Twitter" display-value="Twitter">
-          <span class="company-value">Twitter</span>
-          <md-icon name="icon-twitter_16"></md-icon>
-        </div>
-        <div slot="three" aria-label="Wikipedia" display-value="Wikipedia">
-          <span class="company-value">Wikipedia</span>
-          <md-icon name="icon-wikipedia_16"></md-icon>
-        </div>
-        <div slot="four" aria-label="Google" display-value="Google">
-          <span class="company-value">Google</span>
-          <md-icon name="icon-google_16"></md-icon>
-        </div>
-      </md-combobox>
-  </md-theme>`;
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox with-custom-content is-multi .value=${[{ id: "Wikipedia", value: "Wikipedia" }]}>
+          <div slot="one" aria-label="Facebook" display-value="Facebook">
+            <span>Facebook</span>
+            <md-icon name="icon-facebook_16"></md-icon>
+          </div>
+          <div slot="two" aria-label="Twitter" display-value="Twitter">
+            <span class="company-value">Twitter</span>
+            <md-icon name="icon-twitter_16"></md-icon>
+          </div>
+          <div slot="three" aria-label="Wikipedia" display-value="Wikipedia">
+            <span class="company-value">Wikipedia</span>
+            <md-icon name="icon-wikipedia_16"></md-icon>
+          </div>
+          <div slot="four" aria-label="Google" display-value="Google">
+            <span class="company-value">Google</span>
+            <md-icon name="icon-google_16"></md-icon>
+          </div>
+        </md-combobox>
+      </md-theme>
+    `;
   } else {
     return html`
-    <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
-      <md-combobox .options=${comboBoxOptions} placeholder=${placeholder} ?disabled=${disabled} ?search-trim-space=${trimSpace}></md-combobox>
-    </md-theme>`;
+      <md-theme class="theme-toggle" id="combobox" ?darkTheme=${darkTheme} ?lumos=${lumos}>
+        <md-combobox
+          .options=${comboBoxOptions}
+          placeholder=${placeholder}
+          ?disabled=${disabled}
+          ?search-trim-space=${trimSpace}
+        ></md-combobox>
+      </md-theme>
+    `;
   }
-
 };

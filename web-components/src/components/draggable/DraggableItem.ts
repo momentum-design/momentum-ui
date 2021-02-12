6cb0c2939b9fc82acbfd56e0d9c36de453065dc0
boiarchukk <bk1992@cisco.com>
2021-02-12 19:05:47 +0200

    refactor(*): finish basic functionality

--- web-components/src/components/draggable/DraggableItem.ts
+++ web-components/src/components/draggable/DraggableItem.ts
@@ -8,51 +8,40 @@ import { classMap } from "lit-html/directives/class-map";
 
 export namespace DraggableItem {
   @customElementWithCheck("md-draggable-item")
-  export class ELEMENT extends FocusMixin(LitElement) { 
-    @property({ type: Boolean }) row = false;
+  export class ELEMENT extends FocusMixin(LitElement) {
     @property({ type: Boolean, reflect: true }) disabled = false;
-    private _edit = false;
-    @property({ type: Boolean, reflect: true })
-    get edit() {
-      return this._edit;
-    }
-    set edit(value: boolean) {
-      const oldValue = this._edit;
-      this._edit = value;
-      this.requestUpdate("edit", oldValue);
-    }
-
-    connectedCallback() {
-      super.connectedCallback();
-    }
+    @property({ type: Boolean, reflect: true }) extended = false;
 
     static get styles() {
       return [reset, styles];
     }
 
-    get dragItemClassMap() {
+    get draggableItemClassMap() {
       return {
-        "md-draggable-row": !!this.row,
-        "md-draggable-item": !this.row,
-        "md-draggable_disabled": this.disabled
+        extended: this.extended,
+        default: !this.extended,
+        disabled: this.disabled
       };
     }
-    
+
     render() {
       return html`
         <div
-          class="${classMap(this.dragItemClassMap)}"
+          class="md-draggable-item ${classMap(this.draggableItemClassMap)}"
           part="draggable-item"
-          tabindex="0"
-          aria-disabled=${this.disabled}>
-          ${this.edit ? html`<md-icon name="panel-control-dragger_16"></md-icon>` : nothing}
+          aria-disabled=${this.disabled}
+        >
+          ${this.extended
+            ? html`
+                <md-icon name="panel-control-dragger_16"></md-icon>
+              `
+            : nothing}
           <slot></slot>
         </div>
       `;
     }
   }
 }
-
 
 declare global {
   interface HTMLElementTagNameMap {

e95d80ea147cf47d57979ec99bb418e954d9a643
Rob Stachofsky <rstachof@cisco.com>
2021-02-24 13:10:57 -0500

    fix(tooltip): fix tooltipp proptype

--- react/src/lib/Tooltip/index.js
+++ react/src/lib/Tooltip/index.js
@@ -22,7 +22,7 @@ class Tooltip extends React.Component {
         className='md-tooltip__text'
         {
           ...width
-           && { style: { width: `${width}px` }}
+            && { style: { width: `${width}px` }}
         }
       >
         {tooltip}
@@ -60,7 +60,7 @@ Tooltip.propTypes = {
    /** @prop Optional object for Popover Component props | {} */
   popoverProps: PropTypes.object,
   /** @prop Tooltip text | ''  */
-  tooltip: PropTypes.oneOf([PropTypes.object, PropTypes.string]),
+  tooltip: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   /** @prop Set the action which triggers the Tooltip | 'MouseEnter' */
   tooltipTrigger: PropTypes.oneOf(['MouseEnter', 'Click', 'Focus']),
   /** @prop Set the Tooltip width | null */

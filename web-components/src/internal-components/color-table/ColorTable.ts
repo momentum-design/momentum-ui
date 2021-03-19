1b89004acafb418da2820232418b5ca55550b3f2
Kevin Hyde <kehyde@cisco.com>
2021-03-19 10:39:43 -0700

    fix(web-components):fix missing customElementChecks and namespace, export input-field chunk

--- web-components/src/internal-components/color-table/ColorTable.ts
+++ web-components/src/internal-components/color-table/ColorTable.ts
@@ -1,59 +1,61 @@
 import "@/components/table-advanced/TableAdvanced";
-import { customElement, html, internalProperty, LitElement, PropertyValues } from "lit-element";
-import { colorTableData } from "@/wc_scss/colors/vars/color-table-data-string";
 import { TableAdvanced } from "@/components/table-advanced/TableAdvanced";
+import { customElementWithCheck } from "@/mixins";
+import { colorTableData } from "@/wc_scss/colors/vars/color-table-data-string";
+import reset from "@/wc_scss/reset.scss";
+import { html, internalProperty, LitElement, PropertyValues } from "lit-element";
 import { nothing } from "lit-html";
 import { colorCellTemplates } from "./data";
-import { allColorTemplates } from "./utils";
-import reset from "@/wc_scss/reset.scss";
 import styles from "./scss/module.scss";
+import { allColorTemplates } from "./utils";
 
-@customElement("color-table")
-export class ColorTableSandbox extends LitElement {
-  @internalProperty() colorTokenTableData:
-    | { config: TableAdvanced.Config; data: TableAdvanced.Data }
-    | undefined = undefined;
+export namespace ColorTableSandbox {
+  @customElementWithCheck("color-table")
+  export class ELEMENT extends LitElement {
+    @internalProperty() colorTokenTableData:
+      | { config: TableAdvanced.Config; data: TableAdvanced.Data }
+      | undefined = undefined;
 
-  firstUpdated(changedProperties: PropertyValues) {
-    super.firstUpdated(changedProperties);
+    firstUpdated(changedProperties: PropertyValues) {
+      super.firstUpdated(changedProperties);
 
-    // convert string to 2d array
-    const colorTableRowData = colorTableData.split("\n");
+      // convert string to 2d array
+      const colorTableRowData = colorTableData.split("\n");
 
-    let colorTable2DArray: any = [];
-    for (let i = 1; i < colorTableRowData.length - 1; i++) {
-      colorTable2DArray[i] = colorTableRowData[i].split(",");
-    }
+      const colorTable2DArray: any = [];
+      for (let i = 1; i < colorTableRowData.length - 1; i++) {
+        colorTable2DArray[i] = colorTableRowData[i].split(",");
+      }
 
-    this.colorTokenTableData = {
-      config: {
-        cellTemplates: colorCellTemplates,
-        isStickyHeader: true,
+      this.colorTokenTableData = {
+        config: {
+          cellTemplates: colorCellTemplates,
+          isStickyHeader: true,
 
-        rows: {
-          isDraggable: true,
-          selectable: "multiple"
+          rows: {
+            isDraggable: true,
+            selectable: "multiple"
+          },
+
+          cols: {
+            isResizable: true,
+            define: [
+              { id: "c1", title: "Color Token CSS Variable", width: "40%", sorter: "byString", filters: "forString" },
+              { id: "c2", title: "Light Mode", width: "30%", sorter: "byString", filters: "forString" },
+              { id: "c3", title: "Dark Mode", width: "30%", filters: "forString" }
+            ]
+          }
         },
 
-        cols: {
-          isResizable: true,
-          define: [
-            { id: "c1", title: "Color Token CSS Variable", width: "40%", sorter: "byString", filters: "forString" },
-            { id: "c2", title: "Light Mode", width: "30%", sorter: "byString", filters: "forString" },
-            { id: "c3", title: "Dark Mode", width: "30%", filters: "forString" }
-          ]
+        data: {
+          list2d: colorTable2DArray
         }
-      },
+      };
+    }
 
-      data: {
-        list2d: colorTable2DArray
-      }
-    };
-  }
-
-  renderColorTable() {
-    if (this.colorTokenTableData?.config && this.colorTokenTableData?.data) {
-      return html`
+    renderColorTable() {
+      if (this.colorTokenTableData?.config && this.colorTokenTableData?.data) {
+        return html`
         </br>
         <h3>Color Token Instructions</h3>
         <p>
@@ -68,18 +70,25 @@ export class ColorTableSandbox extends LitElement {
           ${allColorTemplates()}
         </md-table-advanced>
       `;
-    } else {
-      return nothing;
+      } else {
+        return nothing;
+      }
+    }
+
+    static get styles() {
+      return [reset, styles];
+    }
+
+    render() {
+      return html`
+        ${this.renderColorTable()}
+      `;
     }
   }
+}
 
-  static get styles() {
-    return [reset, styles];
-  }
-
-  render() {
-    return html`
-      ${this.renderColorTable()}
-    `;
+declare global {
+  interface HTMLElementTagNameMap {
+    "color-table": ColorTableSandbox.ELEMENT;
   }
 }

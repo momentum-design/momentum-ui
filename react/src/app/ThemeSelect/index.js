0f081603943907df7cd0ff107acb38be15f611df
Colin Read <coread@cisco.com>
2021-05-24 15:38:40 +0100

    feat(themes): theme colors modal

--- react/src/app/ThemeSelect/index.js
+++ react/src/app/ThemeSelect/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select, SelectOption} from '@momentum-ui/react';
+import { Select, SelectOption, Modal, ModalHeader, ModalBody, Icon} from '@momentum-ui/react';
 import { dark, light } from './themes';
 
 const themes = {
@@ -7,7 +7,7 @@ const themes = {
   light: light
 };
 
-const Playground = () => {
+const ThemeSelect = () => {
 
   const setTheme = (theme) => {
     if (!theme) {
@@ -27,15 +27,48 @@ const Playground = () => {
     setTheme(themes[themeName]);
   };
 
+  let modalRef = React.useRef();
+  const [showModal, setShowModal] = React.useState(false);
+
+  const colors = () => {
+    return (<div>
+      {Object.keys(themes.dark).map((key) => {
+        return (<div key={key} style={{padding: '1rem'}}>
+          <div style={{float: 'left', clear: 'both', paddingRight: '1rem', paddingBottom: '1rem'}}>{key}</div>
+          <div style={{backgroundColor: `var(${key})`, float: 'left', width: '2rem', height: '2rem'}} />
+        </div>);
+      })}
+    </div>);
+  };
+
   return (
-    <>
+    <div style={{position: 'realtive'}}>
+      <Modal
+        applicationId='app'
+        onHide={() => { setShowModal(false); }}
+        show={showModal}
+        ref={modalRef}
+        htmlId='theme_color_modal'
+        size='large'
+      >
+        <ModalHeader
+          headerLabel='Theme Colors'
+          showCloseButton
+        />
+        <ModalBody>
+          {colors()}
+        </ModalBody>
+      </Modal>
       <Select defaultValue='Select theme' onSelect={onSelect} >
         <SelectOption value='light' label='Light' />
         <SelectOption value='dark' label='Dark' />
         <SelectOption value='none' label='None' />
       </Select>
-    </>
+      <div style={{ position: 'absolute', right: '3.5rem', top: '0.5rem' }} >
+        <Icon name="icon-settings_12" onClick={() => { setShowModal(true); }}/>
+      </div>
+    </div>
   );
 };
 
-export default Playground;
+export default ThemeSelect;

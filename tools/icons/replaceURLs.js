f2367190cad9bf37ecd9dc30c264c5539aa1e67a
paulwitty <56216033+paulwitty@users.noreply.github.com>
2021-06-02 12:45:41 +0100

    fix(build): fix replaceURLs script (#929)
    Co-authored-by: Paul Witty <pauwitty@cisco.com>

--- tools/icons/replaceURLs.js
+++ tools/icons/replaceURLs.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
+if (process.argv.length != 3) {
+  console.error("Run as replaceURLs.js <package name>");
+}
+
 const packageJSON = require(path.resolve(__dirname, `../package.json`));
 const cssPath = `../css/momentum-ui`;
+const packageName = process.argv[2];
 const version = packageJSON.version;
-const cssFile = path.resolve(__dirname, `${cssPath}-icons.css`);
-const cssMinFile = path.resolve(__dirname, `${cssPath}-icons.min.css`);
+const cssFile = path.resolve(__dirname, `${cssPath}-${packageName}.css`);
+const cssMinFile = path.resolve(__dirname, `${cssPath}-${packageName}.min.css`);
 
 fs.readFile(cssFile, 'utf8', function(err, data) {
   if (err) return console.error(err);
 
   const result = data.replace(
     /\.\.\/fonts/g,
-    `https://downloads.momentum-ui.com/@momentum-ui/icons/${version}/fonts`
+    `https://downloads.momentum-ui.com/@momentum-ui/${packageName}/${version}/fonts`
   );
 
   fs.writeFile(cssFile, result, 'utf8', function(err) {
@@ -25,7 +30,7 @@ fs.readFile(cssMinFile, 'utf8', function(err, data) {
   if (err) return console.error(err);
   const result = data.replace(
     /.\.\/fonts/g,
-    `https://downloads.momentum-ui.com/@momentum-ui/icons/${version}/fonts`
+    `https://downloads.momentum-ui.com/@momentum-ui/${packageName}/${version}/fonts`
   );
 
   fs.writeFile(cssMinFile, result, 'utf8', function(err) {

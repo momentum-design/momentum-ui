9044c0d4f6d7c1f01b9cecf1992842a26380b612
mohaksin <mohaksin@cisco.com>
2021-06-08 18:36:59 +0530

    Avatar title update

--- web-components/src/components/avatar/Avatar.ts
+++ web-components/src/components/avatar/Avatar.ts
@@ -79,15 +79,15 @@ export namespace Avatar {
       };
     }
 
-    private get pretifyTitle() {
-      return this.title.length
-        ? this.title
-            .trim()
-            .split(" ")
-            .map(word => word.charAt(0))
-            .join("")
-            .trim()
-        : "";
+    private get pretifyTitle()  {
+      if(this.title.length) {
+        const fullName = this.title.trim().split(" ");
+        if(fullName.length === 1) return fullName[0].charAt(0);
+        else {
+          return fullName[0].charAt(0) + fullName[fullName.length-1].charAt(0);
+        }
+      }
+      return "";
     }
 
     private get chatIconName() {

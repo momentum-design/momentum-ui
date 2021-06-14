1bf9fda447af7e4a9c0421183dafeb4579aacb2e
sbudhia <shivam.budhia27@gmail.com>
2021-06-14 14:54:59 +0530

    feat(md-tabs): added settings option to md-tabs

--- web-components/src/[sandbox]/examples/tabs.ts
+++ web-components/src/[sandbox]/examples/tabs.ts
@@ -2,12 +2,11 @@ import "@/components/button/Button";
 import "@/components/icon/Icon";
 import "@/components/menu-overlay/MenuOverlay";
 import "@/components/tabs/Tab";
-import { TabClickEvent, TabCloseClickEvent } from "@/components/tabs/Tab";
+import { TabCloseClickEvent } from "@/components/tabs/Tab";
 import "@/components/tabs/TabPanel";
 import "@/components/tabs/Tabs";
 import "@/components/tooltip/Tooltip";
-import { css, customElement, internalProperty, LitElement } from "lit-element";
-import { html } from "lit-element";
+import { css, customElement, html, internalProperty, LitElement } from "lit-element";
 import { repeat } from "lit-html/directives/repeat";
 import { unsafeHTML } from "lit-html/directives/unsafe-html";
 import { nanoid } from "nanoid";
@@ -30,6 +29,7 @@ export class TabsTemplateSandbox extends LitElement {
   @internalProperty() private isModalOpen = false;
   defaultTabsOrder = ["WxM", "History", "Answer", "Admins", "Widgets", "News", "Weather", "Turbo"];
   @internalProperty() private currentTabsOrder = this.defaultTabsOrder;
+  @internalProperty() private isSingleButtonResetEnabled = false;
   closeTabName = "";
 
   private setUpTabs() {
@@ -118,7 +118,7 @@ export class TabsTemplateSandbox extends LitElement {
           border: none;
 
           &:hover {
-            background-color: #B2B2B2;
+            background-color: #b2b2b2;
           }
         }
       `
@@ -180,6 +180,11 @@ export class TabsTemplateSandbox extends LitElement {
     this.currentTabsOrder = [];
     console.log("Got it!!");
   }
+
+  handleToggleClick() {
+    this.isSingleButtonResetEnabled = !this.isSingleButtonResetEnabled;
+  }
+
   renderTab() {
     return repeat(
       this.currentTabsOrder,
@@ -203,46 +208,61 @@ export class TabsTemplateSandbox extends LitElement {
       <div style="max-width: 600px; padding-top: 16px;">
         <h3 style="margin: 0;">Retainable Horizontal md-tabs with More button</h3>
         <p style="margin: 0; padding-bottom: 8px;">Stores tabOrder in Local Storage and also has custom close action</p>
+        <md-toggle-switch @click=${() => this.handleToggleClick()} htmlId="toggleSwitch1">
+          Single Button Reset
+        </md-toggle-switch>
         <div>
           <md-tabs selected="0" draggable justified>
             ${this.currentTabsOrder.map((tabElement: string) => {
               return unsafeHTML(this.tabs[tabElement]);
             })}
-            <!-- <md-menu-overlay slot="settings" size="small" style="display: flex; justify-content: center;height: 100%;">
-              <button class="menu-trigger-button" slot="menu-trigger">
-                <md-icon name="icon-more-adr_16"></md-icon>
-              </button>
-              <div style="padding: 16px">
-                <div>
-                  <md-button @click=${(e: MouseEvent) => this.handleCloseAll(e)} variant="secondary"
-                    ><span slot="text">Close All</span></md-button
+            ${!this.isSingleButtonResetEnabled
+              ? html`
+                  <md-menu-overlay
+                    slot="settings"
+                    size="small"
+                    style="display: flex; justify-content: center;height: 100%;"
                   >
-                  <md-button @click=${(e: MouseEvent) => this.handleResetTabs(e)} variant="primary"
-                    ><span slot="text">Reset</span></md-button
+                    <button class="menu-trigger-button" slot="menu-trigger">
+                      <md-icon name="icon-more-adr_16"></md-icon>
+                    </button>
+                    <div style="padding: 16px">
+                      <div>
+                        <md-button @click=${(e: MouseEvent) => this.handleCloseAll(e)} variant="secondary"
+                          ><span slot="text">Close All</span></md-button
+                        >
+                        <md-button @click=${(e: MouseEvent) => this.handleResetTabs(e)} variant="primary"
+                          ><span slot="text">Reset</span></md-button
+                        >
+                      </div>
+                      <p>Unselect Tabs to Hide</p>
+                      <md-checkboxgroup style="display: flex" group-label="group_process">
+                        ${this.defaultTabsOrder.map(tabLabel => {
+                          return html`
+                            <md-checkbox
+                              checked
+                              @checkbox-change=${(e: Event) => this.handleCheckboxChange(e, tabLabel)}
+                              slot="checkbox"
+                              >${tabLabel}</md-checkbox
+                            >
+                          `;
+                        })}
+                      </md-checkboxgroup>
+                    </div>
+                  </md-menu-overlay>
+                `
+              : html`
+                  <button
+                    slot="settings"
+                    class="menu-trigger-button"
+                    @click=${(e: MouseEvent) => this.handleResetTabs(e)}
                   >
-                </div>
-                <p>Unselect Tabs to Hide</p>
-                <md-checkboxgroup style="display: flex" group-label="group_process">
-                  ${this.defaultTabsOrder.map(tabLabel => {
-                    return html`
-                      <md-checkbox
-                        checked
-                        @checkbox-change=${(e: Event) => this.handleCheckboxChange(e, tabLabel)}
-                        slot="checkbox"
-                        >${tabLabel}</md-checkbox
-                      >
-                    `;
-                  })}
-                </md-checkboxgroup>
-              </div>
-            </md-menu-overlay> -->
-            
-            <button  slot="settings" class="menu-trigger-button" @click=${(e: MouseEvent) => this.handleResetTabs(e)}>
-            <md-tooltip placement="bottom" message=${"Reset Tabs"}>
-                <md-icon name="icon-reset_16"></md-icon>
-                </md-tooltip>
-              </button>              
-          </md-tabs>          
+                    <md-tooltip placement="bottom" message=${"Reset Tabs"}>
+                      <md-icon name="icon-reset_16"></md-icon>
+                    </md-tooltip>
+                  </button>
+                `}
+          </md-tabs>
         </div>
         <br />
         <md-modal htmlId="modal-1" ?show=${this.isModalOpen} size="dialog" hideFooter hideHeader noExitOnEsc>
@@ -260,7 +280,7 @@ export class TabsTemplateSandbox extends LitElement {
 
 export const tabsTemplate = html`
   <default-tabs-sandbox></default-tabs-sandbox>
-  <!-- <div style="max-width: 600px;">
+  <div style="max-width: 600px;">
     <h3>Draggable horizontal md-tabs with More button</h3>
     <div>
       <md-tabs selected="2" draggable>
@@ -460,6 +480,6 @@ export const tabsTemplate = html`
       <md-tab-panel slot="panel">
         <span>Content for "Cisco Turbo"</span>
       </md-tab-panel>
-    </md-tabs> -->
+    </md-tabs>
   </div>
 `;

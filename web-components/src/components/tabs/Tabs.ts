cd7562428856fcf2ba4562c9f223cf8d49f79eb3
Meraj <mmeraj@gmail.com>
2021-09-03 17:32:04 +0530

    feat(md-tabs): persistable tabs on remount of the tabs

--- web-components/src/components/tabs/Tabs.ts
+++ web-components/src/components/tabs/Tabs.ts
@@ -507,10 +507,9 @@ export namespace Tabs {
     }
 
     private updateSelectedTab(newSelectedIndex: number) {
-     
       const { tabs, panels } = this;
       const oldSelectedIndex = this.tabs.findIndex(element => element.hasAttribute("selected"));
-     
+
       if (tabs && panels) {
         [oldSelectedIndex, newSelectedIndex].forEach(index => {
           const tab = tabs[index];
@@ -576,8 +575,8 @@ export namespace Tabs {
       });
       this.updateIsMoreTabMenuSelected();
 
-      if (this.persistSelection  && this.tabsId || this.tabsId.trim() !== "") {
-        localStorage.setItem(this.tabsId, `${newSelectedTabIdx}`);
+      if (this.persistSelection && this.tabsId && newSelectedTabIdx > -1 && this.tabsId.trim() !== "") {
+        sessionStorage.setItem(this.tabsId, `${newSelectedTabIdx}`);
       }
     }
 
@@ -802,22 +801,34 @@ export namespace Tabs {
     connectedCallback() {
       super.connectedCallback();
       this.setupTabsEvents();
-      this.selectTabFromStorage();
     }
 
-    selectTabFromStorage() {
+    private getSelectedTabIndexFromStorage() {
+      const persistedSelectedTabIdx = localStorage.getItem(this.tabsId);
+      return persistedSelectedTabIdx && parseInt(persistedSelectedTabIdx) > -1
+        ? parseInt(persistedSelectedTabIdx)
+        : this.selected;
+    }
+
+    private selectTabFromStorage() {
       if (this.persistSelection) {
         if (!this.tabsId || this.tabsId.trim() === "") {
           console.error("Unique tabs-id attribute is mandatory for persist the selection of tab ");
           return;
         }
-        const persistedSelectedTabIdx = localStorage.getItem(this.tabsId);
-        this.selected =
-          persistedSelectedTabIdx && parseInt(persistedSelectedTabIdx) > -1
-            ? parseInt(persistedSelectedTabIdx)
-            : this.selected;
+        const persistedSelectedTabIdx = sessionStorage.getItem(this.tabsId);
+        let selectedTabIndex = 0;
+        if (persistedSelectedTabIdx) {
+          const idx = parseInt(persistedSelectedTabIdx);
+          selectedTabIndex = idx > -1 ? idx : this.selected;
+        }
 
-            this.requestUpdate();
+        const currentTabsLayout = [...this.tabsFilteredAsVisibleList, ...this.tabsFilteredAsHiddenList];
+        if (currentTabsLayout.length && currentTabsLayout[selectedTabIndex].id) {
+          this.handleNewSelectedTab(currentTabsLayout[selectedTabIndex].id);
+        } else {
+          this.selected = selectedTabIndex;
+        }
       }
     }
 
@@ -830,6 +841,7 @@ export namespace Tabs {
       super.firstUpdated(changedProperties);
       this.setupPanelsAndTabs();
       this.linkPanelsAndTabs();
+      this.selectTabFromStorage();
     }
 
     protected updated(changedProperties: PropertyValues) {
@@ -838,7 +850,6 @@ export namespace Tabs {
       if (changedProperties.has("slotted")) {
         this.initializeTabs();
       }
-
 
       if (this.draggable && !this.visibleTabsSortableInstance && !this.hiddenTabsSortableInstance) {
         this.initializeSortable();
@@ -884,8 +895,7 @@ export namespace Tabs {
         }
       }
 
-      if(changedProperties.has("tabsId")) {
-        console.log("tabs-id changed")
+      if (changedProperties.has("tabsId")) {
         this.selectTabFromStorage();
       }
     }

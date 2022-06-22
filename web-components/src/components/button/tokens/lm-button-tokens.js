eb16b241f1df48f89bc000f82742517badd9e287
rituparnadey1807 <41279423+rituparnadey1807@users.noreply.github.com>
2022-06-22 18:54:47 +0530

    #CX-8495 : Incorrect colour of info icon in station login and in profile settings in dark mode (#1278)
    * dark mode colours corrected for variant white buttons
    
    * redone changes for modal center align
    
    * version-upgrade
    
    * version-mismatch corrected
    
    * colour for dark mode corrected
    
    Co-authored-by: Rituparna Dey <ritudey@cisco.com>

--- web-components/src/components/button/tokens/lm-button-tokens.js
+++ web-components/src/components/button/tokens/lm-button-tokens.js
@@ -100,7 +100,7 @@ const button = {
   white: {
     "bg-color": {
       light: colors.white.default.name,
-      dark: colors.gray[90].name
+      dark: colors.gray[95].name
     },
     "text-color": {
       light: colors.gray[70].name,

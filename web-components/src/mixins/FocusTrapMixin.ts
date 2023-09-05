5e0723a83950f748afe6d3846eda3280aaa5cffb
Aadesh Srivastava <aasrivas@cisco.com>
2023-09-06 01:39:36 +0530

    fix: reverting to fix 1

--- web-components/src/mixins/FocusTrapMixin.ts
+++ web-components/src/mixins/FocusTrapMixin.ts
@@ -245,11 +245,14 @@ export const FocusTrapMixin = <T extends AnyConstructor<FocusClass & FocusTrapCl
       } else {
         if (activeIndex === -1 && this.focusTrapIndex + 1 < this.focusableElements.length) {
           this.focusTrapIndex++;
-        } else if (activeIndex === this.focusableElements.length - 1 && this.focusTrapIndex === 0) {
-          const changedProperties = new Map();
-          changedProperties.set("focusTrapIndex", this.focusTrapIndex);
-          this.updated(changedProperties);
-        } else {
+        } 
+        else if (activeIndex === this.focusableElements.length - 1 && this.focusTrapIndex === 0) {
+          const nextEleToFocus = this.focusableElements[this.focusTrapIndex];
+          if (nextEleToFocus) {
+            this.tryFocus(nextEleToFocus);
+          }
+        } 
+        else {
           this.focusTrapIndex = activeIndex + 1 < this.focusableElements.length ? activeIndex + 1 : 0;
         }
       }

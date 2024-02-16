569dbef434973fcff19d3bd4986e4cb5c15468e1
Ruchi Priya <51110665+iruchipriya@users.noreply.github.com>
2024-02-16 14:25:29 +0530

    CX-10233 : Screen Readers: Resize Modal button is announced without sufficient context (#1530)
    * fix(accessibility): implement dynamic label for 'Resize Modal'
    
    * fix(accessibility): implement dynamic label for 'Resize Modal'
    
    * fix(accessibility): implement dynamic label for 'Resize Modal'
    
    * fix(accessibility): implement dynamic label for 'Resize Modal'
    
    * fix(accessibility): implement dynamic label for 'Resize Modal'
    
    * fix(accessibility): implement dynamic label for 'Resize Modal'
    
    * fix(accessibility): implement dynamic label for 'Resize Modal'
    
    ---------
    
    Co-authored-by: rucpriya <rucpriya@cisco.com>
    Co-authored-by: Mohammed Meraj <mmeraj@gmail.com>

--- web-components/src/components/floating-modal/FloatingModal.ts
+++ web-components/src/components/floating-modal/FloatingModal.ts
@@ -26,6 +26,7 @@ export namespace FloatingModal {
     @property({ type: Boolean, reflect: true, attribute: "full-screen" }) full = false;
     @property({ type: String, attribute: "close-aria-label" }) closeAriaLabel = "Close Modal";
     @property({ type: String, attribute: "resize-aria-label" }) resizeAriaLabel = "Resize Modal";
+    @property({ type: String, attribute: "maximize-aria-label" }) maximizeScreenLabel  = "Maximize Modal";
     @property({ type: String, attribute: "minimize-aria-label" }) minimizeAriaLabel = "Minimize Modal";
     @property({ type: Boolean, reflect: true }) private minimize = false;
     @property({type: Object}) position: {
@@ -312,7 +313,7 @@ export namespace FloatingModal {
                   ${!this.minimize ? html` <md-button
                     color="color-none"
                     class="md-floating__resize"
-                    aria-label="${this.resizeAriaLabel}"
+                    aria-label="${this.full ? this.resizeAriaLabel: this.maximizeScreenLabel}"
                     circle
                     @click=${this.handleToggleExpandCollapse}
                   >

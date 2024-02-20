5efaec7ee695ad2341fe1b2da7a10f5395a31e58
Rituparna Dey <ritudey@cisco.com>
2024-02-21 00:33:54 +0530

    added part in tooltip component

--- web-components/src/components/tooltip/Tooltip.ts
+++ web-components/src/components/tooltip/Tooltip.ts
@@ -208,6 +208,7 @@ export namespace Tooltip {
           </div>
           <div
             class="md-tooltip__reference"
+            part="tooltip-reference"
             @mouseenter=${() => this.notifyTooltipCreate()}
             @mouseleave=${() => this.notifyTooltipDestroy()}
             @focusin=${(event: Event) => this.handleFocusIn(event)}

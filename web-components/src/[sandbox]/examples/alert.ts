d2ab6645a50c0c8e2f3fa40fc1f36cb675ca8e8b
James Nestor <87858909+NotNestor@users.noreply.github.com>
2024-06-28 17:18:09 +0100

    Alert update (#1675)
    * chore: updates for alert
    
    * chore: add pill rounding for lists
    
    ---------
    
    Co-authored-by: James Nestor <jnestor@cisco.com>

--- web-components/src/[sandbox]/examples/alert.ts
+++ web-components/src/[sandbox]/examples/alert.ts
@@ -18,7 +18,7 @@ export const alertTemplate = html`
     </md-badge>
   </md-alert>
   <md-alert message="Who's awesome? You are!" show>
-    <a slot="alert-body" href="/">Test</a>
+    <md-link slot="alert-body" href="/">Test</md-link>
     <div slot="alert-footer">
       <md-button variant="primary"><span slot="text">primary</span></md-button>
     </div>

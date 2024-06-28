b80bf4c959ae07c150e80cc0e8e7cccce01d29a6
James Nestor <jnestor@cisco.com>
2024-06-28 20:29:16 +0100

    chore: accordian and audio player tokens

--- web-components/src/components/audio-player/tokens/mdv2-audio-player-token.js
+++ web-components/src/components/audio-player/tokens/mdv2-audio-player-token.js
@@ -1,44 +1,43 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-const colors = require("@momentum-ui/tokens/dist/colors.json");
 
 const audioPlayer = {
   prefix: "mdv2",
   component: "audio-player",
   background: {
     body: {
-      light: colors.gray[10].name,
-      dark: colors.gray[80].name
+      light: "$mds-color-theme-background-solid-secondary-normal",
+      dark: "$mds-color-theme-background-solid-secondary-normal"
     },
     "progress-bar": {
-      light: colors.gray[80].name,
-      dark: colors.gray["05"].name
+      light: "$mds-color-theme-control-active-normal",
+      dark: "$mds-color-theme-control-active-normal"
     },
     "buffer-bar": {
-      light: colors.gray[40].name,
-      dark: colors.gray["30"].name
+      light: "$mds-color-theme-control-indicator-inactive-normal",
+      dark: "$mds-color-theme-control-indicator-inactive-normal"
     },
     timeline: {
-      light: colors.gray[30].name,
-      dark: colors.gray[60].name
+      light: "$mds-color-theme-control-indicator-inactive-normal",
+      dark: "$mds-color-theme-control-indicator-inactive-normal"
     },
     "speed-popup": {
-      light: colors.white[100].name,
-      dark: colors.gray[90].name,
+      light: "$mds-color-theme-background-solid-secondary-normal",
+      dark: "$mds-color-theme-background-solid-secondary-normal",
       hover: {
-        light: colors.gray["05"].name,
-        dark: colors.gray[70].name
+        light: "$mds-color-theme-background-primary-hover",
+        dark: "$mds-color-theme-background-primary-hover"
       }
     }
   },
   "font-color": {
-    light: colors.gray[100].name,
-    dark: colors.gray["05"].name
+    light: "$mds-color-theme-text-primary-normal",
+    dark: "$mds-color-theme-text-primary-normal"
   },
   "focus-ring": {
     color: {
-      light: colors.blue[60].name,
-      dark: colors.blue[40].name
+      light: "$mds-color-theme-outline-theme-normal",
+      dark: "$mds-color-theme-outline-theme-normal"
     }
   }
 };

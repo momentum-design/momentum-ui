86b62e893cb8c1b5274e2f25e121216d1d7650f6
James Nestor <jnestor@cisco.com>
2024-06-29 18:39:26 +0100

    chore: update tokens

--- web-components/src/components/coachmark/tokens/mdv2-coachmark-tokens.js
+++ web-components/src/components/coachmark/tokens/mdv2-coachmark-tokens.js
@@ -6,133 +6,137 @@ const coachmark = {
   component: "coachmark",
   default: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   blue: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   green: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   purple: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   violet: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   mint: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   darkmint: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   yellow: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   red: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   darkred: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   orange: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   cyan: {
     "bg-color": {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
   },
   outline: {
     color: {
-      light: "$mds-color-theme-inverted-background-normal",
-      dark: "$mds-color-theme-inverted-background-normal"
+      light: "$mds-color-theme-background-solid-primary-normal",
+      dark: "$mds-color-theme-background-solid-primary-normal"
     },
     "text-color": {
-      light: "$mds-color-theme-inverted-text-primary-normal",
-      dark: "$mds-color-theme-inverted-text-primary-normal"
+      light: "$mds-color-theme-text-primary-normal",
+      dark: "$mds-color-theme-text-primary-normal"
     }
+  },
+  "border-color": {
+    light: "$mds-color-theme-outline-primary-normal",
+    dark: "$mds-color-theme-outline-primary-normal"
   }
 };
 

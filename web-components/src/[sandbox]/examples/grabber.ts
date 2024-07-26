b97a24bcaae0e09838225ca74180126ce2cb479b
James Nestor <87858909+NotNestor@users.noreply.github.com>
2024-07-26 17:14:05 +0100

    chore: update colour tokens and add hidden functionality to grabber (#1715)
    Co-authored-by: James Nestor <jnestor@cisco.com>

--- web-components/src/[sandbox]/examples/grabber.ts
+++ web-components/src/[sandbox]/examples/grabber.ts
@@ -8,19 +8,38 @@ export class GrabberTemplateSandbox extends LitElement {
   @query("#grabberTrailing") grabberTrailing!: Grabber.ELEMENT;
   @query("#grabberTop") grabberTop!: Grabber.ELEMENT;
   @query("#grabberBottom") grabberBottom!: Grabber.ELEMENT;
+  @query("#grabberToggle") grabberToggle!: Grabber.ELEMENT;
+
+  constructor() {
+    super();
+    this.onWindowMouseMove = this.onWindowMouseMove.bind(this);
+  }
 
   connectedCallback(): void {
     super.connectedCallback();
 
     this.addEventListener("grabber-toggled", this.grabberToggled as EventListener);
     this.addEventListener("grabber-hover-changed", this.grabberHovered as EventListener);
+    window.addEventListener("mousemove", this.onWindowMouseMove);
   }
 
   disconnectedCallback(): void {
     this.removeEventListener("grabber-toggled", this.grabberToggled as EventListener);
     this.removeEventListener("grabber-hover-changed", this.grabberHovered as EventListener);
+    window.removeEventListener("mousemove", this.onWindowMouseMove);
 
     super.disconnectedCallback();
+  }
+
+  onWindowMouseMove(e: MouseEvent) {
+    if (this.grabberToggle) {
+      const rect = this.grabberToggle.getBoundingClientRect();
+      if (this.isMouseCloseToRect(e, rect)) {
+        this.grabberToggle.visible = true;
+      } else {
+        this.grabberToggle.visible = false;
+      }
+    }
   }
 
   grabberToggled(_e: CustomEvent) {
@@ -52,8 +71,40 @@ export class GrabberTemplateSandbox extends LitElement {
     `;
   }
 
+  get isCollapsed() {
+    return !this.grabberToggle?.checked || false;
+  }
+
   render() {
     return html`
+      <style>
+        .toggle-container {
+          display: flex;
+          align-items: center;
+          height: 800px;
+        }
+
+        .toggle-div {
+          width: 200px;
+          height: 100%;
+          background-color: var(--md-content-bg-color);
+          transition: width 0.3s ease;
+
+          border-radius: 12px;
+          box-shadow: 0px 4px 8px 0px #00000033;
+        }
+        .toggle-div.collapsed {
+          width: 0px;
+        }
+
+        .divider {
+          width: 1px;
+          height: calc(100% - 48px);
+          margin-top: 24px;
+          margin-bottom: 24px;
+          background: var(--md-nav-divider-primary);
+        }
+      </style>
       <div>
         <h3 class="sandbox-header" style="margin: .5rem 1rem">Leading</h3>
         <md-grabber id="grabberLeading" alignment="leading"></md-grabber>
@@ -74,8 +125,38 @@ export class GrabberTemplateSandbox extends LitElement {
         <md-grabber id="grabberBottom" alignment="bottom"></md-grabber>
         ${this.grabberDetailTemplate(this.grabberBottom)}
       </div>
+      <div>
+        <h3 class="sandbox-header" style="margin: .5rem 1rem">Toggle Width Example</h3>
+        <div class="toggle-container">
+          <div class="toggle-div collapsed" id="toggleDiv"></div>
+          <div class="divider"></div>
+          <md-grabber id="grabberToggle" alignment="leading" @click="${this.toggleWidth.bind(this)}"></md-grabber>
+        </div>
+      </div>
     `;
   }
+
+  toggleWidth() {
+    const toggleDiv = this.shadowRoot?.querySelector("#toggleDiv");
+    if (toggleDiv) {
+      if (this.isCollapsed) {
+        toggleDiv.classList.add("collapsed");
+      } else {
+        toggleDiv.classList.remove("collapsed");
+      }
+    }
+  }
+
+  VISIBLE_DISTANCE = 100; //px
+
+  isMouseCloseToRect = (e: MouseEvent, rect: DOMRect) => {
+    return (
+      rect.left - this.VISIBLE_DISTANCE <= e.clientX &&
+      rect.right + this.VISIBLE_DISTANCE >= e.clientX &&
+      rect.top - this.VISIBLE_DISTANCE <= e.clientY &&
+      rect.bottom + this.VISIBLE_DISTANCE >= e.clientY
+    );
+  };
 }
 
 export const grabberTemplate = html`

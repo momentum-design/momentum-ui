2f328cb8e55d547198947ebf33cdf62ddd85ddf4
Jason20020 <55873488+Jason20020@users.noreply.github.com>
2024-07-26 16:51:09 +0100

    fix(tabs): tabs border issue fix (#1708)
    * fix(tabs): tabs border issue fix
    
    * fix(tabs): update underline issue
    
    * add slido-colored svg (#1706)
    
    * chore: fix tabs css issue in chrome (#1712)
    
    Co-authored-by: James Nestor <jnestor@cisco.com>
    
    * fix(icon): accept empty var (#1711)
    
    * fix(icon): accept empty var
    
    * fix(icon): fix
    
    ---------
    
    Co-authored-by: Jason Chai Jing <jachai@cisco.com>
    
    * fix(tabs): fix gap
    
    ---------
    
    Co-authored-by: Jason Chai Jing <jachai@cisco.com>
    Co-authored-by: jingkunhuang <69663725+jingkunhuang@users.noreply.github.com>
    Co-authored-by: James Nestor <87858909+NotNestor@users.noreply.github.com>
    Co-authored-by: James Nestor <jnestor@cisco.com>

--- web-components/src/components/tabs/tokens/mdv2-tabs-tokens.js
+++ web-components/src/components/tabs/tokens/mdv2-tabs-tokens.js
@@ -16,7 +16,10 @@ const tabs = {
       common: "transparent"
     },
     "active-bottom-border": {
-      common: "2px solid $mds-color-theme-outline-input-active"
+      common: "inset 0 -2px 0 0 $mds-color-theme-outline-input-active"
+    },
+    "active-vertical-border": {
+      common: "inset 2px 0 0 0 $mds-color-theme-outline-input-active"
     },
     "focus-bg": {
       light: colors.white[60].name,

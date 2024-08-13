6f49e0ce48db15dd332d231ceb016defa3daf1a0
James Nestor <jnestor@cisco.com>
2024-08-13 16:04:22 +0100

    chore(deps): update dependencies for momentum

--- web-components/src/[sandbox]/examples/icon.ts
+++ web-components/src/[sandbox]/examples/icon.ts
@@ -11,27 +11,9 @@ export const iconTemplate = html`
   <h3>Old icon:</h3>
   <md-icon name="pto_28" class="testClass" color="green-50"></md-icon>
   <h3>Swapping from old icon-> new icon (enabled):</h3>
-  <md-icon
-    name="pto_28"
-    designEnabled="false"
-    class="testClass"
-    size="50"
-    color="var(--avatar-presence-active)"
-  ></md-icon>
+  <md-icon name="pto_28" class="testClass" size="50" color="var(--avatar-presence-active)"></md-icon>
   <h3>Using new icon enabled (enabled):</h3>
-  <md-icon
-    name="search-bold"
-    designEnabled="true"
-    class="testClass"
-    size="50"
-    color="var(--avatar-presence-active)"
-  ></md-icon>
+  <md-icon name="search-bold" designEnabled class="testClass" size="50" color="var(--avatar-presence-active)"></md-icon>
   <h3>Using new icon override (override):</h3>
-  <md-icon
-    name="search-bold"
-    override="true"
-    class="testClass"
-    size="50"
-    color="var(--avatar-presence-active)"
-  ></md-icon>
+  <md-icon name="search-bold" override class="testClass" size="50" color="var(--avatar-presence-active)"></md-icon>
 `;

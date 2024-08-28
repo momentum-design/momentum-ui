c572d5e98ff5e222bb1a4c33251e7a4741e0c49b
nithishkumar98 <nithishindians@gmail.com>
2024-08-29 02:22:12 +0530

    fix(cx-15828): selection fixed

--- web-components/src/[sandbox]/examples/AdvanceList/components/ParentComponentGeneric.ts
+++ web-components/src/[sandbox]/examples/AdvanceList/components/ParentComponentGeneric.ts
@@ -25,10 +25,6 @@ export namespace ParentComponentGeneric {
             try {
                 this.isLoading = true;
                 const newItems = await this.fetchItems(this.page);
-                const infiniteScrollList = this.shadowRoot?.querySelector('md-advance-list');
-                if (infiniteScrollList) {
-                    infiniteScrollList.setItems([...this.items, ...newItems]);
-                }
                 this.items = [...this.items, ...newItems];
                 this.page += 1;
                 this.isLoading = false;
@@ -46,7 +42,8 @@ export namespace ParentComponentGeneric {
             const newItems = Array.from({ length: 20 }, (_, i) => ({
                 data: `Item ${(page - 1) * 20 + i + 1}`,
                 id: crypto.randomUUID(),
-                template: (data: string) => html`<div>${data}</div>`
+                index: i,
+                template: (data: string, index: number) => html`<div indexing="${index}" >${data}</div>`
             }));
             console.log('newItems', newItems)
             return newItems;

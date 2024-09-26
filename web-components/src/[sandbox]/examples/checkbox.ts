c8a971679b5b483c47102f422025b1dd8e78e4ae
James Nestor <jnestor@cisco.com>
2024-09-27 00:42:12 +0100

    chore: update: tooltip and font sizes

--- web-components/src/[sandbox]/examples/checkbox.ts
+++ web-components/src/[sandbox]/examples/checkbox.ts
@@ -13,39 +13,52 @@ export const checkboxTemplate = html`
   <md-checkbox indeterminate>Option Item</md-checkbox>
   <h3>Pre-Checked</h3>
   <md-checkbox checked>Option Item</md-checkbox>
-  <h3>Default Group</h3>
-  <md-checkboxgroup group-label="group_process">
-    <md-checkbox slot="checkbox">Option 1</md-checkbox>
-    <md-checkbox slot="checkbox">Option 2</md-checkbox>
-    <md-checkbox slot="checkbox">Option 3</md-checkbox>
-    <md-checkbox slot="checkbox">Option 4</md-checkbox>
-  </md-checkboxgroup>
-  <h3>Horizontal Group</h3>
-  <md-checkboxgroup group-label="group_process" alignment="horizontal">
-    <md-checkbox slot="checkbox">Option 1</md-checkbox>
-    <md-checkbox slot="checkbox">Option 2</md-checkbox>
-    <md-checkbox slot="checkbox">Option 3</md-checkbox>
-    <md-checkbox slot="checkbox">Option 4</md-checkbox>
-  </md-checkboxgroup>
-  <h3>Disabled Group</h3>
-  <md-checkboxgroup group-label="group_process">
-    <md-checkbox slot="checkbox">Option 1</md-checkbox>
-    <md-checkbox slot="checkbox" disabled>Disabled Option</md-checkbox>
-    <md-checkbox slot="checkbox" disabled checked>Disabled & Checked Option</md-checkbox>
-    <md-checkbox slot="checkbox">Option 2</md-checkbox>
-  </md-checkboxgroup>
-  <h3>Indeterminate Group</h3>
-  <md-checkboxgroup group-label="group_process">
-    <md-checkbox slot="checkbox">Option 1</md-checkbox>
-    <md-checkbox slot="checkbox" disabled indeterminate>Disabled Indeterminate Option</md-checkbox>
-    <md-checkbox slot="checkbox" disabled checked>Disabled Checked Option</md-checkbox>
-    <md-checkbox slot="checkbox" indeterminate>Indeterminate Option</md-checkbox>
-  </md-checkboxgroup>
-  <h3>Pre-Checked Group</h3>
-  <md-checkboxgroup group-label="group_process">
-    <md-checkbox slot="checkbox" checked>Checked Option</md-checkbox>
-    <md-checkbox slot="checkbox">Option 1</md-checkbox>
-    <md-checkbox slot="checkbox" checked>Checked Option</md-checkbox>
-    <md-checkbox slot="checkbox">Option 2</md-checkbox>
-  </md-checkboxgroup>
+  <h3>Default with icon</h3>
+    <md-checkbox>
+      Option Item  
+      <md-icon name="info-badge-filled" size="16" iconSet="momentumDesign"></md-icon>
+    </md-checkbox>
+    <h3>Default with icon and tooltip</h3>
+    <md-checkbox>
+      Option Item
+      <md-tooltip message="Tooltip message" placement="top">
+        <md-icon name="info-badge-filled" size="16" iconSet="momentumDesign"></md-icon>
+      </md-tooltip>
+    </md-checkbox>
+    <h3>Default Group</h3>
+    <md-checkboxgroup group-label="group_process">
+      <md-checkbox slot="checkbox">Option 1</md-checkbox>
+      <md-checkbox slot="checkbox">Option 2</md-checkbox>
+      <md-checkbox slot="checkbox">Option 3</md-checkbox>
+      <md-checkbox slot="checkbox">Option 4</md-checkbox>
+    </md-checkboxgroup>
+    <h3>Horizontal Group</h3>
+    <md-checkboxgroup group-label="group_process" alignment="horizontal">
+      <md-checkbox slot="checkbox">Option 1</md-checkbox>
+      <md-checkbox slot="checkbox">Option 2</md-checkbox>
+      <md-checkbox slot="checkbox">Option 3</md-checkbox>
+      <md-checkbox slot="checkbox">Option 4</md-checkbox>
+    </md-checkboxgroup>
+    <h3>Disabled Group</h3>
+    <md-checkboxgroup group-label="group_process">
+      <md-checkbox slot="checkbox">Option 1</md-checkbox>
+      <md-checkbox slot="checkbox" disabled>Disabled Option</md-checkbox>
+      <md-checkbox slot="checkbox" disabled checked>Disabled & Checked Option</md-checkbox>
+      <md-checkbox slot="checkbox">Option 2</md-checkbox>
+    </md-checkboxgroup>
+    <h3>Indeterminate Group</h3>
+    <md-checkboxgroup group-label="group_process">
+      <md-checkbox slot="checkbox">Option 1</md-checkbox>
+      <md-checkbox slot="checkbox" disabled indeterminate>Disabled Indeterminate Option</md-checkbox>
+      <md-checkbox slot="checkbox" disabled checked>Disabled Checked Option</md-checkbox>
+      <md-checkbox slot="checkbox" indeterminate>Indeterminate Option</md-checkbox>
+    </md-checkboxgroup>
+    <h3>Pre-Checked Group</h3>
+    <md-checkboxgroup group-label="group_process">
+      <md-checkbox slot="checkbox" checked>Checked Option</md-checkbox>
+      <md-checkbox slot="checkbox">Option 1</md-checkbox>
+      <md-checkbox slot="checkbox" checked>Checked Option</md-checkbox>
+      <md-checkbox slot="checkbox">Option 2</md-checkbox>
+    </md-checkboxgroup>
+  </h3>
 `;

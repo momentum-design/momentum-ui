c9432a46e33c25aec5bcb49957cd2b3d6c344c6e
James Nestor <jnestor@cisco.com>
2024-10-07 15:47:40 +0100

    chore: accordian controls

--- web-components/src/components/accordion/Accordion.stories.ts
+++ web-components/src/components/accordion/Accordion.stories.ts
@@ -1,43 +1,47 @@
 import { ThemeNameValues } from "@/components/theme/Theme";
-import { boolean, select } from "@storybook/addon-knobs";
+import type { Args, Meta, StoryObj } from "@storybook/web-components";
+
 import { html } from "lit-element";
 import "./Accordion";
-import mdx from "./Accordion.mdx";
 import "./AccordionItem";
 
-export default {
-  title: "Components/Accordion",
-  component: "md-accordion",
-  parameters: {
-    a11y: {
-      element: "md-accordion"
-    },
-    docs: {
-      page: mdx
-    }
+const render = (args: Args) => html`
+  <md-theme class="theme-toggle" id="activity-button" ?darkTheme=${args.darkTheme} theme=${args.theme}>
+    <md-accordion ?multiple=${args.multiple}>
+      <md-accordion-item slot="accordion-item" label="Header №1" ?expanded=${args.expanded}>
+        <div>Panel №1</div>
+      </md-accordion-item>
+      <md-accordion-item slot="accordion-item" label="Header №2" ?disabled=${args.disabled}>
+        <div>Panel №2</div>
+      </md-accordion-item>
+      <md-accordion-item slot="accordion-item" label="Header №3">
+        <div>Panel №3</div>
+      </md-accordion-item>
+    </md-accordion>
+  </md-theme>
+`;
+
+export const Primary: StoryObj = {
+  args: {
+    theme: "lumos",
+    darkTheme: false
   }
 };
 
-export const Accordion = () => {
-  const darkTheme = boolean("darkMode", false);
-  const theme = select("Theme name", ThemeNameValues, "lumos");
-  const disabled = boolean("disabled", false);
-  const expanded = boolean("expanded", false);
-  const multiple = boolean("multiple", false);
-
-  return html`
-    <md-theme class="theme-toggle" id="activity-button" ?darkTheme=${darkTheme} theme=${theme}>
-      <md-accordion ?multiple=${multiple}>
-        <md-accordion-item slot="accordion-item" label="Header №1" ?expanded=${expanded}>
-          <div>Panel №1</div>
-        </md-accordion-item>
-        <md-accordion-item slot="accordion-item" label="Header №2" ?disabled=${disabled}>
-          <div>Panel №2</div>
-        </md-accordion-item>
-        <md-accordion-item slot="accordion-item" label="Header №3">
-          <div>Panel №3</div>
-        </md-accordion-item>
-      </md-accordion>
-    </md-theme>
-  `;
+const meta: Meta = {
+  title: "Components/Accordian",
+  component: "md-accordion",
+  render,
+  argTypes: {
+    theme: { control: { type: "select", options: ThemeNameValues } },
+    darkTheme: { control: "boolean" }
+  },
+  parameters: {
+    a11y: {
+      element: "md-accordion"
+    }
+  },
+  args: Primary.args
 };
+
+export default meta;

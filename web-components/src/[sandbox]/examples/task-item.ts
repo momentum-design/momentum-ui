857fb80810cfc92c9508bed34d28936dcb3afd02
cormacmchale1 <cormacmchale16@gmail.com>
2024-11-13 18:33:41 +0000

    fix(release): decouple title from rendered text in TextItem (CX-16559) (#1822)
    * fix(release): decouple title from rendered text in TextItem
    
    This will allow users to optionally avoid the browser generated tooltip from title.
    
    * fix(release): use title value as display text when new variable for display text is not provided
    
    preserving legacy functionality
    
    * fix(release): adding test to confirm use of legacy variable
    
    preserving legacy functionality
    
    * fix(release): preserve legacy aria label logic
    
    preserving legacy functionality
    
    * fix(release): refactoring logic and explicit manual check for no tooltip
    
    preserving legacy functionality
    
    * fix(release): cleaning up extra whitespace etc
    
    cleanup
    
    * fix(release): cleaning up extra whitespace etc
    
    cleanup
    
    * fix(release): update for correct logic for tests
    
    testing
    
    * fix(release): update for correct logic for tests
    
    testing
    
    * fix(release): request for changes
    
    feedback

--- web-components/src/[sandbox]/examples/task-item.ts
+++ web-components/src/[sandbox]/examples/task-item.ts
@@ -19,7 +19,7 @@ export const taskItemTemplate = html`
     <md-task-item
       mediaType="outbound telephony"
       status="hold"
-      title="Mihael Varificantare"
+      item-title="No title tooltip test"
       queue="IRV_quelle_11"
       lastmessage=" "
       quantity="123"
@@ -31,6 +31,7 @@ export const taskItemTemplate = html`
       mediaType="inbound telephony"
       status="transfered"
       title="Mihael Varificantare"
+      item-title="Mihael Varificantare"
       queue="IRV_quelle_11"
       lastmessage=" "
       quantity="123"
@@ -40,6 +41,7 @@ export const taskItemTemplate = html`
     <md-task-item
       mediaType="midcall telephony"
       title="MediaType: midcall telephony"
+      item-title="MediaType: midcall telephony"
       queue="IRV_quelle_11"
       lastmessage=" "
       quantity="123"
@@ -51,6 +53,7 @@ export const taskItemTemplate = html`
       mediaType="callback"
       status="campaign"
       title="Media Type callback"
+      item-title="Media Type callback"
       queue="IRV_quelle_11"
       lastmessage=" "
       quantity="123"
@@ -66,28 +69,29 @@ export const taskItemTemplate = html`
     >
       <div>01:08:00</div>
     </md-task-item>
-    <md-task-item mediaType="email" status="play" title="mlittlefoot@gmail.com" queue="IRV_quelle_12" selected>
+    <md-task-item mediaType="email" status="play" title="mlittlefoot@gmail.com" item-title="mlittlefoot@gmail.com" queue="IRV_quelle_12" selected>
       <div>00:08</div>
     </md-task-item>
-    <md-task-item mediaType="telephony" status="consulting" title="Mihael Varificantare" queue="IRV_quelle_11">
+    <md-task-item mediaType="telephony" status="consulting" title="Mihael Varificantare" item-title="Mihael Varificantare" queue="IRV_quelle_11">
       <div>Ringing</div>
     </md-task-item>
-    <md-task-item mediaType="messenger" title="Mihael Varificantare" queue="IRV_quelle_11">
+    <md-task-item mediaType="messenger" title="Mihael Varificantare" item-title="Mihael Varificantare" queue="IRV_quelle_11">
       <div>Ringing</div>
     </md-task-item>
-    <md-task-item mediaType="whatsApp" title="Mihael Varificantare" queue="IRV_quelle_11">
+    <md-task-item mediaType="whatsApp" title="Mihael Varificantare" item-title="Mihael Varificantare" queue="IRV_quelle_11">
       <div>01:10:25</div>
     </md-task-item>
-    <md-task-item mediaType="facebook" title="MediaType: facebook" queue="IRV_quelle_11">
+    <md-task-item mediaType="facebook" title="MediaType: facebook" item-title="Mihael Varificantare" queue="IRV_quelle_11">
       <div>01:10:25</div>
     </md-task-item>
-    <md-task-item mediaType="chat" title="Mihael Varificantare" queue="IRV_quelle_11">
+    <md-task-item mediaType="chat" title="Mihael Varificantare" item-title="Mihael Varificantare" queue="IRV_quelle_11">
       <div>01:10:25</div>
     </md-task-item>
     <md-task-item
       mediaType="sms"
       status="courtesy_callback"
       title="Mihael Varificantare for test text overflow"
+      itemTitle="Mihael Varificantare for test text overflow"
       queue="IRV_quelle_1167676776767676asdadadas"
     >
       <md-badge slot="task-type" color="darkmint" circle>

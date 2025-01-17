84be1e9dd8d2804d2c519a60fc5c2f9e6a19f8df
Liam Roddy <liroddy@cisco.com>
2025-01-17 12:01:48 +0000

    updated banenr to be closer to spec

--- web-components/src/[sandbox]/examples/alert-banner.ts
+++ web-components/src/[sandbox]/examples/alert-banner.ts
@@ -1,6 +1,14 @@
 import "@/components/alert-banner/AlertBanner";
 import "@/components/button/Button";
+import { Dropdown } from "@/components/dropdown/Dropdown";
 import { customElement, html, LitElement, property } from "lit-element";
+
+enum BannerType {
+  Default = "default",
+  Warning = "warning",
+  Error = "error",
+  Success = "success"
+}
 
 @customElement("alert-banner-template-sandbox")
 export class AlertBannerTemplateSandbox extends LitElement {
@@ -9,14 +17,16 @@ export class AlertBannerTemplateSandbox extends LitElement {
   @property({ type: Boolean }) warning = false;
   @property({ type: Boolean }) error = false;
   @property({ type: Boolean }) success = false;
+  @property({ type: Number }) refreshClickCount = 0;
+
+  iconBannerType = BannerType.Default;
+  showLeftIcon = true;
+  showRefreshButton = true;
 
   async openAlert(kind: string) {
     switch (kind) {
       case "default":
         this.default = true;
-        break;
-      case "refresh":
-        this.refresh = true;
         break;
       case "warning":
         this.warning = true;
@@ -26,6 +36,9 @@ export class AlertBannerTemplateSandbox extends LitElement {
         break;
       case "success":
         this.success = true;
+        break;
+      case "refresh":
+        this.refresh = true;
         break;
       default:
         break;
@@ -38,9 +51,6 @@ export class AlertBannerTemplateSandbox extends LitElement {
       case "default":
         this.default = false;
         break;
-      case "refresh":
-        this.refresh = true;
-        break;
       case "warning":
         this.warning = false;
         break;
@@ -49,6 +59,9 @@ export class AlertBannerTemplateSandbox extends LitElement {
         break;
       case "success":
         this.success = false;
+        break;
+      case "refresh":
+        this.refresh = false;
         break;
       default:
         break;
@@ -69,28 +82,10 @@ export class AlertBannerTemplateSandbox extends LitElement {
         <md-button variant="secondary" @click=${() => this.openAlert("default")}>Trigger Default Alert</md-button>
       </div>
       <div style="margin-bottom: 15px;">
-        <md-button variant="secondary" @click=${() => this.openAlert("refresh")}
-          >Trigger Refresh Alert (inline styled icons and button)</md-button
-        >
-      </div>
-      <div style="margin-bottom: 15px;">
         <md-button variant="secondary" @click=${() => this.openAlert("success")}>Trigger Success Alert</md-button>
       </div>
       <md-alert-banner ?show=${this.default} type="default" @alertBanner-hide=${() => this.hideAlert("default")}>
         Test Alert Message
-      </md-alert-banner>
-      <md-alert-banner ?show=${this.refresh} type="default" @alertBanner-hide=${() => this.hideAlert("refresh")}>
-        <md-icon name="info-badge-filled" iconSet="momentumDesign" size="16"> </md-icon>
-        Refresh to see 5 new interactions
-        <md-button variant="ghostInheritTextColor" circle size="24">
-          <md-icon
-            class="md-alert-banner__text"
-            name="refresh-bold"
-            iconSet="momentumDesign"
-            size="16"
-            style="line-height: 16px"
-          ></md-icon>
-        </md-button>
       </md-alert-banner>
       <md-alert-banner
         ?show="${this.warning}"
@@ -118,6 +113,47 @@ export class AlertBannerTemplateSandbox extends LitElement {
         @alertBanner-hide=${() => this.hideAlert("success")}
         message="success text from a message attribute"
       ></md-alert-banner>
+
+      <h2>New Momentum Alert Banner With Icons</h2>
+      <div style="margin-bottom: 15px; display: flex; align-items: center;">
+        Type:
+        <md-dropdown
+          .options="${Object.values(BannerType)}"
+          .defaultOption="${BannerType.Default}"
+          @dropdown-selected="${(e: CustomEvent<Dropdown.EventDetail["dropdown-selected"]>) => {
+            this.iconBannerType = e.detail.option as BannerType;
+          }}"
+        ></md-dropdown>
+        <md-checkboxgroup group-label="group_process" alignment="horizontal">
+          <md-checkbox
+            slot="checkbox"
+            ?checked=${this.showLeftIcon}
+            @checkbox-change=${() => (this.showLeftIcon = !this.showLeftIcon)}
+            >Show left icon</md-checkbox
+          >
+          <md-checkbox
+            slot="checkbox"
+            ?checked=${this.showRefreshButton}
+            @checkbox-change=${() => (this.showRefreshButton = !this.showRefreshButton)}
+            >Show refresh button</md-checkbox
+          >
+        </md-checkboxgroup>
+        <md-button variant="secondary" @click=${() => this.openAlert("refresh")}>Trigger Alert with Icon</md-button>
+      </div>
+      <md-alert-banner
+        ?showBannerTypeIcon=${this.showLeftIcon}
+        ?showRefreshButton=${this.showRefreshButton}
+        ?show=${this.refresh}
+        type=${this.iconBannerType}
+        @alertBanner-hide=${() => this.hideAlert("refresh")}
+        @alertBanner-refresh-button-click=${() => {
+          this.refreshClickCount++;
+          console.log("Refresh button clicked", this.refreshClickCount, "times.");
+        }}
+      >
+        Refresh to see 5 new interactions
+      </md-alert-banner>
+      <span>Refresh button clicked <strong>${this.refreshClickCount}</strong> times.</span>
     `;
   }
 }

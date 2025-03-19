efa2640464ee702f3f536caae4bde15b811a4c6b
Alan <aspillan@cisco.com>
2025-03-19 09:10:47 +0000

    #CX-17617 Add Card component (#1894)
    * feat: card
    
    * feat: scss
    
    * feat: tests
    
    * feat: sandbox
    
    * feat: stories
    
    * feat: feedback 1
    
    * feat: feedback 2

---
+++ web-components/src/components/card-v2/CardV2.stories.ts
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) Cisco Systems, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import "@/components/card-v2/CardV2";
+import { action } from "@storybook/addon-actions";
+import { CardState } from "@/components/card-v2/CardV2";
+import { Args, Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+const render = (args: Args) => {
+  return html`
+      <md-card-v2 
+        state=${args.state} 
+        identifier=${args.identifier} 
+        header=${args.header} 
+        data=${args.data} 
+        info=${args.info} 
+        .expandable=${args.expandable}
+        @expand-card-toggled=${(e: CustomEvent<{ id: string, active: boolean }>) => {
+          action("expand-card-toggled")(e.detail);
+        }}
+      >
+      </md-card-v2>
+  `;
+};
+
+export const CardV2: StoryObj = {
+  args: {
+    state: CardState.DEFAULT,
+    identifier: "1234567890",
+    header: "Test Title",
+    info: "Test Info",
+    data: "00:00:00",
+    expandable: true
+  },
+  render: render
+};
+
+const meta: Meta = {
+  title: "Components/CardV2",
+  component: "md-card-v2",
+  argTypes: { state: { control: { type: "select" }, options: Object.values(CardState) } },
+  parameters: { a11y: { element: "md-card-v2" } }
+};
+
+export default meta;

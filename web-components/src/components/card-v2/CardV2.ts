e764856ec24f5b94498e7b3365a25673f69801cf
Alan Spillane <aspillan@cisco.com>
2025-05-31 16:08:19 +0100

    chore: card

--- web-components/src/components/card-v2/CardV2.ts
+++ web-components/src/components/card-v2/CardV2.ts
@@ -11,16 +11,20 @@ import "@/components/icon/Icon";
 import "@/components/tooltip/Tooltip";
 import { customElementWithCheck } from "@/mixins/CustomElementCheck";
 import reset from "@/wc_scss/reset.scss";
-import { html, LitElement, property } from "lit-element";
+import { html, internalProperty, LitElement, property, PropertyValues } from "lit-element";
 import { nothing } from "lit-html";
 import { classMap } from "lit-html/directives/class-map";
+import { Duration } from "luxon";
 import styles from "./scss/module.scss";
+
+const DISPLAY_FORMAT = "hh:mm:ss";
 
 export enum CardState {
   DEFAULT = "default",
   ACTIVE = "active",
   INACTIVE = "inactive"
 }
+
 export namespace CardV2 {
   @customElementWithCheck("md-card-v2")
   export class ELEMENT extends LitElement {
@@ -29,25 +33,52 @@ export namespace CardV2 {
     @property({ type: String }) header?: string = undefined;
     @property({ type: String }) info?: string = undefined;
     @property({ type: String }) data?: string = undefined;
+    @property({ type: Number }) createdTime = 0;
+    @property({ type: Boolean }) active = false;
     @property({ type: Boolean, reflect: true }) expandable = false;
+
+    @internalProperty() 
+    private interval: number | undefined;
+
+    @internalProperty()
+    private renderedData = "";
 
     connectedCallback() {
       super.connectedCallback();
+    }
+
+    disconnectedCallback() {
+      super.disconnectedCallback();
+      this.clearInterval();
+    }
+
+    private clearInterval() {
+      if (this.interval) {
+        clearInterval(this.interval);
+        this.interval = undefined;
+      }
     }
 
     static get styles() {
       return [reset, styles];
     }
 
-    private get isActive() {
+    private get isCardActive() {
       return this.state === CardState.ACTIVE;
     }
 
+    protected update(changedProperties: PropertyValues): void {
+      super.update(changedProperties);
+      if (changedProperties.has("active") || changedProperties.has("data") || changedProperties.has("createdTime")) {
+        this.calculateRenderedData();
+      }
+    }
+
     expandCardToggled() {
-      this.state = this.isActive ? CardState.DEFAULT : CardState.ACTIVE;
+      this.state = this.isCardActive ? CardState.DEFAULT : CardState.ACTIVE;
       this.dispatchEvent(
         new CustomEvent<{ identifier: string; active: boolean }>("expand-card-toggled", {
-          detail: { identifier: this.identifier ?? "", active: this.isActive },
+          detail: { identifier: this.identifier ?? "", active: this.isCardActive },
           bubbles: true,
           composed: true
         })
@@ -103,7 +134,7 @@ export namespace CardV2 {
             <md-icon
               slot="icon"
               iconSet="momentumDesign"
-              name=${this.isActive ? "arrow-circle-up-bold" : "arrow-circle-down-bold"}
+              name=${this.isCardActive ? "arrow-circle-up-bold" : "arrow-circle-down-bold"}
               size="18"
             >
             </md-icon>
@@ -112,12 +143,24 @@ export namespace CardV2 {
       `;
     }
 
+    private calculateRenderedData() {
+      this.clearInterval();
+      if (!this.active && this.data) {
+        this.renderedData = this.data;
+      } else if (this.createdTime > 0) {
+        this.renderedData = Duration.fromMillis(Date.now() - this.createdTime).toFormat(DISPLAY_FORMAT);
+        this.interval = window.setInterval(() => {
+          this.renderedData = Duration.fromMillis(Date.now() - this.createdTime).toFormat(DISPLAY_FORMAT);
+        }, 1000);
+      }
+    }
+
     render() {
       return html`
         <div class="${classMap(this.cardClassMap)}" @click=${this.expandCardHandler} >
           <div class="md-card-v2-header">${this.renderHeader()}</div>
           <div class="${classMap(this.contentClassMap)}">
-            <h2>${this.data}</h2>
+            <h2>${this.renderedData}</h2>
           </div>
           ${this.renderFooter()}
         </div>

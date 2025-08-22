e8787e0574cfc4c184d6b6191d1e762d2ec44915
MariooC14 <mario.caval@icloud.com>
2025-08-22 16:08:06 +0100

    feat(list-v2): update storybook, fix horizontal orientation

--- web-components/src/components/list-v2/list-v2.stories.ts
+++ web-components/src/components/list-v2/list-v2.stories.ts
@@ -3,19 +3,16 @@ import { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
 import "./list-v2";
 import "./list-item-v2";
+import { ListV2 } from "./list-v2";
 
-const meta = {
-  title: "Components/ListV2",
-  component: "md-list-v2"
-} satisfies Meta;
+type ListV2Args = {
+  gap: ListV2.Gap;
+  orientation: ListV2.Orientation;
+};
 
-export default meta;
-
-type Story = StoryObj;
-
-const render = () => {
+const template = ({ gap, orientation }: ListV2Args) => {
   return html`
-    <md-list-v2>
+    <md-list-v2 gap=${gap} orientation=${orientation}>
       <md-list-item-v2 label="Item 1" expandable>
           <md-list-v2 slot="panel" sort animation="200" style="display: flex; flex-direction: column; gap:8px;">
             <md-list-item-v2 label="Sub-item 1"></md-list-item-v2>
@@ -31,6 +28,30 @@ const render = () => {
   `;
 };
 
+const meta = {
+  title: "Components/ListV2",
+  component: "md-list-v2",
+  argTypes: {
+    gap: {
+      description: "The gap between list items",
+      control: { type: "select" },
+      options: ["none", "sm", "md", "lg"]
+    },
+    orientation: {
+      control: { type: "select" },
+      options: ["vertical", "horizontal"]
+    }
+  },
+  render: template
+} satisfies Meta<ListV2Args>;
+
+export default meta;
+type Story = StoryObj<ListV2Args>;
+
 export const Example: Story = {
-  render
+  render: template,
+  args: {
+    gap: "sm",
+    orientation: "vertical"
+  }
 };

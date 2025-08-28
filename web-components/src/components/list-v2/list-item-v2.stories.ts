6452ee730ebdac630d6d1e33c4ff63874f598ccd
Mario C <mario.caval@icloud.com>
2025-08-28 13:58:44 +0100

    feat(list-v2): List and ListItem V2 Components (#2034)
    * feat(sortable-list): add sortable list component with item support and styles
    
    * feat(sortable-list): remove expandable prop for second sandbox example
    
    * feat(list-v2): replace sortable list with list-v2
    
    * fix(list-v2): use gap property in example
    
    * feat(list-v2): update storybook, fix horizontal orientation
    
    * fix(list-v2): remove role prop
    
    * feat(list-v2): added list-item-v2 storybook, added disabled look for list-item
    
    * feat(list-item-v2): updated docs to include text color
    
    * fix(list-v2): fix accessbility
    
    * feat(list-v2): simplify list rendering by using only slots
    
    * feat(list-v2): enhance keyboard navigation and accessibility for list items
    
    * fix(list-item-v2): improve mouse event handling
    
    * feat(list-item-v2): added focus ring
    
    * fix(list-item-v2): fix ariaExpanded attribute
    
    * fix(list-v2): update CSS variable names for gap and orientation
    
    * feat(list-item-v2): enhance accessibility and expand functionality with keyboard support
    
    * feat(list-v2): improved story
    
    * feat(list-item-v2): add tests for rendering and interactions, enhance event handling
    
    * fix(list-item-v2): fix keyboard navigation for slotted content when used inside list-v2
    
    * fix(list-v2): improve keyboard navigation
    
    * feat(list-v2): make disabled items not keyboard navigatable
    
    * fix(list-item-v2): added test for list-item-click event on disabled
    
    * feat(list-v2): added unit tests for list-v2
    
    * feat(list-item-v2): add secondary and tertiary labels to list item component, improve documentation
    
    * feat(list-v2): add list-item-v2 to global exports and bump version
    
    ---------
    
    Co-authored-by: James Nestor <87858909+NotNestor@users.noreply.github.com>

---
+++ web-components/src/components/list-v2/list-item-v2.stories.ts
@@ -0,0 +1,164 @@
+import "@/components/chip/Chip";
+import { action } from "@storybook/addon-actions";
+import { Meta, StoryFn, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+import { ifDefined } from "lit/directives/if-defined.js";
+import "./list-item-v2";
+import { ListItemV2 } from "./list-item-v2";
+import "./list-v2";
+
+type ListItemV2Args = Partial<{
+  disabled: boolean;
+  expandable: boolean;
+  label: string;
+  "secondary-label": string;
+  "tertiary-label": string;
+  "expand-label": string;
+  expanded: boolean;
+  variant: ListItemV2.Variant;
+}>;
+
+const meta = {
+  title: "Components/List Item V2",
+  component: "md-list-item-v2",
+  argTypes: {
+    disabled: { control: "boolean" },
+    variant: {
+      control: { type: "select" },
+      options: ["full-width", "inset-rectangle", "inset-pill"]
+    },
+    expandable: { control: "boolean" },
+    expanded: { control: "boolean" },
+    label: { control: "text" },
+    "secondary-label": {
+      control: "text",
+      name: "secondary-label"
+    },
+    "expand-label": {
+      control: "text",
+      name: "expand-label"
+    },
+    "tertiary-label": {
+      control: "text",
+      name: "tertiary-label"
+    }
+  },
+  tags: ["autodocs"],
+  decorators: (story) => html`<div @list-item-click=${action("list-item-click")}>${story()}</div>`
+} satisfies Meta<ListItemV2Args>;
+export default meta;
+
+type Story = StoryObj<ListItemV2Args>;
+
+const PanelContentTemplate = () => html`
+  <div slot="panel">
+    <h3>Expandable Content</h3>
+    <p>
+      Lorem ipsum consequat occaecat anim nisi non ipsum amet nulla dolor dolore amet anim culpa dolore quis ut laboris
+      commodo ea esse
+    </p>
+  </div>
+`;
+
+const Template: StoryFn = ({ disabled, expandable, label, expanded, variant, ...rest }) => html`
+  <md-list-v2>
+    <md-list-item-v2
+      label=${label}
+      secondary-label=${rest["secondary-label"]}
+      tertiary-label=${rest["tertiary-label"]}
+      expand-label=${rest["expand-label"]}
+      ?disabled=${disabled}
+      ?expandable=${expandable}
+      ?expanded=${expanded}
+      variant=${ifDefined(variant)}
+    >
+      ${PanelContentTemplate()}
+    </md-list-item-v2>
+  </md-list-v2>
+`;
+
+export const Default: Story = {
+  render: Template,
+  args: {
+    label: "List Item",
+    "secondary-label": "Secondary Label",
+    "tertiary-label": "Tertiary Label"
+  }
+};
+
+export const Disabled: Story = {
+  render: Template,
+  args: {
+    disabled: true,
+    label: "Disabled List Item"
+  }
+};
+
+export const ExpandableWithTrailingChip: Story = {
+  render: ({ disabled, expandable, label, expanded, variant, ...rest }) => html`
+    <md-list-v2>
+      <md-list-item-v2
+        label=${ifDefined(label)}
+        secondary-label=${ifDefined(rest["secondary-label"])}
+        tertiary-label=${ifDefined(rest["tertiary-label"])}
+        expand-label=${ifDefined(rest["expand-label"])}
+        ?disabled=${disabled}
+        ?expandable=${expandable}
+        ?expanded=${expanded}
+        variant=${ifDefined(variant)}
+      >
+        <md-chip slot="trailing-controls" value="Complete" color="positive" decorative></md-chip>
+        ${PanelContentTemplate()}
+      </md-list-item-v2>
+    </md-list-v2>
+  `,
+  args: {
+    label: "Expandable List Item",
+    expandable: true,
+    expanded: false,
+    variant: "inset-rectangle"
+  }
+};
+
+export const OverrideStyles: Story = {
+  render: Template,
+  args: {
+    label: "List Item with Overridden Background",
+    expandable: true,
+    expanded: true,
+    variant: "inset-rectangle"
+  },
+  decorators: [
+    (story) => html`
+      <div>
+        <style>
+          .custom-bg md-list-item-v2 {
+            --list-item-bg-color: lightblue;
+            --list-item-expand-duration: 100ms;
+          }
+        </style>
+        <div class="custom-bg">${story()}</div>
+      </div>
+    `
+  ],
+  parameters: {
+    docs: {
+      description: {
+        story:
+          "The following css variables are available to style the component:\n\
+* `--list-item-border-radius`\n\
+* `--list-item-bg-color`\n\
+* `--list-item-expand-duration`\n\
+* `--list-item-hover-bg-color`\n\
+* `--list-item-active-bg-color`\n\
+* `--list-item-text-color`\n\
+* `--list-item-disabled-text-color`\n\
+* `--list-item-primary-label-color`\n\
+* `--list-item-secondary-label-color`\n\
+* `--list-item-tertiary-label-color`\n\
+* `--list-item-primary-label-font-size`\n\
+* `--list-item-secondary-label-font-size`"
+      }
+    }
+  }
+};

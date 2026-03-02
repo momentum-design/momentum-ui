162bf6b59d34b54126a9a92bc5e8ffe2b3b354f4
Jason20020 <55873488+Jason20020@users.noreply.github.com>
2026-03-02 15:58:51 +0000

    fix(card-v2): fix card hover state and size (#2122)
    * fix(card-v2): fix card hover state and size
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: jachai <jachai@cisco.com>

---
+++ web-components/src/components/card-v2/tokens/mdv2-card-v2-tokens.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+const cardV2 = {
+  prefix: "mdv2",
+  component: "card-v2",
+  "border-hover": {
+    common: "$mds-color-theme-outline-secondary-normal"
+  }
+};
+
+module.exports = cardV2;
